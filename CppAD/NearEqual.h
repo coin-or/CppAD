83b89bf5d7f8bfc2fa6ca04c11956ca260b9420c
bradbell <bradbell@0e562018-b8fb-0310-a933-ecf073c7c197>
2005-12-14 14:03:06 +0000

    Should have been in previous commit.
    SvnCommit: this file.
    WhatsNew05.omh: users view of changes.
    Doc.omh: use AD sequence glossary entry to describe Base type requirements.
    NearEqual.h: correct OMhelp spelling error.
    NearEqual.cpp: delete (moved to CppAD/NearEqual.h).
    library.omh: use NearEqual.h instead of NearEqual.cpp.
    Example.vcproj: remove NearEqual.cpp and ErrFun.cpp.
    TestMore.vcproj: remove NearEqual.cpp and ErrFun.cpp.
    
    
    git-svn-id: https://projects.coin-or.org/svn/CppAD/trunk@230 0e562018-b8fb-0310-a933-ecf073c7c197

--- CppAD/NearEqual.h
+++ CppAD/NearEqual.h
@@ -22,6 +22,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 /*
 $begin NearEqual$$
 $spell
+	sqrt
 	cout
 	endl
 	Microsoft
@@ -106,7 +107,6 @@ condition (defined above) is satisfied, the return value is true.
 Otherwise, the return value is false.
 
 $head Type$$
-$list number$$
 The type $italic Type$$ must be a
 $xref/NumericType/$$.
 The routine $xref/CheckNumericType/$$ will generate
