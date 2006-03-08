8169e8c0eca73cda41ee8734199d4d08af8eae3e
bradbell <bradbell@0e562018-b8fb-0310-a933-ecf073c7c197>
2006-03-08 15:05:03 +0000

    Improve AD standard math unary function examples.
    SvnCommit: file that preformed this commit.
    WhatsNew06.omh: user's view of change.
    TestMore.cpp: add Sin.cpp, Sinh.cpp.
    TestMore.vcproj: add Sin.cpp, Sinh.cpp.
    Makefile.am: add Sin.cpp, Sinh.cpp.
    Sin.cpp: move Example/Sin.cpp to TestMore/Sin.cpp, new Example/Sin.cpp
    Sinh.cpp: move Example/Sinh.cpp to TestMore/Sinh.cpp, new Example/Sinh.cpp
    Sqrt.cpp: include Example/Sqrt.cpp in TestMore/Sqrt.cpp, new Example/Sqrt.cpp
    
    ----------------------------------------------------------------------
    Edit section above for individual cases, section below stays the same
    ----------------------------------------------------------------------
    
    
    git-svn-id: https://projects.coin-or.org/svn/CppAD/trunk@311 0e562018-b8fb-0310-a933-ecf073c7c197

---
+++ TestMore/Sin.cpp
@@ -0,0 +1,95 @@
+/* -----------------------------------------------------------------------
+CppAD: C++ Algorithmic Differentiation: Copyright (C) 2003-06 Bradley M. Bell
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+------------------------------------------------------------------------ */
+
+/*
+Old example now just used for validation testing.
+*/
+# include <CppAD/CppAD.h>
+
+bool Sin(void)
+{	bool ok = true;
+
+	using CppAD::sin;
+	using CppAD::cos;
+	using namespace CppAD;
+
+	// independent variable vector
+	CppADvector< AD<double> > U(1);
+	U[0]     = 1.;
+	Independent(U);
+
+	// dependent variable vector 
+	CppADvector< AD<double> > Z(1);
+	Z[0] = sin(U[0]); 
+
+	// create f: U -> Z and vectors used for derivative calculations
+	ADFun<double> f(U, Z); 
+	CppADvector<double> v(1);
+	CppADvector<double> w(1);
+
+	// check value 
+	double sin_u = sin( Value(U[0]) );
+	double cos_u = cos( Value(U[0]) );
+
+	ok &= NearEqual(sin_u, Value(Z[0]),  1e-10 , 1e-10);
+
+	// forward computation of partials w.r.t. u
+	size_t j;
+	size_t p     = 5;
+	double jfac  = 1.;
+	v[0]         = 1.;
+	for(j = 1; j < p; j++)
+	{	w     = f.Forward(j, v);	
+
+		double value;
+		if( j % 4 == 1 )
+			value = cos_u;
+		else if( j % 4 == 2 )
+			value = -sin_u;
+		else if( j % 4 == 3 )
+			value = -cos_u;
+		else	value = sin_u;
+
+		jfac *= j;
+		ok &= NearEqual(jfac*w[0], value, 1e-10 , 1e-10); // d^jz/du^j
+		v[0]  = 0.;
+	}
+
+	// reverse computation of partials of Taylor coefficients
+	CppADvector<double> r(p); 
+	w[0]  = 1.;
+	r     = f.Reverse(p, w);
+	jfac  = 1.;
+	for(j = 0; j < p; j++)
+	{
+		double value;
+		if( j % 4 == 0 )
+			value = cos_u;
+		else if( j % 4 == 1 )
+			value = -sin_u;
+		else if( j % 4 == 2 )
+			value = -cos_u;
+		else	value = sin_u;
+
+		ok &= NearEqual(jfac*r[j], value, 1e-10 , 1e-10); // d^jz/du^j
+
+		jfac *= (j + 1);
+	}
+
+	return ok;
+}
