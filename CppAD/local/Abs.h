af760bd8bd1718db94d8726aea54b8dcde4c7754
bradbell <bradbell@0e562018-b8fb-0310-a933-ecf073c7c197>
2006-03-28 15:00:07 +0000

    should have been in previous commit
    git-svn-id: https://projects.coin-or.org/svn/CppAD/trunk@350 0e562018-b8fb-0310-a933-ecf073c7c197

--- CppAD/local/Abs.h
+++ CppAD/local/Abs.h
@@ -23,6 +23,9 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 -------------------------------------------------------------------------------
 $begin abs$$
 $spell
+	Vec
+	std
+	faq
 	Taylor
 	Cpp
 	namespace
