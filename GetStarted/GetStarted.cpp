1870fd3ec67f3d82c9d239fe2f94a360279c7943
bradbell <bradbell@0e562018-b8fb-0310-a933-ecf073c7c197>
2006-06-06 13:20:14 +0000

    Improvements to GetStarted and Introduction.
    SvnCommit: file that preformed this commit.
    WhatsNew06.omh: user's view of the changes.
    ExpApx.omh:  change ExpApx to {\rm ExpApx}.
    ExpApxFor.omh:  change ExpApx to {\rm ExpApx} and include more Partial info.
    ExpApxSeq.omh:  change ExpApx to {\rm ExpApx}.
    GetStarted.cpp: major changes.
    
    ----------------------------------------------------------------------
    Edit section above for individual cases, section below stays the same
    Do not use add_list to add directories; use the following instead:
    	svn add --non-recursive dir
    	svn commit directory -m "adding directory dir" dir
    where dir is replaced by the name of the directory. 
    Then use add_list to add the files within that directory.
    ----------------------------------------------------------------------
    
    
    git-svn-id: https://projects.coin-or.org/svn/CppAD/trunk@424 0e562018-b8fb-0310-a933-ecf073c7c197

--- GetStarted/GetStarted.cpp
+++ GetStarted/GetStarted.cpp
@@ -19,18 +19,18 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 /*
 $begin GetStarted.cpp$$
 $spell
-	http://www.coin-or.org/CppAD/
-	getstarted
-	namespace
-	iostream
-	const
-	std
-	powz
-	Jacobian
-	jac
-	endl
-	da
-	cout
+      http://www.coin-or.org/CppAD/
+      getstarted
+      namespace
+      iostream
+      const
+      std
+      powx
+      Jacobian
+      jac
+      endl
+      da
+      cout
 $$
 
 $section Getting Started Using CppAD$$
@@ -44,24 +44,26 @@ Demonstrate the use of CppAD by computing the derivative
 of a simple example function.
 
 $head Function$$
-The example function $latex f : \R \rightarrow \R$$ is defined by 
+The example function $latex y : \R \rightarrow \R$$ is defined by 
 $latex \[
-	f(z) = a_0 + a_1 * z^1 + \cdots + a_{k-1} * z^{k-1}
+      y(x) = a_0 + a_1 * x^1 + \cdots + a_{k-1} * x^{k-1}
 \] $$
-where $italic a$$ is a vector of length $italic k$$.
+where $italic a$$ is a fixed vector of length $italic k$$.
 
 $head Derivative$$
-The derivative of the example function is given by
+The derivative of $latex y(x)$$ is given by
 $latex \[
-	f^{(1)} (z) = a_1 + 2 * a_2 * z +  \cdots + (k-1) * a_{k-1} * z^{k-2} 
+      y'(x) = a_1 + 2 * a_2 * x +  \cdots + (k-1) * a_{k-1} * x^{k-2} 
 \] $$
 
 $head Value$$
-For the particular case in the example,
-$latex k$$ is equal to 5, $latex a = (1, 1, 1, 1, 1)$$, and $latex z = 3$$.
+For the particular case in this example,
+$latex k$$ is equal to 5, 
+$latex a = (1, 1, 1, 1, 1)$$, and 
+$latex x = 3$$.
 If follows that 
 $latex \[
-	f' ( 3 ) = 1 + 2 * 3 + 3 * 3^2 + 4 * 3^3 = 142
+      y'( 3 ) = 1 + 2 * 3 + 3 * 3^2 + 4 * 3^3 = 142
 \] $$
 
 $head Poly$$
@@ -72,66 +74,69 @@ distributed with CppAD (see $xref/Poly/$$).
 
 $head Source Code$$
 $codep */
-# include <iostream>      // standard input/output 
-# include <vector>        // standard vector
-# include <CppAD/CppAD.h> // the CppAD package http://www.coin-or.org/CppAD/
+#include <iostream>      // standard input/output 
+#include <vector>        // standard vector
+#include <CppAD/CppAD.h> // the CppAD package http://www.coin-or.org/CppAD/
 
-namespace { // empty namespace
-	template <class Type>
-	Type Poly(const std::vector<double> &a, const Type &z)
-	{	size_t k  = a.size();
-		Type sum  = 0.;
-		Type powz = 1.;
-		size_t i;
-		for(i = 0; i < k; i++)
-		{	sum  += a[i] * powz;
-			powz *= z;
-		}
-		return sum;
-	}
+namespace { 
+      // define y(x) = Poly(a, x) in the empty namespace
+      template <class Type>
+      Type Poly(const std::vector<double> &a, const Type &x)
+      {     size_t k  = a.size();
+            Type y   = 0.;  // initialize summation
+            Type x_i = 1.;  // initialize x^i
+            size_t i;
+            for(i = 0; i < k; i++)
+            {     y   += a[i] * x_i;  // y   = y + a_i * x^i
+                  x_i *= x;           // x_i = x_i * x
+            }
+            return y;
+      }
 }
-int main(void)             // begin main program
-{	using CppAD::AD;   // use AD in place of CppAD::AD
-	using std::vector; // use vector in place of std::vector
-	size_t i;          // temporary index
+// main program
+int main(void)
+{     using CppAD::AD;           // use AD as abbreviation for CppAD::AD
+      using std::vector;         // use vector as abbreviation for std::vector
+      size_t i;                  // a temporary index
 
-	// vector of polynomial coefficients
-	size_t k = 5;            // size of the vector a
-	vector<double> a(k);     // vector with double elements
-	for(i = 0; i < k; i++)
-		a[i] = 1.;       // value of the elements of a
+      // vector of polynomial coefficients
+      size_t k = 5;              // number of polynomial coefficients
+      vector<double> a(k);       // vector of polynomial coefficients
+      for(i = 0; i < k; i++)
+            a[i] = 1.;           // value of polynomial coefficients
 
-	// domain space vector
-	size_t n = 1;
-	vector< AD<double> > Z(n);
-	Z[0] = 3.;
+      // domain space vector
+      size_t n = 1;              // number of domain space variables
+      vector< AD<double> > X(n); // vector of domain space variables
+      X[0] = 3.;                 // value corresponding to operation sequence
 
-	// declare independent variables and start tape recording
-	CppAD::Independent(Z);
+      // declare independent variables and start recording operation sequence
+      CppAD::Independent(X);
 
-	// range space vector
-	size_t m = 1;
-	vector< AD<double> > P(m);      
-	P[0] = Poly(a, Z[0]);
+      // range space vector
+      size_t m = 1;              // number of ranges space variables
+      vector< AD<double> > Y(m); // vector of ranges space variables
+      Y[0] = Poly(a, X[0]);      // value during recording of operations
 
-	// create f: Z -> P and stop tape recording
-	CppAD::ADFun<double> f(Z, P);
+      // store operation sequence in f: X -> Y and stop recording
+      CppAD::ADFun<double> f(X, Y);
 
-	// compute derivative of f
-	vector<double> jac(m * n);   // Jacobian of f (m by n matrix)
-	vector<double> z(n);         // argument value to compute Jacobian at
-	z[0] = 3.; 
-	jac  = f.Jacobian(z);        // value of Jacobian at z
+      // compute derivative using operation sequence stored in f
+      vector<double> jac(m * n); // Jacobian of f (m by n matrix)
+      vector<double> x(n);       // domain space vector
+      x[0] = 3.;                 // argument value for derivative
+      jac  = f.Jacobian(x);      // Jacobian for operation sequence
 
-	// print the results
-	std::cout << "f'(z) computed by CppAD = " << jac[0] << std::endl;
+      // print the results
+      std::cout << "y'(3) computed by CppAD = " << jac[0] << std::endl;
 
-	// check if the derivative is correct
-	int error_code;
-	if( jac[0] == 142. )
-		error_code = 0;  // correct answer
-	else	error_code = 1;  // incorrect answer
-	return error_code;
+      // check if the derivative is correct
+      int error_code;
+      if( jac[0] == 142. )
+            error_code = 0;      // return code for correct case
+      else  error_code = 1;      // return code for incorrect case
+
+      return error_code;
 }
 /* $$
 $head Output$$
