a53bb777c35742143ba645b62a2e909bd8b16225
bradbell <bradbell@0e562018-b8fb-0310-a933-ecf073c7c197>
2006-07-14 14:43:23 +0000

    trunk: Make some improvements for DIMACS workshop presentaiton
    SvnCommit: file that made this commit.
    WhatsNew06.omh: users view of the changes.
    ExpApx.hpp: add comments to code section.
    Introduction.omh: break text up using more headings and subheadings.
    ExpApxSeq.omh: minor heading changes.
    ExpApxRev.omh: use ration notation for partial derivatives.
    
    
    
    git-svn-id: https://projects.coin-or.org/svn/CppAD/trunk@460 0e562018-b8fb-0310-a933-ecf073c7c197

--- Introduction/ExpApx.hpp
+++ Introduction/ExpApx.hpp
@@ -30,16 +30,18 @@ $section An Example Exponential Approximation Source Code$$
 $codep */
 template <class Type>
 Type ExpApx(const Type &x, const Type &e)
-{	Type a, q, r, s, k;
-	r = s = k = Type(1);
-	a = x;
+{	Type a, q, r, s, k;           // declare local variables
+	r = s = k = Type(1);          // initialize as equal to one
+	a = x;                        // a = |x|
 	if( Type(0) > x ) a = -x;
-	while(r > e) 
-	{	q  = a * r;
-		r  = q / k;
-		s  = s + r;
+	// while the previous term included in the summation is greater than e
+	while(r > e)
+	{	q  = a * r;           // q = |x|^k / (k-1)!
+		r  = q / k;           // r = |x|^k / k!
+		s  = s + r;           // s = 1 + |x| / 1! + ... + |x|^k / k!
 		k  = k + Type(1);
 	}
+	// In the case where x is negative, use exp(x) = 1 / exp(-|x|)
 	if( Type(0) > x ) s = Type(1) / s;
 	return s;
 }
