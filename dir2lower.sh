378a70c7f45587eaf24e156cac43f53822a2192e
bradbell <bradbell@0e562018-b8fb-0310-a933-ecf073c7c197>
2006-11-04 19:39:45 +0000

    improve directory to lower case script
    git-svn-id: https://projects.coin-or.org/svn/CppAD/trunk@605 0e562018-b8fb-0310-a933-ecf073c7c197

--- dir2lower.sh
+++ dir2lower.sh
@@ -1,24 +1,33 @@
 #! /bin/bash
 # file that moves a directory to a lower case name version
-old="SpeedExample"
-new="speed_example"
+old="Speed"
+new="speed_"
 #
-svn move $old $new
+if [ ! -e $new ]
+then
+	svn move $old $new
+fi
 list="
 	Build.sh
 	CheckIncludeFile.sh
 	file2lower.sh
 	Makefile.am
 	configure.ac
+	omh/install_unix.omh
+	omh/install_windows.omh
+	omh/speed.omh
 	$new/*
 "
 dollar='$'
 for file in $list 
 do
+if [ ! -d "$file" ]
+then
 	echo $file
 	svn revert $file
 	sed < $file > dir2lower.tmp \
-		-e "s|$old\([\/]\)|$new\1|g" \
+		-e "s|\([^-]\)$old\([\/]\)|\1$new\2|g" \
+		-e "s|^$old\([\/]\)|$new\1|g" \
 		-e "s|\([= 	\/][= 	\/]*\)$old[ 	]*$|\1$new|"
 	diff $file dir2lower.tmp
 	mv dir2lower.tmp $file
@@ -27,4 +36,9 @@ do
 	then
 		chmod +x $file
 	fi
+	if [ "$ext" = "sln" ] || [ "$ext" = "vcproj" ]
+	then
+		unix2dos $file
+	fi
+fi
 done
