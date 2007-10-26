54f592c1ed9156507af293c109d1cccbcd85f8b5
bradbell <bradbell@0e562018-b8fb-0310-a933-ecf073c7c197>
2007-10-26 13:18:51 +0000

    stable/2.0: add more builts sources
    git-svn-id: https://projects.coin-or.org/svn/CppAD/trunk@1027 0e562018-b8fb-0310-a933-ecf073c7c197

--- cppad/config.h
+++ cppad/config.h
@@ -1,14 +1,14 @@
 /* cppad/config.h.  Generated from config.h.in by configure.  */
 /* cppad/config.h.in.  Generated from configure.ac by autoheader.  */
 
-/* is CppADvector == boost::numeric::ublas::vector */
-#define CPPAD_BOOSTVECTOR 0
+/* is CPPAD_TEST_VECTOR equal to boost::numeric::ublas::vector */
+#define CPPAD_BOOSTVECTOR 1
 
 /* is CppADvector == CppAD::vector */
-#define CPPAD_CPPADVECTOR 1
+#define CPPAD_CPPADVECTOR 0
 
 /* does system support gettimeofday (overidden if _MSC_VER defined) */
-#define CPPAD_GETTIMEOFDAY 0
+#define CPPAD_GETTIMEOFDAY 1
 
 /* is CppADvector == std::vector */
 #define CPPAD_STDVECTOR 0
@@ -23,16 +23,16 @@
 #define PACKAGE_NAME "CppAD"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "CppAD 20071022"
+#define PACKAGE_STRING "CppAD 20071024"
 
 /* Define to the one symbol short name of this package. */
 #define PACKAGE_TARNAME "cppad"
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "20071022"
+#define PACKAGE_VERSION "20071024"
 
 /* Version number of package */
-#define VERSION "20071022"
+#define VERSION "20071024"
 
 /* Define to `__inline__' or `__inline' if that's what the C compiler
    calls it, or to nothing if 'inline' is not supported under any name.  */
