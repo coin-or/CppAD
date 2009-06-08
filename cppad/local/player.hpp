7ea87bbe1952770899270d3b43cac0af0d60c11b
bradbell <bradbell@0e562018-b8fb-0310-a933-ecf073c7c197>
2009-06-08 15:00:23 +0000

    trunk: Improve player private variable names.
    This is preparation for more privatve variables in this class.
    
    
    
    git-svn-id: https://projects.coin-or.org/svn/CppAD/trunk@1382 0e562018-b8fb-0310-a933-ecf073c7c197

--- cppad/local/player.hpp
+++ cppad/local/player.hpp
@@ -23,170 +23,175 @@ template <class Base>
 class player {
 private:
 	/// Number of variables in the recording.
-	size_t    TotalNumberVar_;
+	size_t    num_rec_var_;
 
 	/// Number of operators in the recording. 
-	size_t    NumberOp_;
+	size_t    num_rec_op_;
 
 	/// The operators in the recording.
-	OpCode   *Op_;
+	OpCode   *rec_op_;
 
 	/// Number of VecAD indices in the recording. 
-	size_t    NumberVecInd_;
+	size_t    num_rec_vecad_ind_;
 
 	/// The VecAD indices in the recording.
-	size_t   *VecInd_;
+	size_t   *rec_vecad_ind_;
 
 	/// Number of operation indices in the recording. 
-	size_t    NumberInd_;
+	size_t    num_rec_op_ind_;
 
 	/// The operation indices in the recording
-	size_t   *Ind_;
+	size_t   *rec_op_ind_;
 
 	/// Number of parameters in the recording.
-	size_t    NumberPar_;
+	size_t    num_rec_par_;
 
 	/// The parameters in the recording.
-	Base     *Par_;
+	Base     *rec_par_;
 
 	/// Number of text characters 
 	/// (representing '\\0' terminated strings) in the recording.
-	size_t    NumberTxt_;
+	size_t    num_rec_text_;
 
 	/// Character in the recording.
-	char     *Txt_;
+	char     *rec_text_;
 
 public:
 	/// Default constructor
 	player(void) 
 	{	
-		TotalNumberVar_ = 0;
+		num_rec_var_       = 0;
 
-		NumberOp_       = 0;
-		Op_             = CPPAD_NULL;
+		num_rec_op_        = 0;
+		rec_op_            = CPPAD_NULL;
 
-		NumberVecInd_   = 0;
-		VecInd_         = CPPAD_NULL;
+		num_rec_vecad_ind_ = 0;
+		rec_vecad_ind_     = CPPAD_NULL;
 
-		NumberInd_      = 0;
-		Ind_            = CPPAD_NULL;
+		num_rec_op_ind_    = 0;
+		rec_op_ind_        = CPPAD_NULL;
 
-		NumberPar_      = 0;
-		Par_            = CPPAD_NULL;
+		num_rec_par_       = 0;
+		rec_par_           = CPPAD_NULL;
 
-		NumberTxt_      = 0;
-		Txt_            = CPPAD_NULL;
+		num_rec_text_      = 0;
+		rec_text_          = CPPAD_NULL;
 
 	}
 
 	/// Destructor
 	~player(void)
-	{	if( NumberOp_ > 0 )
-			CPPAD_TRACK_DEL_VEC(Op_);
-		if( NumberVecInd_ > 0 )
-			CPPAD_TRACK_DEL_VEC(VecInd_);
-		if( NumberInd_ > 0 )
-			CPPAD_TRACK_DEL_VEC(Ind_);
-		if( NumberPar_ > 0 )
-			CPPAD_TRACK_DEL_VEC(Par_);
-		if( NumberTxt_ > 0 )
-			CPPAD_TRACK_DEL_VEC(Txt_);
+	{	if( num_rec_op_ > 0 )
+			CPPAD_TRACK_DEL_VEC(rec_op_);
+		if( num_rec_vecad_ind_ > 0 )
+			CPPAD_TRACK_DEL_VEC(rec_vecad_ind_);
+		if( num_rec_op_ind_ > 0 )
+			CPPAD_TRACK_DEL_VEC(rec_op_ind_);
+		if( num_rec_par_ > 0 )
+			CPPAD_TRACK_DEL_VEC(rec_par_);
+		if( num_rec_text_ > 0 )
+			CPPAD_TRACK_DEL_VEC(rec_text_);
 	}
 
 	/// Creating an operation sequence player from a recorder.
 	void operator=(const recorder<Base> &rec)
 	{	size_t i;
 
-		if( NumberOp_ > 0 )
-			CPPAD_TRACK_DEL_VEC(Op_);
-		if( NumberVecInd_ > 0 )
-			CPPAD_TRACK_DEL_VEC(VecInd_);
-		if( NumberInd_ > 0 )
-			CPPAD_TRACK_DEL_VEC(Ind_);
-		if( NumberPar_ > 0 )
-			CPPAD_TRACK_DEL_VEC(Par_);
-		if( NumberTxt_ > 0 )
-			CPPAD_TRACK_DEL_VEC(Txt_);
+		if( num_rec_op_ > 0 )
+			CPPAD_TRACK_DEL_VEC(rec_op_);
+		if( num_rec_vecad_ind_ > 0 )
+			CPPAD_TRACK_DEL_VEC(rec_vecad_ind_);
+		if( num_rec_op_ind_ > 0 )
+			CPPAD_TRACK_DEL_VEC(rec_op_ind_);
+		if( num_rec_par_ > 0 )
+			CPPAD_TRACK_DEL_VEC(rec_par_);
+		if( num_rec_text_ > 0 )
+			CPPAD_TRACK_DEL_VEC(rec_text_);
 
 		// Var
-		TotalNumberVar_  = rec.TotalNumberVar_;
+		num_rec_var_        = rec.TotalNumberVar_;
 
 		// Op
-		NumberOp_        = rec.NumberOp_;
+		num_rec_op_         = rec.NumberOp_;
 
 		// VecInd
-		NumberVecInd_    = rec.NumberVecInd_;
+		num_rec_vecad_ind_  = rec.NumberVecInd_;
 
 		// Ind
-		NumberInd_       = rec.NumberInd_;
+		num_rec_op_ind_     = rec.NumberInd_;
 
 		// Par
-		NumberPar_       = rec.NumberPar_;
+		num_rec_par_        = rec.NumberPar_;
 
 		// Txt
-		NumberTxt_       = rec.NumberTxt_;
+		num_rec_text_       = rec.NumberTxt_;
 
 		// Allocate the memory
-		if( NumberOp_ == 0 )
-			Op_ = CPPAD_NULL;
-		else	Op_ = CPPAD_TRACK_NEW_VEC(NumberOp_,      Op_);
-		if( NumberVecInd_ == 0 )
-			VecInd_ = CPPAD_NULL;
-		else	VecInd_ = CPPAD_TRACK_NEW_VEC(NumberVecInd_, VecInd_);
-		if( NumberInd_ == 0 )
-			Ind_ = CPPAD_NULL;
-		else	Ind_ = CPPAD_TRACK_NEW_VEC(NumberInd_,       Ind_);
-		if( NumberPar_ == 0 )
-			Par_ = CPPAD_NULL;
-		else	Par_ = CPPAD_TRACK_NEW_VEC(NumberPar_,       Par_);
-		if( NumberTxt_ == 0 )
-			Txt_ = CPPAD_NULL;
-		else	Txt_ = CPPAD_TRACK_NEW_VEC(NumberTxt_,       Txt_);
+		if( num_rec_op_ == 0 )
+			rec_op_ = CPPAD_NULL;
+		else	rec_op_ = 
+			CPPAD_TRACK_NEW_VEC(num_rec_op_,        rec_op_);
+		if( num_rec_vecad_ind_ == 0 )
+			rec_vecad_ind_ = CPPAD_NULL;
+		else	rec_vecad_ind_ = 
+			CPPAD_TRACK_NEW_VEC(num_rec_vecad_ind_, rec_vecad_ind_);
+		if( num_rec_op_ind_ == 0 )
+			rec_op_ind_ = CPPAD_NULL;
+		else	rec_op_ind_ = 
+			CPPAD_TRACK_NEW_VEC(num_rec_op_ind_,    rec_op_ind_);
+		if( num_rec_par_ == 0 )
+			rec_par_ = CPPAD_NULL;
+		else	rec_par_ = 
+			CPPAD_TRACK_NEW_VEC(num_rec_par_,       rec_par_);
+		if( num_rec_text_ == 0 )
+			rec_text_ = CPPAD_NULL;
+		else	rec_text_ = 
+			CPPAD_TRACK_NEW_VEC(num_rec_text_,      rec_text_);
 
 		// Copy the data
-		i = NumberOp_;
+		i = num_rec_op_;
 		while(i--)
-			Op_[i] = rec.Op_[i];
-		i = NumberVecInd_;
+			rec_op_[i] = rec.Op_[i];
+		i = num_rec_vecad_ind_;
 		while(i--)
-			VecInd_[i] = rec.VecInd_[i];
-		i = NumberInd_;
+			rec_vecad_ind_[i] = rec.VecInd_[i];
+		i = num_rec_op_ind_;
 		while(i--)
-			Ind_[i] = rec.Ind_[i];
-		i = NumberPar_;
+			rec_op_ind_[i] = rec.Ind_[i];
+		i = num_rec_par_;
 		while(i--)
-			Par_[i] = rec.Par_[i];
-		i = NumberTxt_;
+			rec_par_[i] = rec.Par_[i];
+		i = num_rec_text_;
 		while(i--)
-			Txt_[i] = rec.Txt_[i];
+			rec_text_[i] = rec.Txt_[i];
 	}
 
 	/// Erase all information in an operation sequence player.
 	void Erase(void)
 	{	
-		TotalNumberVar_  = 0;
-		NumberOp_        = 0;
-		NumberVecInd_    = 0;
-		NumberInd_       = 0;
-		NumberPar_       = 0;
-		NumberTxt_       = 0;
+		num_rec_var_  = 0;
+		num_rec_op_        = 0;
+		num_rec_vecad_ind_    = 0;
+		num_rec_op_ind_       = 0;
+		num_rec_par_       = 0;
+		num_rec_text_       = 0;
 
-		if( NumberOp_ > 0 )
-			CPPAD_TRACK_DEL_VEC(Op_);
-		if( NumberVecInd_ > 0 )
-			CPPAD_TRACK_DEL_VEC(VecInd_);
-		if( NumberInd_ > 0 )
-			CPPAD_TRACK_DEL_VEC(Ind_);
-		if( NumberPar_ > 0 )
-			CPPAD_TRACK_DEL_VEC(Par_);
-		if( NumberTxt_ > 0 )
-			CPPAD_TRACK_DEL_VEC(Txt_);
+		if( num_rec_op_ > 0 )
+			CPPAD_TRACK_DEL_VEC(rec_op_);
+		if( num_rec_vecad_ind_ > 0 )
+			CPPAD_TRACK_DEL_VEC(rec_vecad_ind_);
+		if( num_rec_op_ind_ > 0 )
+			CPPAD_TRACK_DEL_VEC(rec_op_ind_);
+		if( num_rec_par_ > 0 )
+			CPPAD_TRACK_DEL_VEC(rec_par_);
+		if( num_rec_text_ > 0 )
+			CPPAD_TRACK_DEL_VEC(rec_text_);
 
-		NumberOp_        = 0;
-		NumberVecInd_    = 0;
-		NumberInd_       = 0;
-		NumberPar_       = 0;
-		NumberTxt_       = 0;
+		num_rec_op_        = 0;
+		num_rec_vecad_ind_    = 0;
+		num_rec_op_ind_       = 0;
+		num_rec_par_       = 0;
+		num_rec_text_       = 0;
 	}
 
 	/*! 
@@ -200,8 +205,8 @@ public:
 	the index of the operator in recording
 	*/
 	OpCode GetOp (size_t i) const
-	{	CPPAD_ASSERT_UNKNOWN(i < NumberOp_);
-		return Op_[i];
+	{	CPPAD_ASSERT_UNKNOWN(i < num_rec_op_);
+		return rec_op_[i];
 	}
 
 	/*! 
@@ -215,8 +220,8 @@ public:
 	the index of the VecAD index in recording
 	*/
 	size_t GetVecInd (size_t i) const
-	{	CPPAD_ASSERT_UNKNOWN(i < NumberVecInd_);
-		return VecInd_[i];
+	{	CPPAD_ASSERT_UNKNOWN(i < num_rec_vecad_ind_);
+		return rec_vecad_ind_[i];
 	}
 
 	/*! 
@@ -230,8 +235,8 @@ public:
 	the index of the parameter in recording
 	*/
 	const Base *GetPar(size_t i) const
-	{	CPPAD_ASSERT_UNKNOWN(i < NumberPar_);
-		return Par_ + i;
+	{	CPPAD_ASSERT_UNKNOWN(i < num_rec_par_);
+		return rec_par_ + i;
 	}
 
 	/*! 
@@ -248,8 +253,8 @@ public:
 	the index where the subsequence begins. 
 	*/
 	const size_t *GetInd(size_t n, size_t i) const
-	{	CPPAD_ASSERT_UNKNOWN(i + n <= NumberInd_)
-		return Ind_ + i;
+	{	CPPAD_ASSERT_UNKNOWN(i + n <= num_rec_op_ind_)
+		return rec_op_ind_ + i;
 	}
 
 	/*! 
@@ -263,8 +268,8 @@ public:
 	the index where the string begins. 
 	*/
 	const char *GetTxt(size_t i) const
-	{	CPPAD_ASSERT_UNKNOWN(i < NumberTxt_);
-		return Txt_ + i;
+	{	CPPAD_ASSERT_UNKNOWN(i < num_rec_text_);
+		return rec_text_ + i;
 	}
 	
 	/*!
@@ -278,41 +283,41 @@ public:
 	is the new normal index value.
 	*/
 	void ReplaceInd(size_t i, size_t value)
-	{	CPPAD_ASSERT_UNKNOWN( i < NumberInd_);
-		Ind_[i] = value;
+	{	CPPAD_ASSERT_UNKNOWN( i < num_rec_op_ind_);
+		rec_op_ind_[i] = value;
 	}
 
 	/// Fetch number of variables in the recording.
 	size_t TotNumVar(void) const
-	{	return TotalNumberVar_; }
+	{	return num_rec_var_; }
 
 	/// Fetch number of operators in the recording.
 	size_t NumOp(void) const
-	{	return NumberOp_; }
+	{	return num_rec_op_; }
 
 	/// Fetch number of VecAD indices in the recording.
 	size_t NumVecInd(void) const
-	{	return NumberVecInd_; }
+	{	return num_rec_vecad_ind_; }
 
 	/// Fetch number of normal indices in the recording.
 	size_t NumInd(void) const
-	{	return NumberInd_; }
+	{	return num_rec_op_ind_; }
 
 	/// Fetch number of parameters in the recording.
 	size_t NumPar(void) const
-	{	return NumberPar_; }
+	{	return num_rec_par_; }
 
 	/// Fetch number of characters (representing strings) in the recording.
 	size_t NumTxt(void) const
-	{	return NumberTxt_; }
+	{	return num_rec_text_; }
 
 	/// Fetch a rough measure of amount of memory used by the recording. 
 	size_t Memory(void) const
-	{	return NumberOp_ * sizeof(OpCode) 
-		     + NumberVecInd_ * sizeof(size_t)
-		     + NumberInd_ * sizeof(size_t)
-		     + NumberPar_ * sizeof(Base)
-		     + NumberTxt_ * sizeof(char);
+	{	return num_rec_op_ * sizeof(OpCode) 
+		     + num_rec_vecad_ind_ * sizeof(size_t)
+		     + num_rec_op_ind_ * sizeof(size_t)
+		     + num_rec_par_ * sizeof(Base)
+		     + num_rec_text_ * sizeof(char);
 	}
 
 };
