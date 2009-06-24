ac35197d99aafa1ec14b722a7e44671f2d7e413c
bradbell <bradbell@0e562018-b8fb-0310-a933-ecf073c7c197>
2009-06-24 18:01:58 +0000

    trunk: missing from previous commit
    git-svn-id: https://projects.coin-or.org/svn/CppAD/trunk@1402 0e562018-b8fb-0310-a933-ecf073c7c197

--- svn_commit.sh
+++ svn_commit.sh
@@ -13,7 +13,7 @@
 # Define your subversion commit by editing the definition of 
 # log_entry, add_list, delete_list, and change_list below:
 # 
-log_entry="Merge in changes from branches/sweep.
+log_entry="
 " 
 add_list="
 "
@@ -23,78 +23,6 @@ old_list="
 "
 #
 change_list="
-	configure
-	svn_status.sh
-	check_doxygen.sh
-	AUTHORS
-	svn_commit.sh
-	check_include_file.sh
-	example/makefile.in
-	example/example.cpp
-	doc.omh.in
-	svn_merge.sh
-	test_more/makefile.in
-	test_more/test_more.cpp
-	test_more/ipopt_cppad.cpp
-	print_for/makefile.in
-	ipopt_cppad/makefile.in
-	ipopt_cppad/ipopt_cppad_ode.cpp
-	ipopt_cppad/ipopt_cppad_simple.cpp
-	configure.ac
-	build.sh
-	makefile.am
-	introduction/exp_apx/makefile.in
-	introduction/exp_apx/main.cpp
-	introduction/get_started/makefile.in
-	makefile.in
-	omh/whats_new_04.omh
-	omh/whats_new_06.omh
-	omh/whats_new_09.omh
-	omh/install_unix.omh.in
-	cppad/speed_test.hpp
-	cppad/local/ad_fun.hpp
-	cppad/local/player.hpp
-	cppad/local/abs_op.hpp
-	cppad/local/for_jac_sweep.hpp
-	cppad/local/reverse.hpp
-	cppad/local/rev_sparse_jac.hpp
-	cppad/local/unary_op.hpp
-	cppad/local/forward.hpp
-	cppad/local/op_code.hpp
-	cppad/local/rev_sparse_hes.hpp
-	cppad/local/config.h.in
-	cppad/local/forward0sweep.hpp
-	cppad/local/independent.hpp
-	cppad/local/recorder.hpp
-	cppad/local/forward_sweep.hpp
-	cppad/local/cppad_assert.hpp
-	cppad/local/rev_jac_sweep.hpp
-	cppad/local/vec_ad.hpp
-	cppad/local/define.hpp
-	cppad/local/fun_construct.hpp
-	cppad/local/rev_hes_sweep.hpp
-	cppad/local/dependent.hpp
-	cppad/local/op.hpp
-	cppad/local/reverse_sweep.hpp
-	cppad/config.h
-	cppad/configure.hpp.in
-	cppad/error_handler.hpp
-	cppad/cppad.hpp
-	cppad/configure.hpp
-	openmp/example_a11c.cpp
-	openmp/multi_newton.cpp
-	openmp/sum_i_inv.cpp
-	openmp/run.sh
-	speed/profile/makefile.in
-	speed/fadbad/makefile.in
-	speed/adolc/makefile.in
-	speed/main.cpp
-	speed/cppad/makefile.in
-	speed/double/makefile.in
-	speed/sacado/makefile.in
-	speed/example/makefile.in
-	speed/example/example.cpp
-	dev.omh
 "
 #
 # log_entry
