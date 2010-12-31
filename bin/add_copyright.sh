995f2ac6712585198988bb4d78a715357fe2b7b0
bradbell <bradbell@0e562018-b8fb-0310-a933-ecf073c7c197>
2010-12-31 05:34:09 +0000

    Delee not_avaiable, svn_add_id and move add_copyright.sh to bin directory.
    git-svn-id: https://projects.coin-or.org/svn/CppAD/trunk@1764 0e562018-b8fb-0310-a933-ecf073c7c197

--- bin/add_copyright.sh
+++ bin/add_copyright.sh
@@ -1,22 +1,38 @@
-#! /bin/bash
+#! /bin/bash -e
+# $Id$
+# -----------------------------------------------------------------------------
+# CppAD: C++ Algorithmic Differentiation: Copyright (C) 2003-10 Bradley M. Bell
 #
+# CppAD is distributed under multiple licenses. This distribution is under
+# the terms of the
+#                     Common Public License Version 1.0.
+#
+# A copy of this license is included in the COPYING file of this distribution.
+# Please visit http://www.coin-or.org/CppAD/ for information on other licenses.
+# -----------------------------------------------------------------------------
+if [ $0 != "bin/add_copyright.sh" ]
+then
+	echo "bin/add_copyright.sh: must be executed from its parent directory"
+	exit 1
+fi
+# -----------------------------------------------------------------------------
 if [ "$1" == "" ]
 then
-	echo "add_copyright.sh: file_name"
+	echo "./add_copyright.sh: file_name"
 	exit 1
 fi
 file_name="$1"
 ext=`echo $file_name | sed -e 's/.*\.//'`
 if [ "$ext" == "" ]
 then
-	echo "add_copyright.sh: file_name does not have an extension"
+	echo "./add_copyright.sh: file_name does not have an extension"
 	exit 1
 fi
 #
-echo "write copyright message to add_copyright.$$"
+echo "write copyright message to bin/add_copyright.$$"
 case $ext in
 	cpp | hpp | omh)
-	cat << EOF  > add_copyright.$$
+	cat << EOF  > bin/add_copyright.$$
 /* --------------------------------------------------------------------------
 CppAD: C++ Algorithmic Differentiation: Copyright (C) 2003-10 Bradley M. Bell
 
@@ -31,7 +47,7 @@ EOF
 	;;
 
 	sh)
-	cat << EOF  > add_copyright.$$
+	cat << EOF  > bin/add_copyright.$$
 # -----------------------------------------------------------------------------
 # CppAD: C++ Algorithmic Differentiation: Copyright (C) 2003-10 Bradley M. Bell
 #
@@ -46,15 +62,15 @@ EOF
 	;;
 
 	*)
-	echo "add_copyright.sh: extension $ext is not yet supported"
+	echo bin/"add_copyright.sh: extension $ext is not yet supported"
 	exit 1
 esac
 #
-echo "cat $file_name >> add_copyright.$$"
-cat $file_name >> add_copyright.$$
+echo "cat $file_name >> bin/add_copyright.$$"
+cat $file_name >> bin/add_copyright.$$
 #
 echo "mv $file_name ~/trash"
 mv $file_name ~/trash
 #
-echo "mv add_copyright.$$ $file_name"
-mv add_copyright.$$ $file_name
+echo "mv bin/add_copyright.$$ $file_name"
+mv bin/add_copyright.$$ $file_name
