bfb25e604321ec5b50d13842a7c528fb10d5f677
bradbell <bradbell@0e562018-b8fb-0310-a933-ecf073c7c197>
2011-01-19 05:20:53 +0000

    Update version and date: AUTHORS, configure, configure.ac, config.h, configure.hpp
    configure: changes automatically transerred from configure.ac.
    makefile.am: remove duplicate test resuld in output, put pkgconfig in datadir.
    makefile.in: changes automatically transfered from makefile.am
    
    git-svn-id: https://projects.coin-or.org/svn/CppAD/trunk@1820 0e562018-b8fb-0310-a933-ecf073c7c197

--- cppad/configure.hpp
+++ cppad/configure.hpp
@@ -60,7 +60,7 @@ yyyy is year, mm is month, and dd is day.
 \def CPPAD_PACKAGE_STRING
 cppad-yyyymmdd as a C string where yyyy is year, mm is month, and dd is day.
 */
-# define CPPAD_PACKAGE_STRING "CppAD 20110116"
+# define CPPAD_PACKAGE_STRING "CppAD 20110118"
 
 /*
 $begin configure$$
