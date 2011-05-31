3a283915f02678a5250571f7f4fc635be9024b18
bradbell <bradbell@0e562018-b8fb-0310-a933-ecf073c7c197>
2011-05-31 16:26:29 +0000

    multi_newton.cpp: 1. change to modern omhelp comands (using modern_omhelp.sh). 2. Add restriction n_sum > 0. 3. Put all local functions in empty namespace (and indent them). 4. Use local routine arg2size_t to convert command line arguments to size_t. 5. Change n_thread to be size_t (like other local variables). 6. Check no omp_alloc memeory in use or avaiable at start. 7. Print omp_alloc memory at end. 8. Move correctness test to be after speed test.
    git-svn-id: https://projects.coin-or.org/svn/CppAD/trunk@1958 0e562018-b8fb-0310-a933-ecf073c7c197

--- openmp/multi_newton.cpp
+++ openmp/multi_newton.cpp
@@ -24,7 +24,7 @@ $index program, OpenMP example$$
 $section Multi-Threaded Newton's Method Main Program$$
 
 $head Syntax$$
-$syntax%multi_newton %n_thread% %repeat% %n_zero% %n_grid% %n_sum% %use_ad%$$ 
+$codei%multi_newton %n_thread% %repeat% %n_zero% %n_grid% %n_sum% %use_ad%$$ 
 
 $head Purpose$$
 Runs a timing test of the $cref/multi_newton/$$ routine.
@@ -34,47 +34,48 @@ CppAD is used to calculate the derivatives required by Newton's method.
 OpenMP is used to parallelize the calculation on the different sub-intervals.
 
 $head n_thread$$
-If the argument $italic n_thread$$ is equal to $code automatic$$, 
+If the argument $icode n_thread$$ is equal to $code automatic$$, 
 dynamic thread adjustment is used.
-Otherwise, $italic n_thread$$ must be a positive number
+Otherwise, $icode n_thread$$ must be a positive number
 specifying the number of OpenMP threads to use.
 
 $head repeat$$
-If the argument $italic repeat$$ is equal to $code automatic$$,
+If the argument $icode repeat$$ is equal to $code automatic$$,
 the number of times to repeat the calculation of the number of zeros
 in total interval is automatically determined.
 In this case, the rate of execution of the total solution is reported.
 $pre
 
 $$
-If the argument $italic repeat$$ is not equal to $italic automatic$$,
+If the argument $icode repeat$$ is not equal to $italic automatic$$,
 it must be a positive integer.
-In this case $italic repeat$$ determination of the number of times 
+In this case $icode repeat$$ determination of the number of times 
 the calculation of the zeros in the total interval is repeated.
 The rate of execution is not reported (it is assumed that the
 program execution time is being calculated some other way).
 
 $head n_zero$$
-The argument $italic n_zero$$ is the actual number of zeros
+The argument $icode n_zero$$ is the actual number of zeros
 that there should be in the test function, $latex \sin(x)$$.
 It must be an integer greater than one.
 The total interval searched  for zeros is 
 $latex [ 0 , (n\_zero - 1) \pi ]$$.
 
 $head n_grid$$
-The argument $italic n_grid$$
+The argument $icode n_grid$$
 specifies the number of sub-intervals to divide the total interval into.
 It must an integer greater than zero
-(should probably be greater than two times $italic n_zero$$).
+(should probably be greater than two times $icode n_zero$$).
 
 $head n_sum$$
 The actual function that is used is 
 $latex \[
 	f(x) = \frac{1}{n\_sum} \sum_{i=1}^{n\_sum} \sin (x)
 \] $$
-where $italic n_sum$$ is a positive integer.
-The larger the value of $italic n_sum$$,
+where $icode n_sum$$ is a positive integer.
+The larger the value of $icode n_sum$$,
 the more computation is required to calculate the function.
+This must be an integer greater than zero.
 
 $head use_ad$$
 If $icode use_ad$$ is $code true$$,
@@ -111,84 +112,110 @@ $end
 
 
 namespace { // empty namespace
+
+	// set by main program and used by functions in this file
 	size_t n_sum;  // larger values make f(x) take longer to calculate
 	size_t n_zero; // number of zeros of f(x) in the total interval
 	bool   use_ad; // compute derivatives using AD
-}
 
-// A slow version of the sine function 
-template <class Float>
-Float f_eval(Float x)
-{	Float sum = 0.;
-	size_t i;
-	for(i = 0; i < n_sum; i++)
-		sum += sin(x);
+	// A slow version of the sine function 
+	template <class Float>
+	Float f_eval(Float x)
+	{	Float sum = 0.;
+		size_t i;
+		for(i = 0; i < n_sum; i++)
+			sum += sin(x);
 
-	return sum / Float(n_sum);
-}
-// Direct calculation of derivative
-double df_direct(double x)
-{	double sum = 0.;
-	size_t i;
-	for(i = 0; i < n_sum; i++)
-		sum += cos(x);
-
-	return sum / double(n_sum);
-}
-// AD calculation of detivative
-void fun_ad(double x, double& f, double& df)
-{	using CppAD::vector;
-	using CppAD::AD;	
-	vector< AD<double> > X(1), Y(1);
-	X[0] = x;
-	CppAD::Independent(X);
-	Y[0] = f_eval(X[0]);
-	CppAD::ADFun<double> F(X, Y);
-	vector<double> dx(1), dy(1);
-	dx[0] = 1.;
-	dy    = F.Forward(1, dx);
-	f     = Value( Y[0] );
-	df    = dy[0];
-	return;
-} 
-
-void fun(double x, double& f, double& df) 
-{	if( use_ad )
-		fun_ad(x, f, df);
-	else
-	{	f  = f_eval(x);
-		df = df_direct(x);
+		return sum / Float(n_sum);
 	}
-	return;
-}
+
+	// Direct calculation of derivative
+	double df_direct(double x)
+	{	double sum = 0.;
+		size_t i;
+		for(i = 0; i < n_sum; i++)
+			sum += cos(x);
+
+		return sum / double(n_sum);
+	}
+
+	// AD calculation of detivative
+	void fun_ad(double x, double& f, double& df)
+	{	using CppAD::vector;
+		using CppAD::AD;	
+		vector< AD<double> > X(1), Y(1);
+		X[0] = x;
+		CppAD::Independent(X);
+		Y[0] = f_eval(X[0]);
+		CppAD::ADFun<double> F(X, Y);
+		vector<double> dx(1), dy(1);
+		dx[0] = 1.;
+		dy    = F.Forward(1, dx);
+		f     = Value( Y[0] );
+		df    = dy[0];
+		return;
+	} 
+
+	// evaulate the function and its derivative
+	void fun(double x, double& f, double& df) 
+	{	if( use_ad )
+			fun_ad(x, f, df);
+		else
+		{	f  = f_eval(x);
+			df = df_direct(x);
+		}
+		return;
+	}
+
+
+	// run the multi-newton test once
+	void test_once(CppAD::vector<double> &xout, size_t n_grid)
+	{	assert( n_zero > 1 );
+		double pi      = 4. * std::atan(1.); 
+		double xlow    = 0.;
+		double xup     = (n_zero - 1) * pi;
+		double epsilon = 1e-6;
+		size_t max_itr = 20;
 	
+		multi_newton(
+			xout    ,
+			fun     ,
+			n_grid  ,
+			xlow    ,
+			xup     ,
+			epsilon ,
+			max_itr
+		);
+		return;
+	}
 
-void test_once(CppAD::vector<double> &xout, size_t n_grid)
-{	assert( n_zero > 1 );
-	double pi      = 4. * std::atan(1.); 
-	double xlow    = 0.;
-	double xup     = (n_zero - 1) * pi;
-	double epsilon = 1e-6;
-	size_t max_itr = 20;
+	// repeat the multi-newton test a specified number of times
+	void test_repeat(size_t size, size_t repeat)
+	{	size_t i;
+		CppAD::vector<double> xout;
+		for(i = 0; i < repeat; i++)
+			test_once(xout, size);
+		return;
+	}
 
-	multi_newton(
-		xout    ,
-		fun     ,
-		n_grid  ,
-		xlow    ,
-		xup     ,
-		epsilon ,
-		max_itr
-	);
-	return;
-}
+	size_t arg2size_t(
+		const char* arg       , 
+		bool automatic_ok     ,
+		int limit             , 
+		const char* error_msg )
+	{	if( std::strcmp(arg, "automatic") == 0 )
+		{	if( automatic_ok )
+				return 0;
+			std::cout << error_msg << std::endl;
+			exit(1);
+		}
+		int i = std::atoi(arg);
+		if( i >= limit )
+			return size_t(i);
+		std::cout << error_msg << std::endl;
+		exit(1);
+	}
 
-void test_repeat(size_t size, size_t repeat)
-{	size_t i;
-	CppAD::vector<double> xout;
-	for(i = 0; i < repeat; i++)
-		test_once(xout, size);
-	return;
 }
 
 int main(int argc, char *argv[])
@@ -198,7 +225,7 @@ int main(int argc, char *argv[])
 	using CppAD::vector;
 
 	const char *usage = 
-		"multi_newton n_thread repeat n_zero n_grid n_sum use_ad";
+	"usage: multi_newton n_thread repeat n_zero n_grid n_sum use_ad";
 	if( argc != 7 )
 	{	std::cerr << usage << endl;
 		exit(1);
@@ -206,45 +233,34 @@ int main(int argc, char *argv[])
 	argv++;
 
 	// n_thread command line argument
-	int n_thread;
-	if( std::strcmp(*argv, "automatic") == 0 )
-		n_thread = 0;
-	else
-	{	n_thread = std::atoi(*argv);
-		if( n_thread == 0 )
-		{	cout << "multi_newton: n_thread is equal to zero" << endl;
-			cout << "perhaps you want n_thread equal to automatic" << endl;
-			exit(1);
-		}
-	}
-	argv++;
+	size_t n_thread = arg2size_t( *argv++, true, 1, 
+		"multi_newton: n_thread is less than or equal zero\n"
+		"perhaps you want n_thread equal automatic"
+	);
 
 	// repeat command line argument
-	size_t repeat;
-	if( std::strcmp(*argv, "automatic") == 0 )
-		repeat = 0;
-	else
-	{	assert( std::atoi(*argv) > 0 );
-		repeat = std::atoi(*argv);
-		if( repeat == 0 )
-		{	cout << "multi_newton: repeat is equal to zero" << endl;
-			cout << "perhaps you want repeat equal to automatic" << endl;
-			exit(1);
-		}
-	}
-	argv++;
+	size_t repeat = arg2size_t( *argv++, true, 1,
+		"multi_newton: repeat is less than or equal zero\n"
+		"perhaps you want repeat equal to automatic"
+	);
 
 	// n_zero command line argument 
-	assert( std::atoi(*argv) > 1 );
-	n_zero = std::atoi(*argv++);
+	n_zero = arg2size_t( *argv++, false, 2,
+		"multi_newton: n_zero is less than or equal one\n"
+		"or it is equal to automatic"
+	);
 
 	// n_grid command line argument
-	assert( std::atoi(*argv) > 0 );
-	size_t n_grid = std::atoi(*argv++);
+	size_t n_grid = arg2size_t( *argv++, false, 1,
+		"multi_newton: n_grid is less than or equal zero\n"
+		"or it is equal to automatic"
+	);
        
 	// n_sum command line argument 
-	assert( std::atoi(*argv) > 0 );
-	n_sum = std::atoi(*argv++);
+	n_sum = arg2size_t( *argv++, false, 1,
+		"multi_netwon: n_sum is less than or equal zero\n"
+		"or it si equal to automatic"
+	);
 
 	// use_ad
 	if( std::strcmp(*argv, "true") == 0 )
@@ -255,21 +271,19 @@ int main(int argc, char *argv[])
 		std::cerr << "multi_newton: use_ad is not true or false" << endl;
 	argv++;
 
-	// minimum time for test (repeat until this much time)
-	double time_min = 1.;
 
 # ifdef _OPENMP
 	if( n_thread > 0 )
-	{	omp_set_dynamic(0);            // off dynamic thread adjust
-		omp_set_num_threads(n_thread); // set the number of threads 
+	{	omp_set_dynamic(0);                 // off dynamic thread adjust
+		omp_set_num_threads(int(n_thread)); // set the number of threads 
 	}
 	// now determine the maximum number of threads
-	n_thread = omp_get_max_threads();
+	n_thread = size_t( omp_get_max_threads() );
 	assert( n_thread > 0 );
-	
+
 	// No tapes are currently active,
 	// so we can inform CppAD of the maximum number of threads
-	CppAD::AD<double>::omp_max_thread(size_t(n_thread));
+	CppAD::AD<double>::omp_max_thread(n_thread);
 
 	// inform the user of the maximum number of threads
 	cout << "OpenMP="     << _OPENMP;
@@ -283,11 +297,47 @@ int main(int argc, char *argv[])
 	cout << ", n_sum="    << n_sum;
 	cout << ", use_ad="   << use_ad;
 	cout << endl;
-	// initialize flag
-	bool ok = true;
 
-	// sub-block so xout gets deallocated before call to CPPAD_TRACK_COUNT
-	{	// Correctness check
+	// check that no memory is in use or avialable at start
+	bool ok = true;
+	size_t thread;
+	for(thread = 0; thread < n_thread; thread++)
+	{	ok &= CppAD::omp_alloc::inuse(thread) == 0; 
+		ok &= CppAD::omp_alloc::available(thread) == 0; 
+	}
+
+	if( repeat > 0 )
+	{	// run the calculation the requested number of times
+		test_repeat(n_grid, repeat);
+	}
+	else
+	{	// determine number of times to the calculation runs per second
+
+		// size of the one test case
+		vector<size_t> size_vec(1);
+		size_vec[0] = n_grid;
+
+		// minimum time for test (repeat until this much time)
+		double time_min = 1.;
+
+		// run the test case
+		vector<size_t> rate_vec =
+		CppAD::speed_test(test_repeat, size_vec, time_min);
+
+		// report results
+		cout << "repeats per sec  = " << rate_vec[0] << endl;
+	}
+	// print amount of memory each thread used
+	cout << "omp_alloc::available = [ ";
+	for(thread = 0; thread < n_thread; thread++)
+	{	cout << CppAD::omp_alloc::available(thread);
+		if( thread + 1 < n_thread )
+			cout << ", ";
+	}
+	cout << " ]" << endl;
+
+	// sub-block so xout is not in use when memory_leak is called.
+	{	// Correctness test
 		vector<double> xout;
 		test_once(xout, n_grid);
 		double epsilon = 1e-6;
@@ -299,24 +349,7 @@ int main(int argc, char *argv[])
 			++i;
 		}
 	}
-	if( repeat > 0 )
-	{	// run the calculation the requested number of time
-		test_repeat(n_grid, repeat);
-	}
-	else
-	{	// actually time the calculation	 
 
-		// size of the one test case
-		vector<size_t> size_vec(1);
-		size_vec[0] = n_grid;
-
-		// run the test case
-		vector<size_t> rate_vec =
-		CppAD::speed_test(test_repeat, size_vec, time_min);
-
-		// report results
-		cout << "repeats per sec  = " << rate_vec[0] << endl;
-	}
 	// check all the threads for a CppAD memory leak
 	if( CppAD::memory_leak() )
 	{	ok = false;
