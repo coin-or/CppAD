c731e526a11268ecc17ffec34160a60a942b05fb
bradbell <bradbell@0e562018-b8fb-0310-a933-ecf073c7c197>
2012-01-16 14:57:36 +0000

    1. Fix problem where multi_thread/test.sh required the openmp flags to be specfied on the configure command line.
    2. In */makefile.in, update copyright data from corresponding makefile.am.
    
    git-svn-id: https://projects.coin-or.org/svn/CppAD/trunk@2267 0e562018-b8fb-0310-a933-ecf073c7c197

--- multi_thread/test.sh
+++ multi_thread/test.sh
@@ -11,12 +11,50 @@
 # Please visit http://www.coin-or.org/CppAD/ for information on other licenses.
 # -----------------------------------------------------------------------------
 # script used by */makefile.am to run a default case for all the the tests
-# 
-# cases that have failed in the past
-./openmp_test multi_newton 1 1 100 700 1 true
-./openmp_test multi_newton 1 2 3 12 1 true
-#
-# standard test cases
+# --------------------------------------------------------------------------
+# setup
+next_program() {
+	(( i_program = i_program + 1 ))
+	if [ $i_program -ge $n_program ]
+	then
+		i_program='0'
+	fi
+	program=${program_list[$i_program]}
+}
+n_program='0'
+for program in openmp_test pthread_test bthread_test
+do
+	if [ -e $program ]
+	then
+		program_list[$n_program]=$program
+		(( n_program = n_program + 1 ))
+	fi
+done
+if [ "$n_program" == '0' ]
+then
+	echo "multi_thread/test.sh: nothing to test"
+	exit 0
+fi
+i_program='0'
+next_program
+# --------------------------------------------------------------------------
+# test_time=1 max_thread=4, mega_sum=1
+./$program harmonic 1 4 1
+next_program
+echo
+# test_time= 2 max_thread=4, num_zero=20, num_sub=30, num_sum=500, use_ad=true
+./$program multi_newton 2 4 20 30 500 true
+next_program
+echo
+# case that failed in the past
+./$program multi_newton 1 1 100 700 1 true
+next_program
+echo
+# case that failed in the past
+./$program multi_newton 1 2 3 12 1 true
+next_program
+echo
+# fast cases, do all programs
 for program in openmp_test pthread_test bthread_test
 do
 	if [ -e "$program" ]
@@ -25,11 +63,5 @@ do
 		echo
 		./$program simple_ad
 		echo
-		# test_time=1 max_thread=4, mega_sum=1
-		./$program harmonic 1 4 1
-		echo
-		# test_time= 2 max_thread=4, 
-		# num_zero=20, num_sub=30, num_sum=500, use_ad=true
-		./$program multi_newton 2 4 20 30 500 true
 	fi
 done
