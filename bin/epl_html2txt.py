e345a5f13c3c8194ae01b68cb45ae53e32ae4f23
bradbell <bradbell@0e562018-b8fb-0310-a933-ecf073c7c197>
2012-10-24 16:00:32 +0000

    Add new license files to repository in preparation for changing licenses.
    epl_html2txt.py: script that created epl-v10.txt from epl-v10.html.
    epl-v10.html: original epl license.
    epl-v10.txt: replacement for cpl1.0.txt
    gpl-3.0.txt: replacement for gpl2.txt
    
    git-svn-id: https://projects.coin-or.org/svn/CppAD/trunk@2502 0e562018-b8fb-0310-a933-ecf073c7c197

---
+++ bin/epl_html2txt.py
@@ -0,0 +1,54 @@
+#! /usr/bin/python
+#
+import re   # see http://docs.python.org/library/re.html
+# --------------------------------------------------------------------------
+def remove_simple_cmd(cmd_start, cmd_end, text_in) :
+	pattern = '(.*?)' + cmd_start + '(.*?)' + cmd_end
+	pattern = re.compile(pattern, re.DOTALL)
+	text_out = ''
+	start    = 0
+	while start < len(text_in):
+		match  = pattern.search(text_in, start) 
+		if match == None :
+			text_out += text_in[start:-1]
+			start     = len(text_in)
+		else :
+			text_out += match.group(1) + match.group(2)
+			start     = match.end(0)
+	return text_out
+# --------------------------------------------------------------------------
+def remove_cmd_indent_text(cmd_start, cmd_end, text_in) :
+	pattern = '(.*?)' + cmd_start + '(.*?)' + cmd_end
+	pattern = re.compile(pattern, re.DOTALL)
+	text_out = ''
+	start    = 0
+	while start < len(text_in):
+		match  = pattern.search(text_in, start) 
+		if match == None :
+			text_out += text_in[start:-1]
+			start     = len(text_in)
+		else :
+			text_out += match.group(1) + '\t'
+			text_out += re.sub('\n', '\n\t', match.group(2) )
+			start     = match.end(0)
+	return text_out
+# --------------------------------------------------------------------------
+#
+file_in  = open('epl-v10.html', 'rb')
+data_in  = file_in.read()
+# --------------------------------------------------------------------------
+# Extract body
+pattern  = '<body lang="EN-US">(.*)</body>'
+match    = re.search(pattern, data_in, re.DOTALL)
+text     = match.group(1)
+# --------------------------------------------------------------------------
+text = remove_simple_cmd('<b>', '</b>', text)
+text = remove_simple_cmd('<p>', '</p>', text)
+text = remove_simple_cmd('<h2>', '</h2>', text)
+text = remove_cmd_indent_text('<p class="list">', '</p>', text)
+# --------------------------------------------------------------------------
+data_out = text
+file_out = open('epl-v10.txt',  'wb')
+file_out.write(data_out)
+file_out.close()
+
