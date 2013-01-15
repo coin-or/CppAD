533a4e17a1a934ff1998fb830706a503555595c9
bradbell <bradbell@0e562018-b8fb-0310-a933-ecf073c7c197>
2013-01-15 14:15:31 +0000

    cmake_target.sh: Demonstrate problem on Red-hat 6.
    git-svn-id: https://projects.coin-or.org/svn/CppAD/trunk@2746 0e562018-b8fb-0310-a933-ecf073c7c197

---
+++ bug/cmake_target.sh
@@ -0,0 +1,37 @@
+#! /bin/bash -e
+if [ -e build ]
+then
+	rm -r build
+fi
+mkdir build
+cd    build
+cat << EOF > hello_one.cpp
+# include <iostream>
+int main(void)
+{	std::cout << "hello_one" << std::endl << std::endl;
+	return 0;
+}
+EOF
+cat << EOF > hello_two.cpp
+# include <iostream>
+int main(void)
+{	std::cout << "hello_two" << std::endl << std::endl;
+	return 0;
+}
+EOF
+cat << EOF > CMakeLists.txt
+CMAKE_MINIMUM_REQUIRED(VERSION 2.6)
+#
+PROJECT(hello)
+#
+ADD_EXECUTABLE(hello_one EXCLUDE_FROM_ALL hello_one.cpp )
+ADD_EXECUTABLE(hello_two EXCLUDE_FROM_ALL hello_two.cpp )
+#
+ADD_CUSTOM_TARGET(check_one hello_one DEPENDS hello_one)
+ADD_CUSTOM_TARGET(check_two hello_two DEPENDS hello_two)
+#
+ADD_CUSTOM_TARGET(check DEPENDS check_one check_two)
+EOF
+#
+cmake .
+make check
