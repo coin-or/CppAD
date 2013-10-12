fd68d16dfd3ffab23cc8b10a4891e0faf17fe620
bradbell <bradbell@0e562018-b8fb-0310-a933-ecf073c7c197>
2013-10-12 15:08:34 +0000

    std_vector.sh: deomonstrate unexpected behavior by standard vector.
    git-svn-id: https://projects.coin-or.org/svn/CppAD/trunk@2932 0e562018-b8fb-0310-a933-ecf073c7c197

---
+++ bug/std_vector.sh
@@ -0,0 +1,36 @@
+#! /bin/bash -e
+# -----------------------------------------------------------------------------
+# Using g++ 4.8.1 results in the following error message:
+#
+# std_vector.cpp:8:7: error: no match for ‘operator|=’ (operand types are 
+# ‘std::vector<bool>::reference {aka std::_Bit_reference}’ and ‘bool’)
+#  y[1] |= true;
+#       ^
+# -----------------------------------------------------------------------------
+if [ ! -e build ]
+then
+	mkdir build
+fi
+cd build
+echo "$0"
+name=`echo $0 | sed -e 's|.*/||' -e 's|\..*||'`
+cat << EOF > $name.cpp
+# include <vector>
+int main(void)
+{	int N = 1;
+	std::vector<bool> y(N);
+	for(int i = 0; i < N; i++ )
+		y[i] = false;
+	y[0]  = y[0] | true;
+	y[1] |= true;
+	return 0;
+}
+EOF
+echo "g++ -g $name.cpp -o $name"
+g++ -g $name.cpp -o $name
+#
+echo "./$name"
+./$name
+#
+echo "rm $name $name.cpp"
+rm $name $name.cpp
