684ea6a377b22c2e4c290b1fa1e4463eeab9c8b1
bradbell <bradbell@0e562018-b8fb-0310-a933-ecf073c7c197>
2013-11-12 13:54:14 +0000

    atomic_atomic.sh: New bug.
    git-svn-id: https://projects.coin-or.org/svn/CppAD/trunk@3005 0e562018-b8fb-0310-a933-ecf073c7c197

---
+++ bug/atomic_atomic.sh
@@ -0,0 +1,81 @@
+#! /bin/bash -e
+# -----------------------------------------------------------------------------
+# Bug when result of an atomic fucntion is used as argument to another
+# atomic function (and the tape is optimized)
+# -----------------------------------------------------------------------------
+if [ ! -e build ]
+then
+	mkdir build
+fi
+cd build
+echo "$0"
+name=`echo $0 | sed -e 's|.*/||' -e 's|\..*||'`
+cat << EOF > $name.cpp
+# include <cppad/cppad.hpp>
+
+namespace {
+	using CppAD::vector;
+	using CppAD::AD;
+
+	class atomic_user : public CppAD::atomic_base<double> {
+	public:
+		atomic_user(const std::string& name) :
+			CppAD::atomic_base<double>(name) {}
+	private:
+		virtual bool forward(
+			size_t                     q , // lowest order Taylor coeff
+			size_t                     p , // highest order Taylor coeff
+			const vector<bool>&       vx , // which x elements are variables
+			vector<bool>&             vy , // which y elements are variables
+			const vector<double>&     tx , // input vector of x
+			vector<double>&           ty ) // output vector of y
+		{
+			if( vx.size() > 0 )
+				vy[0] = vx[0];
+	
+			if ((q <= 0) && (p == 0))
+			{	ty[0] = tx[0];      
+				return true;
+			}
+			return false;
+		}
+	}; // end class
+}
+int main()  {
+	atomic_user func("atomic_user");
+	size_t n = 1;
+	vector< AD<double> > ax(n), ay(1), au(1), av(1);
+	vector<double> x(n), y_before(1), y_after(1);
+
+	ax[0] = x[0] = 3.0;
+	CppAD::Independent(ax);
+	//
+	au[0] = ax[0];
+	func(au, av);
+	//
+	au[0] = av[0];
+	func(au, av);
+	//
+	ay[0] = av[0];
+
+	CppAD::ADFun<double> f;
+	f.Dependent(ax,ay);
+
+	y_before = f.Forward(0,x);
+	f.optimize();
+	y_after = f.Forward(0,x);
+
+	if( fabs( y_before[0] - y_after[0] ) <= 1e-5 )
+		std::cout << "opt_atomic: OK" << std::endl;
+	else	std::cout << "opt_atomic: Error" << std::endl;
+	return(0);
+}
+EOF
+echo "g++ -I../.. --std=c++11 -g $name.cpp -o $name"
+g++ -I../.. --std=c++11 -g $name.cpp -o $name
+#
+echo "./$name"
+./$name
+#
+echo "rm $name $name.cpp"
+rm $name $name.cpp
