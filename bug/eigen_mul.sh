cd97de2077c86eaef235988a71409dd6de7f02b0
bradbell <bradbell@0e562018-b8fb-0310-a933-ecf073c7c197>
2013-12-10 12:46:20 +0000

    eigen_io.sh: bring up to date. eigen_mul.sh: new bug.
    git-svn-id: https://projects.coin-or.org/svn/CppAD/trunk@3020 0e562018-b8fb-0310-a933-ecf073c7c197

--- bug/eigen_mul.sh
+++ bug/eigen_mul.sh
@@ -10,6 +10,12 @@
 # A copy of this license is included in the COPYING file of this distribution.
 # Please visit http://www.coin-or.org/CppAD/ for information on other licenses.
 # -----------------------------------------------------------------------------
+# eigen_mul.cpp:15:12: warning: ISO C++ says that these are ambiguous, 
+# even though the worst conversion for the first is better than the worst 
+# conversion for the second: [enabled by default]
+#  A2 * B2 * A2;
+#            ^
+# ------------------------------------------------------------------------------
 if [ ! -e build ]
 then
 	mkdir build
@@ -19,18 +25,22 @@ echo "$0"
 name=`echo $0 | sed -e 's|.*/||' -e 's|\..*||'`
 #
 cat << EOF > $name.cpp
-# include <cppad/cppad.hpp>
-# include <cppad/example/cppad_eigen.hpp>
+#include <Eigen/Core>
+#include <cppad/cppad.hpp>
 
-int main()
-{	
-	typedef Eigen::Matrix< CppAD::AD<double>, 2, 2> MatrixAD;
-	MatrixAD X;
-	X << 1, 2, 3, 4;
-	std::cout << X << std::endl;
-	std::cout << "output above should be" << std::endl;
-	std::cout << "1 2" << std::endl;
-	std::cout << "3 4" << std::endl;
+int main() {
+	using Eigen::Matrix;
+	using Eigen::Dynamic;
+	typedef CppAD::AD<double> AScalar;
+
+	Matrix<AScalar, Dynamic, Dynamic> A2(1,1);
+	Matrix<AScalar, Dynamic, Dynamic> B2(1,1);
+
+	A2(0,0) = 1.0;
+	B2(0,0) = 2.0;
+
+	A2 * B2 * A2; 
+
 	return 0;
 }
 EOF
