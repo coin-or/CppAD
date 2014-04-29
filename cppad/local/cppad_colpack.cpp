a5abea8dd919670a7405fc0f54086e925e4eebf5
bradbell <bradbell@0e562018-b8fb-0310-a933-ecf073c7c197>
2014-04-29 13:12:19 +0000

    1. Better check of assumptions for colpack in jacobian case. 2. Skelton of routine for colpack in hessian case (not yet used).
    git-svn-id: https://projects.coin-or.org/svn/CppAD/trunk@3240 0e562018-b8fb-0310-a933-ecf073c7c197

--- cppad/local/cppad_colpack.cpp
+++ cppad/local/cppad_colpack.cpp
@@ -48,11 +48,90 @@ namespace CppAD {
 		for(k = 0; k < size_t(n_seed_row); k++)
 		{	for(i = 0; i < m; i++)
 			{	if( seed_matrix[k][i] != 0.0 ) 
-				{	CPPAD_ASSERT_UNKNOWN( color[i] == m );
+				{	// check that no row appears twice in the coloring
+					CPPAD_ASSERT_UNKNOWN( color[i] == m );
 					color[i] = k;
 				}
 			}
 		}
+# ifndef NDEBUG
+		// check that all non-zero rows appear in the coloring
+		for(i = 0; i < m; i++)
+			CPPAD_ASSERT_UNKNOWN(color[i] < m || adolc_pattern[i][0] == 0);
+
+		// check that no rows with the same color have overlapping entries
+		CppAD::vector<bool> found(n);
+		for(k = 0; k < size_t(n_seed_row); k++)
+		{	size_t j, ell;
+			for(j = 0; j < n; j++)
+				found[j] = false;
+			for(i = 0; i < m; i++) if( color[i] == k )
+			{	for(ell = 0; ell < adolc_pattern[i][0]; ell++)
+				{	j = adolc_pattern[i][1 + ell];
+					CPPAD_ASSERT_UNKNOWN( ! found[j] );
+					found[j] = true;
+				}
+			}
+		}
+# endif
+		return;
+	}
+	// The following routine is not yet used or tested.
+	void cppad_colpack_symmetric(
+		      CppAD::vector<size_t>&         color         ,
+		size_t                               n             ,
+		const CppAD::vector<unsigned int*>&  adolc_pattern )
+	{	size_t i, k;
+		CPPAD_ASSERT_UNKNOWN( adolc_pattern.size() == n );
+	
+		// Use adolc sparsity pattern to create corresponding bipartite graph
+		ColPack::GraphColoringInterface graph(
+				SRC_MEM_ADOLC,
+				adolc_pattern.data(),
+				n
+		);
+	
+		// Color the graph with the speciied ordering
+		// graph.Coloring("SMALLEST_LAST", "STAR") is slower in adolc testing
+		graph.Coloring("SMALLEST_LAST", "ACYCLIC_FOR_INDIRECT_RECOVERY");
+	
+		// Use coloring information to create seed matrix
+		int n_seed_row;
+		int n_seed_col;
+		double** seed_matrix = graph.GetSeedMatrix(&n_seed_row, &n_seed_col);
+		CPPAD_ASSERT_UNKNOWN( size_t(n_seed_col) == n );
+	
+		// now return coloring in format required by CppAD
+		for(i = 0; i < n; i++)
+			color[i] = n;
+		for(k = 0; k < size_t(n_seed_row); k++)
+		{	for(i = 0; i < n; i++)
+			{	if( seed_matrix[k][i] != 0.0 ) 
+				{	CPPAD_ASSERT_UNKNOWN( color[i] == n );
+					color[i] = k;
+				}
+			}
+		}
+# ifndef NDEBUG
+		for(i = 0; i < n; i++)
+			CPPAD_ASSERT_UNKNOWN(color[i] < n || adolc_pattern[i][0] == 0);
+
+		// The coloring above will probably fail this  test.
+		// Check that no rows with the same color have overlapping entries:
+		CppAD::vector<bool> found(n);
+		for(k = 0; k < size_t(n_seed_row); k++)
+		{	size_t j, ell;
+			for(j = 0; j < n; j++)
+				found[j] = false;
+			for(i = 0; i < n; i++) if( color[i] == k )
+			{	for(ell = 0; ell < adolc_pattern[i][0]; ell++)
+				{	j = adolc_pattern[i][1 + ell];
+					CPPAD_ASSERT_UNKNOWN( ! found[j] );
+					found[j] = true;
+				}
+			}
+		}
+# endif
 		return;
 	}
 }
--- cppad/local/cppad_colpack.hpp
+++ cppad/local/cppad_colpack.hpp
@@ -20,7 +20,7 @@ External interface to Colpack routines used by cppad.
 */
 // ---------------------------------------------------------------------------
 /*!
-Link from CppAD to ColPack.
+Link from CppAD to ColPack used for general sparse matrices.
 
 This CppAD library routine is necessary because 
 <code>ColPack/ColPackHeaders.h</code> has a
@@ -45,24 +45,54 @@ The input value of its elements does not matter.
 Upon return, it is a coloring for the rows of the sparse matrix.
 \n
 \n
-If for come \c i, <code>color[i] == m</code>, then 
-the i-th row does not appear in the vector \c row.
+If for some \c i, <code>color[i] == m</code>, then 
+<code>adolc_pattern[i][0] == 0</code>.
 Otherwise, <code>color[i] < m</code>.
 \n
 \n
-Suppose two differen rows, <code>i != r</code> have the same color and
-column index \c j is such that both of the pairs 
+Suppose two differen rows, <code>i != r</code> have the same color.
+It follows that for all column indices \c j;
+it is not the case that both
 <code>(i, j)</code> and <code>(r, j)</code> appear in the sparsity pattern.
-It follows that neither of these pairs appear in the set of
-<code>(row[k], col[k])</code> entries.
 \n
 \n
 This routine tries to minimize, with respect to the choice of colors,
-the maximum, with respct to \c k, of <code>color[ row[k] ]</code>.
+the number of colors.
 */
 extern void cppad_colpack_general(
 	      CppAD::vector<size_t>&         color         ,
 	size_t                               m             ,
+	size_t                               n             ,
+	const CppAD::vector<unsigned int*>&  adolc_pattern
+);
+
+/*!
+Link from CppAD to ColPack used for symmetric sparse matrices
+(not yet used or tested).
+
+This CppAD library routine is necessary because 
+<code>ColPack/ColPackHeaders.h</code> has a
+<code>using namespace std</code> at the global level.
+
+\param n [in]
+is the nubmer of rows and columns in the symmetric sparse matrix.
+
+\param adolc_pattern [in]
+This vector has size \c n,
+<code>adolc_pattern[i][0]</code> is the number of non-zeros in row \c i.
+For <code>j = 1 , ... , adolc_sparsity[i]<code>,
+<code>adolc_pattern[i][j]</code> is the column index (base zero) for the 
+non-zeros in row \c i.
+
+\param color [out]
+The input value of its elements does not matter.
+Upon return, it is a coloring for the rows of the sparse matrix.
+The properties of this coloring have not yet been determined; see
+Efficient Computation of Sparse Hessians Using Coloring
+and Automatic Differentiation (pdf/ad/gebemedhin14.pdf)
+*/
+extern void cppad_colpack_symmetric(
+	      CppAD::vector<size_t>&         color         ,
 	size_t                               n             ,
 	const CppAD::vector<unsigned int*>&  adolc_pattern
 );
