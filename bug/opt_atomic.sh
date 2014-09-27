18c455787a2894e899d85fb88c858f6db8d6ddf8
bradbell <bradbell@0e562018-b8fb-0310-a933-ecf073c7c197>
2014-09-27 09:35:28 +0000

    opt_atomic.sh: Simplify code for bug demonstration.
    git-svn-id: https://projects.coin-or.org/svn/CppAD/trunk@3365 0e562018-b8fb-0310-a933-ecf073c7c197

--- bug/opt_atomic.sh
+++ bug/opt_atomic.sh
@@ -15,8 +15,88 @@ Assert during optimization of an atomic function.
 EOF
 cat << EOF > bug.$$
 # include <cppad/cppad.hpp>
-# include <example/atomic/reciprocal.cpp>  // Get an atomic test function
-using namespace CppAD;
+
+using CppAD::vector;
+using CppAD::AD;
+using CppAD::ADFun;
+
+class atomic_reciprocal : public CppAD::atomic_base<double> {
+public:
+	atomic_reciprocal(const std::string& name) :
+	CppAD::atomic_base<double>(name)
+	{ }
+private:
+	virtual bool forward(
+		size_t                    p ,
+		size_t                    q ,
+		const vector<bool>&      vx ,
+		      vector<bool>&      vy ,
+		const vector<double>&    tx ,
+		      vector<double>&    ty
+	)
+	{	size_t n = tx.size() / (q + 1);
+		size_t m = ty.size() / (q + 1);
+		assert( n == 1 );
+		assert( m == 1 );
+		assert( p <= q );
+
+		// return flag
+		bool ok = true;
+
+		// check for defining variable information
+		// This case must always be implemented
+		if( vx.size() > 0 )
+			vy[0] = vx[0];
+
+		// Order zero forward mode.
+		// This case must always be implemented
+		// y^0 = f( x^0 ) = 1 / x^0
+		double f = 1. / tx[0];
+		if( p <= 0 )
+			ty[0] = f;
+		if( q <= 0 )
+			return ok;
+		//
+		assert( vx.size() == 0 );
+		ok = false;
+		return ok;
+	}
+	virtual bool rev_sparse_jac(
+		size_t                                p  ,
+		const vector<bool>&                   rt ,
+		      vector<bool>&                   st )
+	{	// This function needed if using RevSparseJac or optimize
+		// with afun.option( CppAD::atomic_base<double>::bool_sparsity_enum )
+		size_t n = st.size() / p;
+		size_t m = rt.size() / p;
+		assert( n == 1 );
+		assert( m == 1 );
+
+		// sparsity for S(x)^T = f'(x)^T * R^T is same as sparsity for R^T
+		for(size_t i = 0; i < p; i++)
+			st[i] = rt[i];
+
+		return true; 
+	}
+	// reverse Jacobian set sparsity routine called by CppAD
+	virtual bool rev_sparse_jac(
+		size_t                                p  ,
+		const vector< std::set<size_t> >&     rt ,
+		      vector< std::set<size_t> >&     st )
+	{	// This function needed if using RevSparseJac or optimize
+		// with afun.option( CppAD::atomic_base<double>::set_sparsity_enum )
+		size_t n = st.size();
+		size_t m = rt.size();
+		assert( n == 1 );
+		assert( m == 1 );
+
+		// sparsity for S(x)^T = f'(x)^T * R^T is same as sparsity for R^T
+		st[0] = rt[0];
+
+		return true; 
+	}
+};
+
 
 int main(){
   vector<AD<double> > x(2);
