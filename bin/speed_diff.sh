30cb30b7cffead69d276ab5e3ef5a5e9516cdb34
bradbell <bradbell@0e562018-b8fb-0310-a933-ecf073c7c197>
2014-12-12 18:35:25 +0000

    Change name and program so can compare any two speed output files.
    git-svn-id: https://projects.coin-or.org/svn/CppAD/trunk@3466 0e562018-b8fb-0310-a933-ecf073c7c197

---
+++ bin/speed_diff.sh
@@ -0,0 +1,49 @@
+#! /bin/bash -e
+# $Id$
+# -----------------------------------------------------------------------------
+# CppAD: C++ Algorithmic Differentiation: Copyright (C) 2003-14 Bradley M. Bell
+#
+# CppAD is distributed under multiple licenses. This distribution is under
+# the terms of the 
+#                     Eclipse Public License Version 1.0.
+#
+# A copy of this license is included in the COPYING file of this distribution.
+# Please visit http://www.coin-or.org/CppAD/ for information on other licenses.
+# -----------------------------------------------------------------------------
+if [ "$0" != 'bin/speed_diff.sh' ]
+then
+	echo 'bin/speed_diff.sh: must be executed from its parent directory'
+	exit 1
+fi
+if [ "$2" == '' ]
+then
+cat << EOF
+usage: bin/speed_diff.sh speed_one.out speed_two.out
+where speed_one.out and speed_two.out are outputs from the speed_cppad program
+runing its 'speed' test.
+EOF
+	exit 1
+fi
+speed_one="$1"
+speed_two="$2"
+if [ ! -f "$speed_one" ]
+then
+	echo "speed_diff.sh: the file $speed_one does not exist."
+	exit 1
+fi
+if [ ! -f "$speed_two" ]
+then
+	echo "speed_diff.sh: the file $speed_two does not exist."
+	exit 1
+fi
+#
+sed -n -e 's|_rate|_rate_one|' -e '/_rate_/p' \
+	-e 's|available|available_one|' -e '/available_one/p' \
+	$speed_one > speed_diff.$$
+#
+sed -n -e 's|_rate|_rate_two|' -e '/_rate_/p' \
+	-e 's|available|available_two|' -e '/available_two/p' \
+	$speed_two >> speed_diff.$$
+#
+cat speed_diff.$$ | sort -u
+rm speed_diff.$$
