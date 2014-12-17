001b349037ffcb0e5caa71a3ed9704af749bbfd5
bradbell <bradbell@0e562018-b8fb-0310-a933-ecf073c7c197>
2014-12-17 23:39:47 +0000

    */makefile.am: put speed_ in front of speed test names (same as cmake). */makefile.am: make sure main.o is different for each package. */makefile.in: changes automatically generated by changes to */makefile.am
    get_adolc.sh: get newer version.
    jenkins.sh: recompile external packages.
    makefile.am: add colpack to speed_adolc option list.
    makefile.in: make sure all objects are separate for profile case.
    makefile.am: teuchos library changed (split to sublibraries).
    whats_new_14.omh: printable version cannot link to _search.
    build.sh: supress another warning that occures when testing eigen::vector.
    
    git-svn-id: https://projects.coin-or.org/svn/CppAD/trunk@3481 0e562018-b8fb-0310-a933-ecf073c7c197

--- build.sh
+++ build.sh
@@ -219,7 +219,7 @@ then
 	cxx_flags="-Wall -ansi -pedantic-errors -std=c++98 -Wshadow"
 #_build_test_only:	if [ -e $EIGEN_DIR/include/Eigen ]
 #_build_test_only:	then
-#_build_test_only:	cxx_flags="-Wall -ansi -pedantic-errors -std=c++98 -Wno-long-long"
+#_build_test_only:	cxx_flags="-Wall -ansi -pedantic-errors -std=c++98 -Wno-long-long -Wno-sign-compare"
 #_build_test_only:	fi
 cat << EOF
 ../configure > $log_file \\
