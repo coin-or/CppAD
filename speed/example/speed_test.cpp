7da402f36162eb4fa55d2d85139958b1a2e42a79
bradbell <bradbell@0e562018-b8fb-0310-a933-ecf073c7c197>
2014-12-29 13:24:37 +0000

    Clang++ compiler was optimizing out the calculations in these speed tests.
    git-svn-id: https://projects.coin-or.org/svn/CppAD/trunk@3522 0e562018-b8fb-0310-a933-ecf073c7c197

--- speed/example/speed_test.cpp
+++ speed/example/speed_test.cpp
@@ -39,40 +39,38 @@ $end
 */
 // BEGIN C++
 # include <cppad/speed_test.hpp>
-# include <vector>
+# include <cppad/vector.hpp>
 
 namespace { // empty namespace
+	using CppAD::vector;
+	vector<double> a, b, c;
 	void test(size_t size, size_t repeat)
 	{	// setup
-		double *a = new double[size];
-		double *b = new double[size];
-		double *c = new double[size];
+		a.resize(size);
+		b.resize(size);
+		c.resize(size);
 		size_t i  = size;;
 		while(i)
 		{	--i;
 			a[i] = i;
 			b[i] = 2 * i;
+			c[i] = 0.0;
 		}
 		// operations we are timing
 		while(repeat--)
 		{	i = size;;
 			while(i)
 			{	--i;
-				c[i] = a[i] * b[i];
+				c[i] += std::sqrt(a[i] * a[i] + b[i] * b[i]);
 			}
 		}
-		// teardown
-		delete [] a;
-		delete [] b;
-		delete [] c;
-		return;
 	}
 }
 bool speed_test(void)
 {	bool ok = true;
 
 	// size of the test cases
-	std::vector<size_t> size_vec(2);
+	vector<size_t> size_vec(2);
 	size_vec[0] = 40;
 	size_vec[1] = 80;
 
@@ -80,7 +78,7 @@ bool speed_test(void)
 	double time_min = 0.5;
 
 	// run the test cases
-	std::vector<size_t> rate_vec(2);
+	vector<size_t> rate_vec(2);
 	rate_vec = CppAD::speed_test(test, size_vec, time_min);
 
 	// time per repeat loop (note counting setup or teardown)
@@ -94,6 +92,9 @@ bool speed_test(void)
 	if( ! ok )
 		std::cout << std::endl << "rel_diff = " << rel_diff << std::endl;
  
+	a.clear();
+	b.clear();
+	c.clear();
 	return ok;
 }
 // END C++
