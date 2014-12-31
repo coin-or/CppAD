9dea851744d3bdcfa8e120ee544b8e0c09fb6655
bradbell <bradbell@0e562018-b8fb-0310-a933-ecf073c7c197>
2014-12-31 03:47:30 +0000

    clean_tar.sh: clean directory and then create a tarball copy.
    git-svn-id: https://projects.coin-or.org/svn/CppAD/trunk@3534 0e562018-b8fb-0310-a933-ecf073c7c197

---
+++ bin/clean_tar.sh
@@ -0,0 +1,70 @@
+#! /bin/bash -e
+# $Id$
+# -----------------------------------------------------------------------------
+# CppAD: C++ Algorithmic Differentiation: Copyright (C) 2003-14 Bradley M. Bell
+#
+# CppAD is distributed under multiple licenses. This distribution is under
+# the terms of the 
+#                     Eclipse Public License Version 1.0.
+#
+# A copy of this license is included in the COPYING file of this distribution.
+# Please visit http://www.coin-or.org/CppAD/ for information on other licenses.
+# -----------------------------------------------------------------------------
+if [ $0 != "bin/clean.sh" ]
+then
+	echo "bin/clean.sh: must be executed from its parent directory"
+	exit 1
+fi
+# bash function that echos and executes a command
+echo_eval() {
+	echo $*
+	eval $*
+}
+# -----------------------------------------------------------------------------
+list=`find . -name '*.swp'`
+for file in $list
+do
+	response=''
+	read -p "Remove the editor temporary file $file [y/n]" response
+	if [ "$response" == 'y' ]
+	then
+		echo_eval rm $file
+	fi
+done
+for dir in build doc doxydoc
+do
+	echo_eval rm -rf $dir
+done
+list=`find . -name 'new'`
+for dir in $list
+do
+	echo_eval rm -r $dir
+done
+list=`find . \
+	\( -name '*.err' \) -or \
+	\( -name '*.log' \) -or \
+	\( -name '*.tmp' \) -or \
+	\( -name 'junk.*' \) -or \
+	\( -name 'temp.*' \) -or \
+	\( -name 'test_one.exe' \) -or \
+	\( -name 'test_one.sh' \) -or \
+	\( -name 'test_one.cpp' \) -or \
+	\( -name 'aclocal.m4' \) -or \
+	\( -name 'ar-lib' \) -or \
+	\( -name 'configure.hpp' \) -or \
+	\( -name 'git_commit.sh' \) -or \
+	\( -name 'junk' \) -or \
+	\( -name 'temp' \)`
+for file in $list
+do
+	if ! ( echo $file | grep '\.git\/' > /dev/null )
+	then
+		echo "rm $file"
+	fi
+done 
+version=`date +%Y%m%d`
+name=`pwd | sed -e 's|.*/||'`
+cd ..
+cp -r $name $name-$version
+echo_eval tar -czf $name-$version.tgz $name-$version
+rm -rf $name-$version
