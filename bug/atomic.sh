3d9f194dc898e2fd9e8099b811569bd157b10505
Brad Bell <bradbell@seanet.com>
2015-02-12 18:14:15 -0700

    atomic.sh: remove trailing white space.

--- bug/atomic.sh
+++ bug/atomic.sh
@@ -4,7 +4,7 @@
 # CppAD: C++ Algorithmic Differentiation: Copyright (C) 2003-15 Bradley M. Bell
 #
 # CppAD is distributed under multiple licenses. This distribution is under
-# the terms of the 
+# the terms of the
 #                     Eclipse Public License Version 1.0.
 #
 # A copy of this license is included in the COPYING file of this distribution.
@@ -73,20 +73,20 @@ double my_dot(const VectorXd& x, const VectorXd& y)
 	return dot;
 }
 
-inline void my_union( 
-	      std::set<size_t>&         result  , 
-	      const std::set<size_t>&   left    , 
-	      const std::set<size_t>&   right   ) 
-{  
-  std::set<size_t> temp; 
-  std::set_union( 
-		 left.begin()              , 
-		 left.end()                , 
-		 right.begin()             , 
-		 right.end()               , 
-		 std::inserter(temp, temp.begin()) 
-		  ); 
-  result.swap(temp); 
+inline void my_union(
+	      std::set<size_t>&         result  ,
+	      const std::set<size_t>&   left    ,
+	      const std::set<size_t>&   right   )
+{
+  std::set<size_t> temp;
+  std::set_union(
+		 left.begin()              ,
+		 left.end()                ,
+		 right.begin()             ,
+		 right.end()               ,
+		 std::inserter(temp, temp.begin())
+		  );
+  result.swap(temp);
 }
 
 
@@ -94,29 +94,15 @@ class multpow_cl {
 
   // Function:  f = a*b^c
   // eval returns the function, function+gradient, or function+gradient+hessian
-  
+
  public:
-  
+
   void eval(const VectorXd& x, double& f) {
     f = x[0]*pow(x[1],x[2]);
   }
 
   void eval(const VectorXd& x, double& f, VectorXd& df) {
-    
-    const double a = x[0];
-    const double b = x[1];
-    const double c = x[2];
-    
-    f = a*pow(b, c);
-    df[0] = pow(b, c);
-    df[1] = a*c*pow(b,c-1);
-    df[2] = a*pow(b, c)*log(b);
-  }
-  
-  
-  template<typename TH>
-  void eval(const VectorXd& x, double& f, VectorXd& df, MatrixBase<TH>& hess) {
-    
+
     const double a = x[0];
     const double b = x[1];
     const double c = x[2];
@@ -125,7 +111,21 @@ class multpow_cl {
     df[0] = pow(b, c);
     df[1] = a*c*pow(b,c-1);
     df[2] = a*pow(b, c)*log(b);
-    
+  }
+
+
+  template<typename TH>
+  void eval(const VectorXd& x, double& f, VectorXd& df, MatrixBase<TH>& hess) {
+
+    const double a = x[0];
+    const double b = x[1];
+    const double c = x[2];
+
+    f = a*pow(b, c);
+    df[0] = pow(b, c);
+    df[1] = a*c*pow(b,c-1);
+    df[2] = a*pow(b, c)*log(b);
+
     hess(0,0) = 0;
     hess(1,1) = a*c*(c-1)*pow(b, c-2);
     hess(2,2) = a*pow(log(b),2)*pow(b,c);
@@ -134,7 +134,7 @@ class multpow_cl {
     hess(0,2) = pow(b,c)*log(b);
     hess(2,0) = hess(0,2);
     hess(1,2) = a*(1+c*log(b))*pow(b,c-1);
-    hess(2,1) = hess(1,2); 
+    hess(2,1) = hess(1,2);
   }
 };
 
@@ -145,19 +145,19 @@ class mb_atomic : public CppAD::atomic_base<double> {
   // inherits from atomic_base
   // templated so I can just point to different functions, as above
   // this class should NEVER have to be modified for scalar-valued atomics.
-  
+
  public:
   std::shared_ptr<TF> func;
-      
+
  mb_atomic(const std::string& name) :
   CppAD::atomic_base<double>(name) {
     func = std::make_shared<TF>();
   }
 
   ~mb_atomic() {}
-      
- private:     
-      
+
+ private:
+
   // ----------------------------------------------------------------------
   // forward mode routine called by CppAD
   virtual bool forward(
@@ -169,46 +169,46 @@ class mb_atomic : public CppAD::atomic_base<double> {
 		       vector<double>&       ty   // output vector of y
 		       )
   {
-	
+
     size_t n = tx.size() / (p+1);
-	
+
     Map<VectorXd> y = VectorXd::Map(&(ty[0]),p+1);
-	
+
     bool ok = (p <= 2) && (q <= p);
-	
+
     if( vx.size() > 0 ) {
       vy[0] = vx[0];
       for (size_t j=1; j<n; j++) {
 	vy[0] = vy[0] || vx[j];
       }
     }
-    
+
     if ((q <= 0) && (p == 0)) {
       double f;
       VectorXd x_row_0 = my_column(tx.data(), n, q+1, 0);
       func->eval(x_row_0,f);
-      y[0] = f;      
+      y[0] = f;
     }
-	
+
     if ((q <= 1) && (p == 1)) {
       double f;
       VectorXd df(n);
       VectorXd x_row_0 = my_column(tx.data(), n, q+1, 0);
       func->eval(x_row_0,f,df);
-      y[0] = f;      
+      y[0] = f;
       VectorXd x_row_1 = my_column(tx.data(), n, q+1, 1);
-      y[1] = my_dot(df, x_row_1);    
+      y[1] = my_dot(df, x_row_1);
     }
-	
+
     if ((q <= 2) && (p == 2)) {
       double f;
       VectorXd df(n);
       MatrixXd hess(n,n);
       VectorXd x_row_0 = my_column(tx.data(), n, q+1, 0);
       func->eval(x_row_0, f, df, hess);
-      y[0] = f;      
+      y[0] = f;
       VectorXd x_row_1 = my_column(tx.data(), n, q+1, 1);
-      y[1] = my_dot(df, x_row_1); 	
+      y[1] = my_dot(df, x_row_1);
       y[2] = 0.0;
       for(size_t i = 0; i < n; i++)
       {   for(size_t j = 0; j < n; j++)
@@ -218,10 +218,10 @@ class mb_atomic : public CppAD::atomic_base<double> {
       VectorXd x_row_2 = my_column(tx.data(), n, q+1, 2);
       y[2] += my_dot(df, x_row_2);
     }
-	
+
     return ok;
   }
-      
+
   // ----------------------------------------------------------------------
   // reverse mode routine called by CppAD
   virtual bool reverse(
@@ -231,30 +231,30 @@ class mb_atomic : public CppAD::atomic_base<double> {
 		       vector<double>&        px ,
 		       const vector<double>&  py
 		       )
-  {		
+  {
     size_t n = tx.size() / (q+1);
-	
+
     double f;
-    VectorXd df(n);     
+    VectorXd df(n);
     VectorXd dy( n * (q+1) );
 
     bool ok = (q < 2);
-	
+
     VectorXd x_row_0 = my_column(tx.data(), n, q+1, 0);
     func->eval(x_row_0, f, df);
 	for(size_t j = 0; j < n; j++)
-       dy[ j*(q+1) + 0 ] = df[j]; 
-    
+       dy[ j*(q+1) + 0 ] = df[j];
+
     if (q >= 1) {
       MatrixXd hess(n,n);
       func->eval(x_row_0, f, df, hess);
       VectorXd x_row_1 = my_column(tx.data(), n, q+1, 1);
       VectorXd dy_col_1 = hess * x_row_1;
 	  for(size_t j = 0; j < n; j++)
-         dy[ j*(q+1) + 1 ] = dy_col_1[j]; 
-      
+         dy[ j*(q+1) + 1 ] = dy_col_1[j];
+
     }
-	
+
     for (size_t j=0; j<n; j++){
       for (size_t L=0; L <= q; L++) {
 	      px[j*(q+1)+L] = 0;
@@ -262,54 +262,54 @@ class mb_atomic : public CppAD::atomic_base<double> {
 	          px[j*(q+1)+L] += py[k]*dy[ j*(q+1) + k-L];
 	      }
       }
-    }	
+    }
     return ok;
   }
-      
-   
+
+
   // ----------------------------------------------------------------------
   // forward Jacobian sparsity routine called by CppAD
   virtual bool for_sparse_jac(
 			      size_t                             q ,
-			      const vector<std::set<size_t> >&   r , 
+			      const vector<std::set<size_t> >&   r ,
 			      vector<std::set<size_t> >&         s
 			      ) {
     size_t n = r.size() / q;
     s[0] = r[0];
     for (size_t i=0; i<n; i++) {
       my_union(s[0], s[0], r[i]);
-    }  
-    
-    return true; 
+    }
+
+    return true;
   }
-      
-  
+
+
   // ----------------------------------------------------------------------
   // reverse Jacobian sparsity routine called by CppAD
   virtual bool rev_sparse_jac(
 			      size_t  p ,
-			      const vector< std::set<size_t> >& rt , 
-			      vector< std::set<size_t> >& st 
+			      const vector< std::set<size_t> >& rt ,
+			      vector< std::set<size_t> >& st
 			      ) {
 
     size_t n = st.size();
     for (size_t i=0; i<n; i++) {
       st[i] = rt[0];
-    }	
-    return true; 
+    }
+    return true;
   }
 
   // ----------------------------------------------------------------------
   // reverse Hessian sparsity routine called by CppAD
   virtual bool rev_sparse_hes(
-			      const vector<bool>&             vx, 
-			      const vector<bool>&             s, 
-			      vector<bool>&                   t, 
-			      size_t                          p ,             
+			      const vector<bool>&             vx,
+			      const vector<bool>&             s,
+			      vector<bool>&                   t,
+			      size_t                          p ,
 			      const vector<std::set<size_t> >&  r ,
 			      const vector<std::set<size_t> >&  u ,
 			      vector<std::set<size_t> >&  v
-			      ) 
+			      )
   {
     size_t n = vx.size();
     size_t m = s.size();
@@ -323,15 +323,15 @@ class mb_atomic : public CppAD::atomic_base<double> {
     	  my_union(v[i], v[i], r[j] );
     	}
       }
-    }    
+    }
     return true;
-  }     
+  }
 }; // end class
 
 
-AScalar multpow_atomic(const AScalar& a, const AScalar& b, const AScalar& c) {      
+AScalar multpow_atomic(const AScalar& a, const AScalar& b, const AScalar& c) {
   static mb_atomic<multpow_cl> multpow_func("atomic_multpow");
-  VectorXA y(1);     
+  VectorXA y(1);
   VectorXA x(3);
   x[0] = a;
   x[1] = b;
@@ -340,13 +340,13 @@ AScalar multpow_atomic(const AScalar& a, const AScalar& b, const AScalar& c) {
   return(y[0]);
 }
 
-struct multpow_test_atomic {  
+struct multpow_test_atomic {
   AScalar eval(const VectorXA& Y) {
 
 
     // f = (a1*b1^c1 + a2*b2^c2)^2;
     // where ai = Y[3*i], bi = Y[3*i+1], ci = Y[3*i+2]
-    
+
     size_t n = Y.size()/3;
     AScalar res1 = 0.0;
     for (size_t i=0; i<n; i++) {
@@ -357,12 +357,12 @@ struct multpow_test_atomic {
 };
 
 
-struct multpow_test_cppad {  
+struct multpow_test_cppad {
   AScalar eval(const VectorXA& Y) {
 
     // f = (a1*b1^c1 + a2*b2^c2)^2;
     // where ai = Y[3*i], bi = Y[3*i+1], ci = Y[3*i+2]
-    
+
     size_t n = Y.size()/3;
     AScalar res1 = 0.0;
     for (size_t i=0; i<n; i++) {
@@ -396,7 +396,7 @@ int main() {
   multpow_test_atomic func1;
   multpow_test_cppad  func2;
 
-  // record tapes  
+  // record tapes
   CppAD::Independent(P);
   f1[0] = func1.eval(P);
   tape1.Dependent(P, f1);
@@ -412,23 +412,23 @@ int main() {
 
   tape1.Forward(0, X);
   VectorXd hess_sparse1 = tape1.SparseHessian(X, w);
-     
+
   tape2.Forward(0, X);
   VectorXd hess_sparse2 = tape2.SparseHessian(X, w);
-  
+
   std::cout << "mb_atomic:" << std::endl;
   for(size_t i = 0; i < nvars; i++)
   {  for(size_t j = 0; j < nvars; j++)
-        std::cout << " " << std::setw(10) 
-        << hess_sparse1[ i * nvars + j]; 
+        std::cout << " " << std::setw(10)
+        << hess_sparse1[ i * nvars + j];
      std::cout << std::endl;
   }
 
   std::cout << "cppad:" << std::endl;
   for(size_t i = 0; i < nvars; i++)
   {  for(size_t j = 0; j < nvars; j++)
-        std::cout << " " << std::setw(10) 
-        << hess_sparse2[ i * nvars + j]; 
+        std::cout << " " << std::setw(10)
+        << hess_sparse2[ i * nvars + j];
      std::cout << std::endl;
   }
 
