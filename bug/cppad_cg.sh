8db4278063e864804640d301bba8f29148079a0a
Brad Bell <bradbell@seanet.com>
2015-05-31 16:04:05 -0700

    Set version in soruce code to 20150601. cppad_cg.sh: add copyright message.

--- bug/cppad_cg.sh
+++ bug/cppad_cg.sh
@@ -1,4 +1,16 @@
 #! /bin/bash -e
+# $Id:$
+# -----------------------------------------------------------------------------
+# CppAD: C++ Algorithmic Differentiation: Copyright (C) 2003-15 Bradley M. Bell
+#
+# CppAD is distributed under multiple licenses. This distribution is under
+# the terms of the
+#                     Eclipse Public License Version 1.0.
+#
+# A copy of this license is included in the COPYING file of this distribution.
+# Please visit http://www.coin-or.org/CppAD/ for information on other licenses.
+# -----------------------------------------------------------------------------
+#! /bin/bash -e
 web_page='https://github.com/joaoleal/CppADCodeGen.git'
 # -----------------------------------------------------------------------------
 # bash function that echos and executes a command
