3923c32b8bc295d6c9c124210dc573d4777c0f75
Brad Bell <bradbell@seanet.com>
2015-08-17 13:06:31 -0700

    Remove invisible white space.

--- test_more/checkpoint.cpp
+++ test_more/checkpoint.cpp
@@ -3,7 +3,7 @@
 CppAD: C++ Algorithmic Differentiation: Copyright (C) 2003-13 Bradley M. Bell
 
 CppAD is distributed under multiple licenses. This distribution is under
-the terms of the 
+the terms of the
                     Eclipse Public License Version 1.0.
 
 A copy of this license is included in the COPYING file of this distribution.
@@ -43,26 +43,26 @@ namespace {
 		using CppAD::NearEqual;
 		size_t i, j, k, n = 4, ell = n-1 , m = ell + 1;
 		double eps = 10. * std::numeric_limits<double>::epsilon();
-	
+
 		// checkpoint version of the function F(x)
 		ADVector ax(n), ay(ell), az(m);
 		for(j = 0; j < n; j++)
 			ax[j] = double(j);
-		checkpoint<double> f_check("f_check", f_algo, ax, ay); 
-		checkpoint<double> g_check("g_check", g_algo, ay, az); 
-	
+		checkpoint<double> f_check("f_check", f_algo, ax, ay);
+		checkpoint<double> g_check("g_check", g_algo, ay, az);
+
 		// Record a version of z = g[f(x)] without checkpointing
 		Independent(ax);
 		f_algo(ax, ay);
 		g_algo(ay, az);
 		ADFun<double> check_not(ax, az);
-	
+
 		// Record a version of z = g[f(x)] with checkpointing
 		Independent(ax);
 		f_check(ax, ay);
 		g_check(ay, az);
 		ADFun<double> check_yes(ax, az);
-	
+
 		// compare forward mode results for orders 0, 1, 2
 		size_t p = 2;
 		CPPAD_TESTVECTOR(double) x_p(n*(p+1)), z_not(m*(p+1)), z_yes(m*(p+1));
@@ -79,7 +79,7 @@ namespace {
 				ok &= NearEqual(zik_not, zik_yes, eps, eps);
 			}
 		}
-	
+
 		// compare reverse mode results
 		CPPAD_TESTVECTOR(double) w(m*(p+1)), dw_not(n*(p+1)), dw_yes(n*(p+1));
 		dw_not = check_not.Reverse(p+1, w);
@@ -91,11 +91,11 @@ namespace {
 				ok &= NearEqual(dwjk_not, dwjk_yes, eps, eps);
 			}
 		}
-	
+
 		// mix sparsity so test both cases
 		f_check.option( CppAD::atomic_base<double>::bool_sparsity_enum );
 		g_check.option( CppAD::atomic_base<double>::set_sparsity_enum );
-	
+
 		// compare forward mode Jacobian sparsity patterns
 		size_t q = n - 1;
 		CppAD::vector< std::set<size_t> > r(n), s_not(m), s_yes(m);
@@ -111,7 +111,7 @@ namespace {
 		s_yes = check_yes.ForSparseJac(q, r);
 		for(i = 0; i < m; i++)
 			ok &= s_not[i] == s_yes[i];
-	
+
 		// compare reverse mode Jacobian sparsity patterns
 		CppAD::vector< std::set<size_t> > s(m), r_not(m), r_yes(m);
 		for(i = 0; i < m; i++)
@@ -120,17 +120,17 @@ namespace {
 		r_yes = check_yes.RevSparseJac(m, s);
 		for(i = 0; i < m; i++)
 			ok &= s_not[i] == s_yes[i];
-		
-	
+
+
 		// compare reverse mode Hessian sparsity patterns
-		CppAD::vector< std::set<size_t> > s_one(1), h_not(q), h_yes(q); 
+		CppAD::vector< std::set<size_t> > s_one(1), h_not(q), h_yes(q);
 		for(i = 0; i < m; i++)
 			s_one[0].insert(i);
 		h_not = check_not.RevSparseHes(q, s_one);
 		h_yes = check_yes.RevSparseHes(q, s_one);
 		for(i = 0; i < q; i++)
 			ok &= h_not[i] == h_yes[i];
-		
+
 		checkpoint<double>::clear();
 		return ok;
 	}
