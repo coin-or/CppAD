0603e033afebe957046312a39fab9b2d66aa7a83
Brad Bell <bradbell@seanet.com>
2015-09-22 09:50:01 -0700

    mul_cond.cpp: copy version from cond_exp_1 branch.

--- test_more/mul_cond.cpp
+++ test_more/mul_cond.cpp
@@ -19,56 +19,86 @@ bool mul_cond(void)
 {
 	bool ok = true;
 	using CppAD::vector;
+	using CppAD::NearEqual;
 	double eps = 10. * std::numeric_limits<double>::epsilon();
 	//
 	typedef CppAD::AD<double>   a1double;
 	typedef CppAD::AD<a1double> a2double;
 	//
-	a2double a2zero = a2double(0.0);
-	a2double a2one  = a2double(1.0);
+	a1double a1zero = 0.0;
+	a2double a2zero = a1zero;
+	a1double a1one  = 1.0;
+	a2double a2one  = a1one;
 	//
 	// --------------------------------------------------------------------
 	// create a1f = f(x)
 	size_t n = 1;
 	size_t m = 2;
+	size_t i;
 	vector<a2double> a2x(n), a2y(m);
 	a2x[0] = a2double( 5.0 );
 	Independent(a2x);
-
+	//
+	i = 0;
 	// div
-	a2y[0]  = CondExpGt(a2x[0], a2zero, a2one / a2x[0], a2zero);
-
-	// abs
-	a2y[1]  = CondExpGt(a2x[0], a2zero, abs( a2one / a2x[0] ), a2zero);
-
+	a2y[i++]  = CondExpGt(a2x[0], a2zero, a2one / a2x[0], a2zero);
+	// acosh
+	a2y[i++]  = CondExpGt(a2x[0], a2zero, acosh( a2x[0] ), a2zero);
+	//
 	CppAD::ADFun<a1double> a1f;
 	a1f.Dependent(a2x, a2y);
 	// --------------------------------------------------------------------
-	// create g = sum_i f_i '(x)
-	vector<a1double> a1x(n), a1dy(n), a1w(m);
+	// create h = f(x)
+	vector<a1double> a1x(n), a1y(m);
+	a1x[0] = 5.0;
+	//
+	Independent(a1x);
+	i = 0;
+	// div
+	a1y[i++]  = CondExpGt(a1x[0], a1zero, a1one / a1x[0], a1zero);
+	// acosh
+	a1y[i++]  = CondExpGt(a1x[0], a1zero, acosh( a1x[0] ), a1zero);
+	//
+	CppAD::ADFun<double> h;
+	h.Dependent(a1x, a1y);
+	// --------------------------------------------------------------------
+	// create g = f'(x)
+	vector<a1double> a1dy(m), a1w(m);
 	a1x[0] = 2.0;
-	for(size_t i = 0; i < m; i++)
-		a1w[i] = 1.0;
+	for(i = 0; i < m; i++)
+		a1w[i] = 0.0;
 	//
 	Independent(a1x);
 	a1f.Forward(0, a1x);
-	a1dy   = a1f.Reverse(1, a1w);
+	//
+	for(i = 0; i < m; i++)
+	{	a1w[i] = 1.0;
+		vector<a1double> dyi_dx = a1f.Reverse(1, a1w);
+		a1dy[i] = dyi_dx[0];
+		a1w[i] = 0.0;
+	}
 	CppAD::ADFun<double> g;
 	g.Dependent(a1x, a1dy);
 	// --------------------------------------------------------------------
-	// check g where x[0] > 0
-	vector<double> x(1), y(1);
-	x[0] = 2.0;
-	y    = g.Forward(0, x);
-	double check = 0.0;
-	check += -1.0 / (x[0] * x[0]); //  1 / x[0]
-	check += -1.0 / (x[0] * x[0]); // abs( 1 / x[0] )
-	ok  &= CppAD::NearEqual(y[0], check, eps, eps);
+	// check case where x[0] > 0
+	vector<double> x(1), dx(1), dg(m), dh(m);
+	x[0]  = 2.0;
+	dx[0] = 1.0;
+	h.Forward(0, x);
+	dh   = h.Forward(1, dx);
+	dg   = g.Forward(0, x);
+	for(i = 0; i < m; i++)
+		ok  &= NearEqual(dg[i], dh[i], eps, eps);
 	// --------------------------------------------------------------------
-	// check g where x[0] = 0
+	// check case where x[0] = 0
 	x[0] = 0.0;
-	y    = g.Forward(0, x);
-	ok  &= CppAD::NearEqual(y[0], 0.0, eps, eps);
+	dg   = g.Forward(0, x);
+	h.Forward(0, x);
+	dh   = h.Forward(1, dx);
+	for(i = 0; i < m; i++)
+	{	ok  &= dg[i] == 0.0;
+		ok  &= dh[i] == 0.0;
+	}
 	// --------------------------------------------------------------------
 	return ok;
 }
