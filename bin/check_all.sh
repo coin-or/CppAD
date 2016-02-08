1319927a007df8eab3b3bb037a85a463657ddf3f
Brad Bell <bradbell@seanet.com>
2016-02-08 04:38:16 -0700

    stable/20160000 branch: check_all.sh: fix abort when build directory in in use. CMakeLists.txt: Add version number to library (copied from master).

--- bin/check_all.sh
+++ bin/check_all.sh
@@ -48,6 +48,14 @@ then
 fi
 top_srcdir=`pwd`
 echo "top_srcdir = $top_srcdir"
+#
+if ! random_zero_one=`expr $RANDOM % 2`
+then
+	# expr exit status is 1 when the expression result is zero
+	# supress shell exit in this case
+	:
+fi
+echo "random_zero_one = $random_zero_one"
 # ---------------------------------------------------------------------------
 # circular shift program list and set program to first entry in list
 next_program() {
@@ -55,19 +63,10 @@ next_program() {
 	program=`echo "$program_list" | sed -e 's| *\([^ ]*\).*|\1|'`
 }
 # ---------------------------------------------------------------------------
-list="
-	$HOME/prefix/cppad
-	build
-"
-for name in $list
-do
-	if [ -e "$name" ]
-	then
-		echo_log_eval rm -r $name
-	fi
-done
-random_zero_one=`expr $RANDOM % 2`
-echo "random_zero_one = $random_zero_one"
+if [ -e "$HOME/prefix/cppad" ]
+then
+	echo_log_eval rm -r $HOME/prefix/cppad
+fi
 # ---------------------------------------------------------------------------
 # Create package to run test in
 echo "bin/package.sh"
