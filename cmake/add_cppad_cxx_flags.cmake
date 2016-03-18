4714099e4eef9226536760e206f988440eec2f48
Brad Bell <bradbell@seanet.com>
2016-03-18 09:50:37 -0700

    Use system include option to supress all warnings in eigen and fadbad source.

--- cmake/add_cppad_cxx_flags.cmake
+++ cmake/add_cppad_cxx_flags.cmake
@@ -1,52 +1,43 @@
 # $Id$
 # -----------------------------------------------------------------------------
-# CppAD: C++ Algorithmic Differentiation: Copyright (C) 2003-13 Bradley M. Bell
+# CppAD: C++ Algorithmic Differentiation: Copyright (C) 2003-16 Bradley M. Bell
 #
 # CppAD is distributed under multiple licenses. This distribution is under
-# the terms of the 
+# the terms of the
 #                     Eclipse Public License Version 1.0.
 #
 # A copy of this license is included in the COPYING file of this distribution.
 # Please visit http://www.coin-or.org/CppAD/ for information on other licenses.
 # -----------------------------------------------------------------------------
-# -----------------------------------------------------------------------------
 # add_cppad_cxx_flags(target_name)
 #
 # target_name: (in)
-# For package equal to "eigen", "fadbad", "scaado", if target_name ends in
-# _package, the compiler flags for this target are set to 
-#	cppad_cxx_flags_package
+# If target_name ends in _sacado,
+# the compiler flags for this target are set to
+#	cppad_cxx_flags_sacado
 # Otherwise, the compiler flags for this target are set to
 #	cppad_cxx_flags.
 #
 # Side Effects:
-# The variables package and flags are used for scratch space and not
+# The variable flags is used for scratch space and not
 # defined after the completion of this macro.
 #
-MACRO(add_cppad_cxx_flags target_name) 
+MACRO(add_cppad_cxx_flags target_name)
 	#
 	SET(flags ${cppad_cxx_flags})
-	FOREACH(package eigen fadbad sacado)
-		IF( ${target_name} MATCHES ".*_${package}$" )
-			SET(flags "${cppad_cxx_flags_${package}}")
-			MESSAGE(STATUS 
-				"${target_name} cxx_flags = ${cppad_cxx_flags_${package}}"
-			)
-		ENDIF( ${target_name} MATCHES ".*_${package}$" )
-		IF( ${target_name} MATCHES ".*_${package}_lib$" )
-			SET(flags "${cppad_cxx_flags_${package}}")
-			MESSAGE(STATUS 
-				"${target_name} cxx_flags = ${cppad_cxx_flags_${package}}"
-			)
-		ENDIF( ${target_name} MATCHES ".*_${package}_lib$" )
-	ENDFOREACH(package)
+	IF( ${target_name} MATCHES ".*_sacado$" )
+		SET(flags "${cppad_cxx_flags_sacado}")
+		MESSAGE(STATUS
+			"${target_name} cxx_flags = ${cppad_cxx_flags_sacado}"
+		)
+	ENDIF( ${target_name} MATCHES ".*_sacado$" )
 	IF( flags )
-		SET_TARGET_PROPERTIES( 
+		SET_TARGET_PROPERTIES(
 			${target_name} PROPERTIES COMPILE_FLAGS "${flags}"
 		)
 	ELSE( flags )
-		SET_TARGET_PROPERTIES( 
+		SET_TARGET_PROPERTIES(
 			${target_name} PROPERTIES COMPILE_FLAGS ""
 		)
 	ENDIF( flags )
-ENDMACRO(add_cppad_cxx_flags) 
+ENDMACRO(add_cppad_cxx_flags)
--- cmake/optional_package.cmake
+++ cmake/optional_package.cmake
@@ -1,30 +1,38 @@
 # $Id$
 # -----------------------------------------------------------------------------
-# CppAD: C++ Algorithmic Differentiation: Copyright (C) 2003-13 Bradley M. Bell
+# CppAD: C++ Algorithmic Differentiation: Copyright (C) 2003-16 Bradley M. Bell
 #
 # CppAD is distributed under multiple licenses. This distribution is under
-# the terms of the 
+# the terms of the
 #                     Eclipse Public License Version 1.0.
 #
 # A copy of this license is included in the COPYING file of this distribution.
 # Please visit http://www.coin-or.org/CppAD/ for information on other licenses.
 # -----------------------------------------------------------------------------
-# optional_package(prefix_variable description)
+# optional_package(prefix_variable system_include description)
 #
 # prefix_variable: (out)
-# is a PATH variable that holds the prefix for this optional package.
-# If this variable is not set by the cmake command line (or gui),
-# it is set to the default value NOTFOUND. 
-# If this variable is set by the cmake command line, the following is done:
-# 1. All the valid include subdirectories are added using INCLUDE_DIRECTORIES.
-# 2. All the valid library subdirectories are added using LINK_DIRECTORIES. 
-# where the valid include and library directories are determined by
-# cmake_install_includedirs and cmakd_install_libdirs respectively.
+# is a PATH variable that holds the install prefix for this optional package.
+#
+# system_include: (in)
+# If this is true, the include files for this package should be treated as
+# system files (no warnings).
 #
 # description: (in)
 # is a description for the install prefix for this optional package.
 #
-MACRO(optional_package prefix_variable description) 
+# If package_variable is not set by the cmake command line (or gui),
+# it is set to the default value NOTFOUND.
+#
+# If package_variable is set by the cmake command line, the following is done:
+# 1. All the valid include subdirectories are added using INCLUDE_DIRECTORIES.
+# 2. All the valid library subdirectories are added using LINK_DIRECTORIES.
+# The set of valid include and library directories are determined by
+# cmake_install_includedirs and cmakd_install_libdirs respectively.
+#
+# description: (in)
+#
+MACRO(optional_package prefix_variable system_include description)
 	SET(${prefix_variable} NOTFOUND CACHE PATH "${description}")
 	SET(prefix ${${prefix_variable}} )
 	MESSAGE(STATUS "${prefix_variable} = ${prefix}")
@@ -32,8 +40,13 @@ MACRO(optional_package prefix_variable description)
 		# List of preprocessor include file search directories
 		FOREACH(dir ${cmake_install_includedirs})
 			IF(IS_DIRECTORY ${prefix}/${dir} )
-				INCLUDE_DIRECTORIES( ${prefix}/${dir} )
-				MESSAGE(STATUS "Found ${prefix}/${dir}")
+				IF( ${system_include} )
+					INCLUDE_DIRECTORIES( SYSTEM ${prefix}/${dir} )
+					MESSAGE(STATUS "    Found SYSTEM ${prefix}/${dir}")
+				ELSE( ${system_include} )
+					INCLUDE_DIRECTORIES( ${prefix}/${dir} )
+					MESSAGE(STATUS "    Found ${prefix}/${dir}")
+				ENDIF( ${system_include} )
 			ENDIF(IS_DIRECTORY ${prefix}/${dir} )
 		ENDFOREACH(dir)
 		# Paths in which the linker will search for libraries,
@@ -41,7 +54,7 @@ MACRO(optional_package prefix_variable description)
 		FOREACH(dir ${cmake_install_libdirs})
 			IF(IS_DIRECTORY ${prefix}/${dir} )
 				LINK_DIRECTORIES( ${prefix}/${dir} )
-				MESSAGE(STATUS "Found ${prefix}/${dir}")
+				MESSAGE(STATUS "    Found ${prefix}/${dir}")
 			ENDIF(IS_DIRECTORY ${prefix}/${dir} )
 		ENDFOREACH(dir)
 	ENDIF ( prefix )
