e54f0449e101bca65d9018ffdfd77c9e0c03e753
Brad Bell <bradbell@seanet.com>
2016-11-30 05:11:35 -0700

    optimize branch: Move cexp_compare definition to separate file.

--- cppad/local/optimize/op_info.hpp
+++ cppad/local/optimize/op_info.hpp
@@ -17,49 +17,11 @@ Please visit http://www.coin-or.org/CppAD/ for information on other licenses.
 Create operator information tables
 */
 
+# include <cppad/local/optimize/cexp_compare.hpp>
 # include <cppad/local/optimize/fast_empty_set.hpp>
 
-// BEGIN_CPPAD_LOCAL__OPTIMIZE_NAMESPACE
+// BEGIN_CPPAD_LOCAL_OPTIMIZE_NAMESPACE
 namespace CppAD { namespace local { namespace optimize {
-
-/// information that identifies
-// a conditional expression and its comparison result.
-class cexp_compare {
-private:
-	/// Conditional expression index, cexp2op[ index_ ]
-	/// is the coreresponding operator index.
-	size_t index_;
-
-	/// does this correspond to a true or false result for the comparison
-	bool   compare_;
-
-public:
-	/// constructor
-	cexp_compare(size_t index, bool compare) :
-	index_ ( index )  ,
-	compare_( compare )
-	{ }
-
-	/// index()
-	size_t index(void) const
-	{	return index_; }
-
-	/// compare()
-	bool compare(void) const
-	{	return compare_; }
-
-	/// comparison operator (necessary for set elements)
-	bool operator < (const cexp_compare& right) const
-	{	size_t left_key  = 2 * index_ + size_t(compare_);
-		size_t right_key = 2 * right.index_ + size_t( right.compare_ );
-		return left_key < right_key;
-	}
-};
-
-inline std::ostream& operator << (std::ostream& os, const cexp_compare& cexp)
-{	os << '(' << cexp.index() << ',' << cexp.compare() << ')';
-	return os;
-}
 
 /// information for one operator
 struct struct_op_info {
