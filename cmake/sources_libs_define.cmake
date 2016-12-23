d2555cd4e77a47fc3c6e62534a07feea56e68f94
Brad Bell <bradbell@seanet.com>
2016-12-22 17:28:00 -0700

    Print out avaiable make check commands during camke command execution.

--- cmake/sources_libs_define.cmake
+++ cmake/sources_libs_define.cmake
@@ -1,9 +1,9 @@
 # $Id$
 # -----------------------------------------------------------------------------
-# CppAD: C++ Algorithmic Differentiation: Copyright (C) 2003-14 Bradley M. Bell
+# CppAD: C++ Algorithmic Differentiation: Copyright (C) 2003-16 Bradley M. Bell
 #
 # CppAD is distributed under multiple licenses. This distribution is under
-# the terms of the 
+# the terms of the
 #                     Eclipse Public License Version 1.0.
 #
 # A copy of this license is included in the COPYING file of this distribution.
@@ -18,7 +18,7 @@
 # Otherwise ${prefix_name}_sources is set to ${sources},
 # ${prefix_name}_libs is set to ${libs}. If ${define} is not empty "",
 # the defintion -DCPPAD_${define} is added; i.e.,
-#	ADD_DEFINITIONS("-DCPPAD_${define}") 
+#	ADD_DEFINITIONS("-DCPPAD_${define}")
 #
 MACRO(sources_libs_define prefix_name soruces libs define)
 	IF ( ${prefix_name}_prefix )
@@ -26,7 +26,7 @@ MACRO(sources_libs_define prefix_name soruces libs define)
 		SET( ${prefix_name}_libs    ${libs}    )
 		IF( NOT "${define}" STREQUAL "" )
 			ADD_DEFINITIONS("-DCPPAD_${define}")
-			MESSAGE(STATUS "-DCPPAD_${define}" )
+			# MESSAGE(STATUS "-DCPPAD_${define}" )
 		ENDIF( NOT "${define}" STREQUAL "" )
 	ELSE ( ${prefix_name}_prefix )
 		SET( ${prefix_name}_sources "" )
