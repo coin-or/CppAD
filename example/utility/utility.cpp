6a6adf630109d5787716900210917898d511fd0f
Brad Bell <bradbell@seanet.com>
2017-01-30 04:56:45 -0700

    sparsity branch: Sort test into increasing (not decreasing) aplhabetical order.

--- example/utility/utility.cpp
+++ example/utility/utility.cpp
@@ -19,28 +19,28 @@ Please visit http://www.coin-or.org/CppAD/ for information on other licenses.
 # include <cppad/utility/thread_alloc.hpp>
 
 // external compiled tests
-extern bool vectorBool(void);
-extern bool to_string(void);
-extern bool thread_alloc(void);
-extern bool SimpleVector(void);
-extern bool set_union(void);
-extern bool runge_45_1(void);
-extern bool RombergOne(void);
-extern bool RombergMul(void);
+extern bool CheckNumericType(void);
+extern bool CheckSimpleVector(void);
+extern bool CppAD_vector(void);
+extern bool ErrorHandler(void);
+extern bool index_sort(void);
+extern bool LuFactor(void);
+extern bool LuInvert(void);
+extern bool LuSolve(void);
+extern bool nan(void);
+extern bool Near_Equal(void);
+extern bool OdeErrControl(void);
+extern bool OdeErrMaxabs(void);
 extern bool OdeGearControl(void);
 extern bool OdeGear(void);
-extern bool OdeErrMaxabs(void);
-extern bool OdeErrControl(void);
-extern bool Near_Equal(void);
-extern bool nan(void);
-extern bool LuSolve(void);
-extern bool LuInvert(void);
-extern bool LuFactor(void);
-extern bool index_sort(void);
-extern bool ErrorHandler(void);
-extern bool CppAD_vector(void);
-extern bool CheckSimpleVector(void);
-extern bool CheckNumericType(void);
+extern bool RombergMul(void);
+extern bool RombergOne(void);
+extern bool runge_45_1(void);
+extern bool set_union(void);
+extern bool SimpleVector(void);
+extern bool thread_alloc(void);
+extern bool to_string(void);
+extern bool vectorBool(void);
 
 namespace {
 	// function that runs one test
@@ -68,28 +68,28 @@ int main(void)
 	// This line is used by test_one.sh
 
 	// external compiled tests
-	ok &= Run( vectorBool,                    "vectorBool" );
-	ok &= Run( to_string,                    "to_string" );
-	ok &= Run( thread_alloc,                    "thread_alloc" );
-	ok &= Run( SimpleVector,                    "SimpleVector" );
-	ok &= Run( set_union,                    "set_union" );
-	ok &= Run( runge_45_1,                    "runge_45_1" );
-	ok &= Run( RombergOne,                    "RombergOne" );
-	ok &= Run( RombergMul,                    "RombergMul" );
-	ok &= Run( OdeGearControl,                    "OdeGearControl" );
-	ok &= Run( OdeGear,                    "OdeGear" );
-	ok &= Run( OdeErrMaxabs,                    "OdeErrMaxabs" );
-	ok &= Run( OdeErrControl,                    "OdeErrControl" );
-	ok &= Run( Near_Equal,                    "Near_Equal" );
+	ok &= Run( CheckNumericType,       "CheckNumericType" );
+	ok &= Run( CheckSimpleVector,      "CheckSimpleVector" );
+	ok &= Run( CppAD_vector,           "CppAD_vector" );
+	ok &= Run( ErrorHandler,           "ErrorHandler" );
+	ok &= Run( index_sort,             "index_sort" );
+	ok &= Run( LuFactor,               "LuFactor" );
+	ok &= Run( LuInvert,               "LuInvert" );
+	ok &= Run( LuSolve,                "LuSolve" );
 	ok &= Run( nan,                    "nan" );
-	ok &= Run( LuSolve,                    "LuSolve" );
-	ok &= Run( LuInvert,                    "LuInvert" );
-	ok &= Run( LuFactor,                    "LuFactor" );
-	ok &= Run( index_sort,                    "index_sort" );
-	ok &= Run( ErrorHandler,                    "ErrorHandler" );
-	ok &= Run( CppAD_vector,                    "CppAD_vector" );
-	ok &= Run( CheckSimpleVector,                    "CheckSimpleVector" );
-	ok &= Run( CheckNumericType,                    "CheckNumericType" );
+	ok &= Run( Near_Equal,             "Near_Equal" );
+	ok &= Run( OdeErrControl,          "OdeErrControl" );
+	ok &= Run( OdeErrMaxabs,           "OdeErrMaxabs" );
+	ok &= Run( OdeGearControl,         "OdeGearControl" );
+	ok &= Run( OdeGear,                "OdeGear" );
+	ok &= Run( RombergMul,             "RombergMul" );
+	ok &= Run( RombergOne,             "RombergOne" );
+	ok &= Run( runge_45_1,             "runge_45_1" );
+	ok &= Run( set_union,              "set_union" );
+	ok &= Run( SimpleVector,           "SimpleVector" );
+	ok &= Run( thread_alloc,           "thread_alloc" );
+	ok &= Run( to_string,              "to_string" );
+	ok &= Run( vectorBool,             "vectorBool" );
 
 
 	// check for errors
