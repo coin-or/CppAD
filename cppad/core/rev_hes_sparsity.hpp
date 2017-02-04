8c04f4747f94b373ced977ef0e270a2830644e25
Brad Bell <bradbell@seanet.com>
2017-02-04 04:42:19 -0700

    sparsity branch: Remove old separator from when routines were in other files.

--- cppad/core/rev_hes_sparsity.hpp
+++ cppad/core/rev_hes_sparsity.hpp
@@ -12,7 +12,6 @@ A copy of this license is included in the COPYING file of this distribution.
 Please visit http://www.coin-or.org/CppAD/ for information on other licenses.
 -------------------------------------------------------------------------- */
 /*
-===============================================================================
 $begin rev_hes_sparsity$$
 $spell
 	Jacobian
