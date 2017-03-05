9df72f48e785d9fdc9dd31010b069759960cd133
Brad Bell <bradbell@seanet.com>
2017-03-05 15:44:48 -0700

    sparse_list.sh: more simplification.

--- bug/sparse_list.sh
+++ bug/sparse_list.sh
@@ -34,8 +34,7 @@ std::vector<CppAD::AD<double> > evaluateModel(const std::vector<CppAD::AD<double
                                               size_t repeat,
                                               CppAD::atomic_base<double>& atomModel);
 
-std::vector<double> getTypicalValues(std::vector<double> xa,
-                                     size_t repeat);
+std::vector<double> getTypicalValues(size_t repeat);
 
 const size_t K_ = 3;
 const size_t ns_ = 4;
@@ -55,39 +54,9 @@ int main(int argc, char *argv[]) {
         dxdt[3] = ind[2];
     };
 
-    std::vector<double> xx(na_);
-    xx[0] = 0.3; // h
-    xx[1] = 7.82e3; // Ca
-    xx[2] = 304.65; // Tr
-    xx[3] = 301.15; // Tj
-    xx[4] = 2.3333e-04; // u1
-    xx[5] = 6.6667e-05; // u2
-    xx[6] = 6.2e14; //
-    xx[7] = 10080; //
-    xx[8] = 2e3; //
-    xx[9] = 10e3; //
-    xx[10] = 1e-11; //
-    xx[11] = 6.6667e-05; //
-    xx[12] = 294.15; //
-    xx[13] = 294.15; //
-    xx[14] = 1000; //
-    xx[15] = 4184; //Cp
-    xx[16] = -33488; //deltaH
-    xx[17] = 299.15; // Tj0
-    xx[18] = 302.65; //   Tj2
-    xx[19] = 7e5; // cwallj
-    xx[20] = 1203; // csteam
-    xx[21] = 3.22; //dsteam
-    xx[22] = 950.0; //Ug
-    xx[23] = 0.48649427192323; //vc6in
-    xx[24] = 1000; //rhoj
-    xx[25] = 4184; //Cpj
-    xx[26] = 0.014; //Vj
-    xx[27] = 1e-7; //cwallr
-
     std::vector<AD<double>> ay(ns_), ax(na_);
     for(size_t i = 0; i < na_; ++i)
-        ax[i] = xx[i];
+        ax[i] = 1.0;
 
     checkpoint<double> atomic("cstr",
                               atomicFunction,
@@ -100,7 +69,7 @@ int main(int argc, char *argv[]) {
      */
     size_t repeat = 1;
 
-    std::vector<double> xTypical = getTypicalValues(xx, repeat);
+    std::vector<double> xTypical = getTypicalValues(repeat);
     size_t n = xTypical.size();
     std::vector <AD<double>> u(n);
     for (size_t j = 0; j < n; j++)
@@ -132,52 +101,13 @@ int main(int argc, char *argv[]) {
     return 0;
 }
 
-std::vector<double> getTypicalValues(std::vector<double> xa,
-                                     size_t repeat) {
-    /**
-     * collocation model values
-     */
+std::vector<double> getTypicalValues(size_t repeat) {
     size_t nvarsk = ns_;
     size_t nMstart = npar_ + nvarsk * K_ * repeat + nvarsk;
 
     std::vector<double> x(nMstart + repeat * nm_, 1.0);
-    // parameters
-    for (size_t j = 0; j < npar_; j++)
-        x[j] = xa[ns_ + nm_ + j];
-
-    size_t s = npar_;
-
-    // i = 0 K = 0
-    // states
-    for (size_t j = 0; j < ns_; j++) {
-        x[s++] = xa[j];
-    }
-
-    for (size_t i = 0; i < repeat; i++) {
-        // controls
-        for (size_t j = 0; j < nm_; j++) {
-            x[nMstart + nm_ * i + j] = xa[ns_ + j];
-        }
-
-        // K = 1
-        // states
-        for (size_t j = 0; j < ns_; j++) {
-            x[s] = 1.0 + 0.01 * i;
-            x[s++] = xa[j];
-        }
-
-        // K = 2
-        // states
-        for (size_t j = 0; j < ns_; j++) {
-            x[s++] = xa[j];
-        }
-
-        // K = 3
-        // states
-        for (size_t j = 0; j < ns_; j++) {
-            x[s++] = xa[j];
-        }
-    }
+	for(size_t j = 0; j < x.size(); j++)
+		x[j] = 1.0;
 
     return x;
 }
