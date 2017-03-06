5f2b76e6131cdc480d0976a25bf68bf2961aad99
Brad Bell <bradbell@seanet.com>
2017-03-05 17:44:56 -0700

    sparse_list.sh: more simplification, changechange  std::vector->CppAD::vector.

--- bug/sparse_list.sh
+++ bug/sparse_list.sh
@@ -29,22 +29,20 @@ cat << EOF > bug/build/test_jac_nnz.cpp
 #include <vector>
 
 using namespace CppAD;
-std::vector<CppAD::AD<double> > evaluateModel(
-	const std::vector<CppAD::AD<double> >& x,
-	size_t repeat,
+CppAD::vector<CppAD::AD<double> > evaluateModel(
+	const CppAD::vector<CppAD::AD<double> >& x,
 	CppAD::atomic_base<double>& atomModel
 );
 
 const size_t K_ = 3;
 const size_t ns_ = 4;
 const size_t nm_ = 2;
-const size_t npar_ = 22;
-const size_t na_ = ns_ + nm_ + npar_;
+const size_t na_ = ns_ + nm_;
 
 namespace {
     void algo(
-		const std::vector <AD<double>>& ind ,
-		std::vector <AD<double>>&      dxdt )
+		const CppAD::vector <AD<double>>& ind ,
+		CppAD::vector <AD<double>>&      dxdt )
 	{
         dxdt[2] = 1.0;
         dxdt[0] = 1.0;
@@ -55,7 +53,7 @@ namespace {
 
 int main(int argc, char *argv[]) {
 
-    std::vector<AD<double>> ay(ns_), ax(na_);
+    CppAD::vector<AD<double>> ay(ns_), ax(na_);
     for(size_t i = 0; i < na_; ++i)
         ax[i] = 1.0;
 
@@ -70,19 +68,17 @@ int main(int argc, char *argv[]) {
     /**
      * create tape
      */
-    size_t repeat = 1;
-    size_t nvarsk = ns_;
-    size_t nMstart = npar_ + nvarsk * K_ * repeat + nvarsk;
+    size_t nMstart = ns_ * K_  + ns_;
 
-    size_t n = nMstart + repeat * nm_;
+    size_t n = nMstart + nm_;
 
-    std::vector <AD<double>> u(n);
+    CppAD::vector <AD<double>> u(n);
     for (size_t j = 0; j < n; j++)
         u[j] = 1.0;
 
     CppAD::Independent(u);
 
-    std::vector<AD<double>> v = evaluateModel(u, repeat, atomic_fun);
+    CppAD::vector<AD<double>> v = evaluateModel(u, atomic_fun);
 
     ADFun<double> fun;
     fun.Dependent(v);
@@ -92,66 +88,46 @@ int main(int argc, char *argv[]) {
      */
     size_t m = fun.Range();
 
-    std::vector<std::set<size_t> > s_s(m);
+    CppAD::vector<std::set<size_t> > s_s(m);
     for (size_t i = 0; i < m; i++)
         s_s[i].insert(i);
 
     auto sparsity = fun.RevSparseJac(m, s_s);
 
     size_t nnz = 0;
-    for (const auto& s: sparsity) {
-        nnz += s.size();
+    for (size_t i = 0; i < sparsity.size(); i++) {
+		std::set<size_t> s_i = sparsity[i];
+        nnz += s_i.size();
     }
 	std::cout << nnz << "\n";
     return 0;
 }
 
-std::vector<CppAD::AD<double> > evaluateModel(
-	const std::vector<CppAD::AD<double> >& x,
-	size_t                                 repeat,
+CppAD::vector<CppAD::AD<double> > evaluateModel(
+	const CppAD::vector<CppAD::AD<double> >& x,
 	CppAD::atomic_base<double>&            atom_fun )
 {
-    size_t m2 = repeat * K_ * ns_;
+    size_t m2 = K_ * ns_;
 
     // dependent variable vector
-    std::vector<AD<double> > dep(m2);
+    CppAD::vector<AD<double> > dep(m2);
 	for(size_t i = 0; i < m2; i++)
 		dep[i] = 0.0;
 
-    std::vector<AD<double> > dxikdt(ns_);
-    std::vector<AD<double> > xik(na_);
+    CppAD::vector<AD<double> > dxikdt(ns_);
+    CppAD::vector<AD<double> > xik(na_);
 	for(size_t j = 0; j < na_; j++)
 		xik[j] = 1.0;
 
-    // parameters
-    for (size_t j = 0; j < npar_; j++)
-        xik[ns_ + nm_ + j] = x[j];
+	for (size_t j = 0; j < ns_; j++) {
+		xik[j] = x[j]; // states
+	}
+	atom_fun(xik, dxikdt); // ODE
+	for (size_t j = 0; j < ns_; j++) {
+		dep[j] = dxikdt[j] +  x[j];
+	}
 
-    size_t s = npar_;
-    size_t nvarsk = ns_;
-    size_t nMstart = npar_ + nvarsk * K_ * repeat + nvarsk;
-    size_t eq = 0;
-
-	size_t i = 0;
-    {
-        size_t s0 = s;
-
-        s += 2 * nvarsk;
-        eq += 2 * ns_;
-
-        // K = 3
-        for (size_t j = 0; j < ns_; j++) {
-            xik[j] = x[s + j]; // states
-        }
-
-        atom_fun(xik, dxikdt); // ODE
-        for (size_t j = 0; j < ns_; j++) {
-            dep[j] = dxikdt[j] +  x[s0 + 2 * nvarsk + j];
-        }
-
-    }
-
-    return dep;
+	return dep;
 }
 EOF
 # -----------------------------------------------------------------------------
@@ -160,4 +136,3 @@ sed -e '505s|^|check_data_not_used();\n|' -i cppad/local/sparse_list.hpp
 cd bug/build
 echo_eval g++ -I${cppad_path} --std=c++11 -g test_jac_nnz.cpp -o test_jac_nnz
 echo_eval ./test_jac_nnz
-#
