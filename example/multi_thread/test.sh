bbc2f92371869916de83753a705d4859230dd11c
Brad Bell <bradbell@seanet.com>
2017-06-10 11:30:13 -0600

    test.sh: fun multi_atomic test, drop old failure cases.

--- example/multi_thread/test.sh
+++ example/multi_thread/test.sh
@@ -54,16 +54,12 @@ next_program
 ./$program harmonic 1 4 1
 next_program
 echo
+# test_time=1 max_thread=4, num_itr=20
+./$program multi_atomic 1 4 20
+next_program
+echo
 # test_time= 2 max_thread=4, num_zero=20, num_sub=30, num_sum=500, use_ad=true
 ./$program multi_newton 2 4 20 30 500 true
-next_program
-echo
-# case that failed in the past
-./$program multi_newton 1 1 100 700 1 true
-next_program
-echo
-# case that failed in the past
-./$program multi_newton 1 2 3 12 1 true
 next_program
 echo
 # fast cases, do all programs
