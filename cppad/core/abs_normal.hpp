01366fe9ac6d773b06631d3cebaf01d94f9a8dea
Brad Bell <bradbell@seanet.com>
2017-06-14 05:59:09 -0600

    abs_normal.hpp: improve discussion.

--- cppad/core/abs_normal.hpp
+++ cppad/core/abs_normal.hpp
@@ -68,8 +68,9 @@ For $latex i = 0 , \ldots , {s-1}$$ define
 $latex \[
 a_i (x)
 =
-| \zeta_i | \left[ x, a_0 (x) , \ldots , a_{i-1} (x) \right]
+| \zeta_i [ x, a_0 (x) , \ldots , a_{i-1} (x) ] |
 \] $$
+This defines $latex a : \B{R}^n \rightarrow \B{R}^s$$.
 
 $subhead z(x, u)$$
 Define the smooth function
@@ -79,77 +80,101 @@ z_i ( x , u ) = \zeta_i ( x , u_0 , \ldots , u_{i-1} )
 \] $$
 
 $subhead y(x, u)$$
-We define
-$latex y : \B{R}^{n + s} \rightarrow  \B{R}^m$$ as the smooth function
+There is a smooth function
+$latex y : \B{R}^{n + s} \rightarrow  \B{R}^m$$
 such that $latex y( x , u ) = f(x)$$ whenever $latex u = a(x)$$.
 
 $head Abs-normal Approximation$$
-Suppose we are given a point $latex x \in \B{R}^n$$,
-and an increment $latex \Delta x$$,
-and define
-$latex u = a(x)$$,
-$latex \Delta u = a( x + \Delta x) - a(x)$$.
+
+$subhead Approximating a(x)$$
+The function $latex a(x)$$ is not smooth, but it is equal to
+$latex | z(x, u) |$$ when $latex u = a(x)$$.
+Furthermore
 $latex \[
-\begin{array}{rcl}
-z_i ( x + \Delta x, u + \Delta u )
-& = &
-z_i (x, u)
-	+ \partial_x z_i (x, u) \Delta x
-	+ \partial_u z_i (x, u) \Delta u
-	+ o( \Delta x )
-\end{array}
+z ( x + \Delta x, u + \Delta u )
+=
+z (x, u)
+	+ \partial_x z (x, u) \Delta x
+	+ \partial_u z (x, u) \Delta u
+	+ o( \Delta x , \Delta u )
 \] $$
 Now the partial of $latex z_i$$ with respect to $latex u_j$$ is zero
-for $latex i \leq j$$. It follows that
+for $latex j \geq i$$. It follows that
 $latex \[
-\begin{array}{rcl}
 z_i ( x + \Delta x, u + \Delta u )
-& = &
+=
 z_i (x, u)
 	+ \partial_x z_i (x, u) \Delta x
 	+ \sum_{j < i} \partial_{u(j)} z_i (x, u) \Delta u_j
-	+ o( \Delta x )
-\\
-a_i ( x + \Delta x )
-& = &
-\left| z_i (x, u)
-	+ \partial_x z_i (x, u) \Delta x
-	+ \sum_{j < i} \partial_{u(j)} z_i (x, u) \Delta u_j
-\right|
-+ o( \Delta x )
-\\
-\Delta u_i
-& = &
-\left| z_i (x, u)
-	+ \partial_x z_i (x, u) \Delta x
-	+ \sum_{j < i} \partial_{u(j)} z_i (x, u) \Delta u_j
-\right|
-- | z_i (x, u) |
-+ o( \Delta x )
-\end{array}
+	+ o( \Delta x , \Delta u )
 \] $$
-Given $latex u = a(x)$$ and $latex \Delta u_j$$ for $latex j < i$$,
-we can use the formula above to solve for $latex \Delta u_i$$ to
-within $latex o( \Delta x)$$.
-We call the resulting vector $latex \Delta \tilde{u}$$.
+Choosing $latex u = a(x)$$ and
+$latex \Delta u = a( x + \Delta x ) - a(x)$$,
+and taking absolute values, we have
 $latex \[
-\begin{array}{rcl}
+a_i ( x + \Delta x )
+=
+\left|
+	z_i [x, a(x) ]
+	+ \partial_x z_i [x, a(x) ] \Delta x
+	+ \sum_{j < i} \partial_{u(j)} z_i [x, a(x) ]
+		[ a_j ( x + \Delta x ) - a_j (x) ]
+\right|
++ o( \Delta x )
+\] $$
+We define $latex \tilde{a}_i (x, \Delta x )$$,
+for $latex i = 0, \ldots , s-1$$ by
+$latex \[
+\tilde{a}_i ( x , \Delta x )
+=
+\left|
+	z_i [x, a(x) ]
+	+ \partial_x z_i [x, a(x) ] \Delta x
+	+ \sum_{j < i} \partial_{u(j)} z_i [x, a(x) ]
+		[ \tilde{a}_j ( x , \Delta x ) - a_j (x) ]
+\right|
+\] $$
+It follows that
+$latex \[
+	\tilde{a} ( x ,  \Delta x ) = a( x + \Delta x ) + o ( \Delta x )
+\] $$
+Note that in the case where $latex z(x, u)$$ and $latex y(x, u)$$ are
+affine,
+$latex \[
+	\tilde{a} ( x ,  \Delta x ) = a( x + \Delta x )
+\] $$
+
+
+$subhead Approximating f(x)$$
+The function $latex y(x, u)$$ is smooth
+$latex \[
 y ( x + \Delta x, u + \Delta u )
-& = &
-y(x, u)
+=
+y (x, u)
 	+ \partial_x y (x, u) \Delta x
 	+ \partial_u y (x, u) \Delta u
-	+ o( \Delta x )
-\\
-f( x + \Delta x )
-& = &
-f(x)
-	+ \partial_x y (x, u) \Delta x
-	+ \partial_u y (x, u) \Delta \tilde{u}
-	+ o( \Delta x )
-\end{array}
+	+ o( \Delta x , \Delta u )
 \] $$
-This is the abs-normal approximation for $latex f$$ near $latex x$$.
+Choosing $latex u = a(x)$$ and
+$latex \Delta u = a( x + \Delta x ) - a(x)$$ we have
+$latex \[
+f(x + \Delta x )
+=
+f(x)
++ \partial_x y [ x, a(x) ] \Delta x
++ \partial_u y [ x, a(x) ] [ a(x + \Delta x ) - a(x) ]
++ o( \Delta x )
+\] $$
+Replacing $latex a(x + \Delta x)$$ by $latex \tilde{a} ( x, \Delta x )$$
+we have
+$latex \[
+f(x + \Delta x )
+=
+f(x)
++ \partial_x y [ x, a(x) ] \Delta x
++ \partial_u y [ x, a(x) ] [ \tilde{a}(x , \Delta x ) - a(x) ]
++ o( \Delta x )
+\] $$
 
 $head Correspondence to Literature$$
 Using the notation
@@ -171,8 +196,21 @@ y(\hat{x}, \hat{u}) + J \Delta x + Y \Delta u + o( \Delta x , \Delta u)
 \end{array}
 \] $$
 Using the notation
-$latex x = \hat{x} + \Delta x$$,
-$latex u = \hat{u} + \Delta u$$,
+$latex x = \hat{x} + \Delta x$$ and $latex u = \hat{u} + \Delta u$$ we have
+$latex \[
+\begin{array}{rcl}
+z(x, u)
+& = &
+z(\hat{x}, \hat{u})  - Z \hat{x} - L \hat{u} + Z x + L u
++ o( \Delta x , \Delta u)
+\\
+y(x, u)
+& = &
+y(\hat{x}, \hat{u}) - J \hat{x} - Y \hat{u} + J x + Y u
++ o( \Delta x , \Delta u)
+\end{array}
+\] $$
+Using the notation
 $latex c = z(\hat{x}, \hat{u}) - Z \hat{x} - L \hat{u}$$,
 $latex b = y(\hat{x}, \hat{u}) - J \hat{x} - Y \hat{u}$$,
 we have
@@ -180,31 +218,28 @@ $latex \[
 \begin{array}{rcl}
 z(x, u)
 & = &
-c + Z x + L u  + o( \Delta x , \Delta u)
+c + Z x + L u + o( \Delta x , \Delta u)
 \\
 y(x, u)
 & = &
 b + J x + Y u + o( \Delta x , \Delta u)
 \end{array}
 \] $$
-Choosing
-$latex \[
-	u = a(x) = |z(x, u)|
-\] $$
-Thus we obtain
+Choosing $latex u = a(x) = |z(x, u)|$$, we obtain
 $latex \[
 \begin{array}{rcl}
-z(x, u)
+z[ x , a(x) ]
 & = &
-c + Z x + L | z(x, u) |  + o( \Delta x )
+c + Z x + L |z[ x , a(x) ]| + o( \Delta x )
 \\
-y(x, u)
+y[ x , a(x) ]
 & = &
-b + J x + Y | z(x, u) | + o( \Delta x )
+b + J x + Y |z[ x , a(x) ]| + o( \Delta x )
 \end{array}
 \] $$
-This shows the correspondence between the notation above and
-Equation (2) of the reference.
+In the case where $latex y(x, u)$$ and $latex z(x, u)$$ are affine,
+this approximation is exact and we get Equation (2) of the reference.
+
 
 
 $end
