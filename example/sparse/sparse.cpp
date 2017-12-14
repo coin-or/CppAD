9344e3b04e68f8ad944de0f146388e690c43aca3
Brad Bell <bradbell@seanet.com>
2017-12-14 06:09:40 -0700

    Sort some lists.

--- example/sparse/sparse.cpp
+++ example/sparse/sparse.cpp
@@ -26,33 +26,33 @@ Please visit http://www.coin-or.org/CppAD/ for information on other licenses.
 
 // BEGIN_SORT_THIS_LINE_PLUS_2
 // external compiled tests
-extern bool subgraph_hes2jac(void);
-extern bool subgraph_jac_rev(void);
-extern bool subgraph_sparsity(void);
-extern bool sub_sparse_hes(void);
-extern bool sparsity_sub(void);
-extern bool sparse_sub_hes(void);
-extern bool sparse_hes(void);
-extern bool sparse_jac_for(void);
-extern bool sparse_jac_rev(void);
-extern bool sparse_jacobian(void);
-extern bool sparse_hessian(void);
-extern bool rev_hes_sparsity(void);
-extern bool rev_jac_sparsity(void);
-extern bool RevSparseJac(void);
-extern bool rev_sparse_hes(void);
-extern bool subgraph_reverse(void);
+extern bool colpack_hessian(void);
+extern bool colpack_hes(void);
+extern bool colpack_jacobian(void);
+extern bool colpack_jac(void);
+extern bool conj_grad(void);
+extern bool dependency(void);
 extern bool for_hes_sparsity(void);
 extern bool for_jac_sparsity(void);
-extern bool ForSparseJac(void);
 extern bool for_sparse_hes(void);
-extern bool dependency(void);
-extern bool conj_grad(void);
-extern bool colpack_jac(void);
-extern bool colpack_jacobian(void);
-extern bool colpack_hes(void);
-extern bool colpack_hessian(void);
+extern bool ForSparseJac(void);
 extern bool rc_sparsity(void);
+extern bool rev_hes_sparsity(void);
+extern bool rev_jac_sparsity(void);
+extern bool rev_sparse_hes(void);
+extern bool RevSparseJac(void);
+extern bool sparse_hessian(void);
+extern bool sparse_hes(void);
+extern bool sparse_jac_for(void);
+extern bool sparse_jacobian(void);
+extern bool sparse_jac_rev(void);
+extern bool sparse_sub_hes(void);
+extern bool sparsity_sub(void);
+extern bool subgraph_hes2jac(void);
+extern bool subgraph_jac_rev(void);
+extern bool subgraph_reverse(void);
+extern bool subgraph_sparsity(void);
+extern bool sub_sparse_hes(void);
 // END_SORT_THIS_LINE_MINUS_1
 
 // main program that runs all the tests
@@ -65,30 +65,30 @@ int main(void)
 
 	// BEGIN_SORT_THIS_LINE_PLUS_2
 	// external compiled tests
-	Run( subgraph_hes2jac,          "subgraph_hes2jac" );
-	Run( subgraph_jac_rev,          "subgraph_jac_rev" );
-	Run( subgraph_sparsity,         "subgraph_sparsity" );
-	Run( sub_sparse_hes,            "sub_sparse_hes" );
-	Run( sparsity_sub,              "sparsity_sub" );
-	Run( sparse_sub_hes,            "sparse_sub_hes" );
-	Run( sparse_hes,                "sparse_hes" );
-	Run( sparse_jac_for,            "sparse_jac_for" );
-	Run( sparse_jac_rev,            "sparse_jac_rev" );
-	Run( sparse_jacobian,           "sparse_jacobian" );
-	Run( sparse_hessian,            "sparse_hessian" );
-	Run( rev_hes_sparsity,          "rev_hes_sparsity" );
-	Run( rev_jac_sparsity,          "rev_jac_sparsity" );
-	Run( RevSparseJac,              "RevSparseJac" );
-	Run( rev_sparse_hes,            "rev_sparse_hes" );
-	Run( subgraph_reverse,          "reverse_subgraph");
+	Run( conj_grad,                 "conj_grad" );
+	Run( dependency,                "dependency" );
 	Run( for_hes_sparsity,          "for_hes_sparsity" );
 	Run( for_jac_sparsity,          "for_jac_sparsity" );
-	Run( ForSparseJac,              "ForSparseJac" );
 	Run( for_sparse_hes,            "for_sparse_hes" );
-	Run( dependency,                "dependency" );
-	Run( conj_grad,                 "conj_grad" );
+	Run( ForSparseJac,              "ForSparseJac" );
 	Run( rc_sparsity,               "rc_sparsity" );
-	// BEGIN_SORT_THIS_LINE_MINUS_1
+	Run( rev_hes_sparsity,          "rev_hes_sparsity" );
+	Run( rev_jac_sparsity,          "rev_jac_sparsity" );
+	Run( rev_sparse_hes,            "rev_sparse_hes" );
+	Run( RevSparseJac,              "RevSparseJac" );
+	Run( sparse_hessian,            "sparse_hessian" );
+	Run( sparse_hes,                "sparse_hes" );
+	Run( sparse_jac_for,            "sparse_jac_for" );
+	Run( sparse_jacobian,           "sparse_jacobian" );
+	Run( sparse_jac_rev,            "sparse_jac_rev" );
+	Run( sparse_sub_hes,            "sparse_sub_hes" );
+	Run( sparsity_sub,              "sparsity_sub" );
+	Run( subgraph_hes2jac,          "subgraph_hes2jac" );
+	Run( subgraph_jac_rev,          "subgraph_jac_rev" );
+	Run( subgraph_reverse,          "reverse_subgraph");
+	Run( subgraph_sparsity,         "subgraph_sparsity" );
+	Run( sub_sparse_hes,            "sub_sparse_hes" );
+	// END_SORT_THIS_LINE_MINUS_1
 	//
 # if CPPAD_HAS_COLPACK
 	Run( colpack_jac,               "colpack_jac" );
