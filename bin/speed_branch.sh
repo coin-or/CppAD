6c7f9c06d5acc06648e0b2173e654a99d0ab8f23
Brad Bell <bradbell@seanet.com>
2018-04-28 06:20:27 -0700

    speed_branch.sh: use master version of sizes, put logs in build directory.

--- bin/speed_branch.sh
+++ bin/speed_branch.sh
@@ -23,6 +23,7 @@ where the possible options are:
 EOF
 	exit 1
 fi
+#
 branch_one="$1"
 shift
 branch_two="$1"
@@ -38,6 +39,12 @@ do
 	fi
 done
 # ----------------------------------------------------------------------------
+build_dir='build/speed/cppad'
+if [ ! -e $build_dir ]
+then
+	echo_eval mkdir -p $build_dir
+fi
+# ----------------------------------------------------------------------------
 # bash function that echos and executes a command
 echo_eval() {
 	echo $*
@@ -50,39 +57,77 @@ then
 	exit 1
 fi
 # -----------------------------------------------------------------------------
-dir='build/speed/cppad'
+# get sizes in master speed/main.cpp
+git show master:speed/main.cpp > speed_branch.main.$$
+cat << EOF > speed_branch.sed.$$
+/^\\tfor(size_t i = 0; i < n_size; i++)\$/! b skip
+: loop
+N
+/\\n\\t}\$/! b loop
+s/^\\tfor(size_t i = 0; i < n_size; i++)\\n\\t{/\\t/
+s/\\n\\t}\$//
+p
+: skip
+EOF
+sed speed_branch.main.$$ -n -f speed_branch.sed.$$ > speed_branch.size.$$
+#
+# sed script to mark size location (must work if size_t not present)
+cat << EOF > speed_branch.sed.$$
+/^\\tfor([a-z_]* *i = 0; i < n_size; i++) *\$/! b skip
+: loop
+N
+/\\n\\t}\$/! b loop
+s|{|{\\n\\t// BEGIN_SIZES\\n\\t|
+s|\\n\\t}\$|\\n\\t// END_SIZES&|
+: skip
+EOF
+# -----------------------------------------------------------------------------
 for branch in $branch_one $branch_two
 do
-	if [ -e "build/speed/cppad/$branch.$option_list.out" ]
+	if [ -e "$build_dir/$branch.$option_list.out" ]
 	then
-		echo "Using existing build/speed/cppad/$branch.$option_list.out"
+		echo "Using existing $build_dir/$branch.$option_list.out"
 	else
 		# use --quiet to supress detachec HEAD message
 		echo_eval git checkout --quiet $branch
 		#
+		# changes sizes in speed/main.cpp to be same as in master branch
+		sed -i speed/main.cpp -f speed_branch.sed.$$
+		sed  speed/main.cpp  -n -e '1,/BEGIN_SIZES/p' > speed_branch.main.$$
+		cat  speed_branch.size.$$                    >> speed_branch.main.$$
+		sed  speed/main.cpp  -n -e '/END_SIZES/,$p'  >> speed_branch.main.$$
+		mv speed_branch.main.$$ speed/main.cpp
+		#
 		# versions of CppAD before 20170625 did not have --debug_none option
-		echo "bin/run_cmake.sh --debug_none > $branch.log"
-		if ! bin/run_cmake.sh --debug_none > $branch.log
+		echo "bin/run_cmake.sh --debug_none > $build_dir/$branch.log"
+		if ! bin/run_cmake.sh --debug_none > $build_dir/$branch.log
 		then
-			echo "bin/run_cmake.sh > $branch.log"
-			bin/run_cmake.sh > $branch.log
+			echo "bin/run_cmake.sh > $build_dir/$branch.log"
+			bin/run_cmake.sh > $build_dir/$branch.log
 		fi
 		#
-		echo_eval cd $dir
+		echo_eval cd $build_dir
 		#
-		echo "make check_speed_cppad >> $branch.log"
-		make check_speed_cppad >> ../../../$branch.log
+		echo "make check_speed_cppad >> $build_dir/$branch.log"
+		make check_speed_cppad >> $branch.log
 		#
-		echo "./speed_cppad speed 123 $* > $branch.$option_list.out"
+		echo "./speed_cppad speed 123 $* > $build_dir/$branch.$option_list.out"
 		./speed_cppad speed 123 $* > $branch.$option_list.out
 		#
 		cd ../../..
+		#
+		# restore speed/main.cpp to state in repo
+		git checkout speed/main.cpp
 	fi
 done
+rm speed_branch.sed.$$
+rm speed_branch.size.$$
+#
 # compare the results
 echo "	one=$branch_one , two=$branch_two"
 bin/speed_diff.sh \
-	$dir/$branch_one.$option_list.out $dir/$branch_two.$option_list.out
+	$build_dir/$branch_one.$option_list.out \
+	$build_dir/$branch_two.$option_list.out
 # ----------------------------------------------------------------------------
 echo "$0: OK"
 exit 0
