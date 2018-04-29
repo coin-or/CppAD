9bc04f552e328e84971722967cff140bc16e0829
Brad Bell <bradbell@seanet.com>
2018-04-29 08:09:22 -0700

    Correct copyright dates.

--- bin/speed_branch.sh
+++ bin/speed_branch.sh
@@ -1,6 +1,6 @@
 #! /bin/bash -e
 # -----------------------------------------------------------------------------
-# CppAD: C++ Algorithmic Differentiation: Copyright (C) 2003-17 Bradley M. Bell
+# CppAD: C++ Algorithmic Differentiation: Copyright (C) 2003-18 Bradley M. Bell
 #
 # CppAD is distributed under multiple licenses. This distribution is under
 # the terms of the
