9f237e9b852063bfdb0aaf621e45f751ab252a2a
Brad Bell <bradbell@seanet.com>
2018-07-24 05:29:01 -0700

    master: bug/template.sh: change to run from top source directory.

--- bug/template.sh
+++ bug/template.sh
@@ -1,7 +1,7 @@
 #! /bin/bash -e
 # $Id$
 # -----------------------------------------------------------------------------
-# CppAD: C++ Algorithmic Differentiation: Copyright (C) 2003-17 Bradley M. Bell
+# CppAD: C++ Algorithmic Differentiation: Copyright (C) 2003-18 Bradley M. Bell
 #
 # CppAD is distributed under multiple licenses. This distribution is under
 # the terms of the
@@ -10,18 +10,25 @@
 # A copy of this license is included in the COPYING file of this distribution.
 # Please visit http://www.coin-or.org/CppAD/ for information on other licenses.
 # -----------------------------------------------------------------------------
+name=`echo $0 | sed -e 's|.*/||' -e 's|\..*||'`
+if [ "$0" != "bug/$name.sh" ]
+then
+	echo "usage: bug/$name.sh"
+	exit 1
+fi
 cat << EOF
 Description
 EOF
-cat << EOF > bug.$$
+cat << EOF > $name.cpp
 # include <cppad/cppad.hpp>
 int main(void)
 {	bool ok = true;
 	using std::cout;
+	using CppAD::AD;
 	//
-	cout << "1. copy template.sh to <name>.sh\n";
-	cout << "2. Edit <name>.sh replacing description and C++ source code\n";
-	cout << "3. Run ./<name>.sh\n";
+	cout << "1. copy bug/template.sh to bug/<name>.sh\n";
+	cout << "2. Edit bug/<name>.sh replacing description and C++ source code\n";
+	cout << "3. Run bug/<name>.sh\n";
 	cout << "where <name> is a name that describes the bug\n";
 	//
 	if( ok )
@@ -30,32 +37,33 @@ int main(void)
 }
 EOF
 # -----------------------------------------------------------------------------
-if [ ! -e ../cppad/configure.hpp ]
+if [ ! -e cppad/configure.hpp ]
 then
 	echo
-	echo 'Cannot find the file cppad/configure.hpp in directory ..'
-	echo 'Must change into .. directory and run bin/run_cmake.sh'
-	rm bug.$$
+	echo 'Cannot find the file cppad/configure.hpp.'
+	echo 'Must run bin/run_cmake.sh to create it.'
+	rm $name.cpp
 	exit 1
 fi
-if [ ! -e build ]
+if [ -e build/bug ]
 then
-	mkdir build
+	rm -r build/bug
 fi
-cd build
-echo "$0"
-name=`echo $0 | sed -e 's|.*/||' -e 's|\..*||'`
-mv ../bug.$$ $name.cpp
-echo "g++ -I../.. --std=c++11 -g $name.cpp -o $name"
-g++ -I../.. --std=c++11 -g $name.cpp -o $name
+mkdir -p build/bug
+mv $name.cpp build/bug/$name.cpp
+cd build/bug
+cxx_flags='-Wall -pedantic-errors -std=c++11 -Wshadow -Wconversion -g -O0'
+eigen_dir="$HOME/prefix/eigen/include"
+echo "g++ -I../.. -isystem $eigen_dir $cxx_flags $name.cpp -o $name"
+g++ -I../.. -isystem $eigen_dir $cxx_flags $name.cpp -o $name
 #
-echo "./$name"
+echo "build/bug/$name"
 if ! ./$name
 then
 	echo
-	echo "$name.sh: Error"
+	echo "build/bug/$name: Error"
 	exit 1
 fi
 echo
-echo "$name.sh: OK"
+echo "./$name.sh: OK"
 exit 0
