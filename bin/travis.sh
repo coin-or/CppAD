c66da17eb6f6bd11e910d8266e3dab4ef4cc31d6
Brad Bell <bradbell@seanet.com>
2018-10-22 16:40:17 -0700

    master: travis: adding option to run just one test. test_one.sh.in: check for case where ColPack is not available. .travis.yml: run just one case that seems to be failing.

--- bin/travis.sh
+++ bin/travis.sh
@@ -14,11 +14,12 @@ then
 	echo "bin/travis.sh: must be executed from its parent directory"
 	exit 1
 fi
-if [ "$1" != '' ]
+if [ "$2" != '' ]
 then
-	echo 'bin/travis.sh does not have arguments'
+	echo 'usage: bin/travis.sh [path_to_one_test]'
 	exit 1
 fi
+path_to_one_test="$1"
 # -----------------------------------------------------------------------------
 # bash function that echos and executes a command
 echo_eval() {
@@ -33,7 +34,14 @@ fi
 echo_eval mkdir build
 echo_eval cd build
 echo_eval cmake ..
-echo_eval make check
+#
+if [ "$path_to_one_test" == '' ]
+then
+	echo_eval make check
+else
+	cd ..
+	echo_eval bin/test_one.sh $path_to_one_test
+fi
 # -----------------------------------------------------------------------------
 echo 'bin/travis.sh: OK'
 exit 0
