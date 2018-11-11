552168ac4dea9b061893d80832dc841d7c6695f1
Brad Bell <bradbell@seanet.com>
2018-11-11 05:36:50 -0800

    master: bin/devel.sh: new configuration file for tools in ~/devel.

---
+++ bin/devel.sh
@@ -0,0 +1,16 @@
+#! /bin/bash -e
+# -----------------------------------------------------------------------------
+# CppAD: C++ Algorithmic Differentiation: Copyright (C) 2003-18 Bradley M. Bell
+#
+# CppAD is distributed under multiple licenses. This distribution is under
+# the terms of the
+#                     Eclipse Public License Version 1.0.
+#
+# A copy of this license is included in the COPYING file of this distribution.
+# Please visit http://www.coin-or.org/CppAD/ for information on other licenses.
+# -----------------------------------------------------------------------------
+# configuration file for tools in ~/devel.
+#
+# web address linked by run_omhelp.sh to the Home icon on each page.
+image_link='https://coin-or.github.io/CppAD'
+echo "image_link=$image_link"
