f06e402342b80f9a9ebdc843c2dcdfd2cdb1b48e
Brad Bell <bradbell@seanet.com>
2018-11-21 20:53:26 -0700

    batch_edit: start conversion of license to EPL 2.0

--- speed/adolc/adolc_alloc_mat.hpp
+++ speed/adolc/adolc_alloc_mat.hpp
@@ -1,17 +1,16 @@
-// $Id$
+# ifndef CPPAD_SPEED_ADOLC_ADOLC_ALLOC_MAT_HPP
+# define CPPAD_SPEED_ADOLC_ADOLC_ALLOC_MAT_HPP
 /* --------------------------------------------------------------------------
 CppAD: C++ Algorithmic Differentiation: Copyright (C) 2003-16 Bradley M. Bell
 
-CppAD is distributed under multiple licenses. This distribution is under
-the terms of the
-                    Eclipse Public License Version 1.0.
+CppAD is distributed under the terms of the
+             Eclipse Public License Version 2.0.
 
-A copy of this license is included in the COPYING file of this distribution.
-Please visit http://www.coin-or.org/CppAD/ for information on other licenses.
--------------------------------------------------------------------------- */
-# ifndef CPPAD_SPEED_ADOLC_ADOLC_ALLOC_MAT_HPP
-# define CPPAD_SPEED_ADOLC_ADOLC_ALLOC_MAT_HPP
-
+This Source Code may also be made available under the following
+Secondary License when the conditions for such availability set forth
+in the Eclipse Public License, Version 2.0 are satisfied:
+           GNU General Public License, Version 3.0.
+---------------------------------------------------------------------------- */
 double** adolc_alloc_mat(size_t m, size_t n);
 void adolc_free_mat(double** mat);
 
