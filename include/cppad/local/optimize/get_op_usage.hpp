7961c4db37351e3ca78b33b1ba975a99f2dd2fd0
Brad Bell <bradbell@seanet.com>
2019-06-08 17:19:16 -0700

    master: get_op_usage.hpp: more conversion of add_element -> post_element.

--- include/cppad/local/optimize/get_op_usage.hpp
+++ include/cppad/local/optimize/get_op_usage.hpp
@@ -57,9 +57,11 @@ do not need to call op_add_or_sub for result).
 
 \param i_result
 is the operator index for the result operator.
+There are no posting waiting to be processed for the corresponding cexp_set.
 
 \param i_arg
 is the operator index for the argument to the result operator.
+There may be postings waiting to be processed for the corresponding cexp_set.
 
 \param op_usage
 structure that holds the information for each of the operators.
@@ -115,10 +117,13 @@ void op_inc_arg_usage(
     //
     if( arg_usage == no_usage )
     {   // set[i_arg] = set[i_result]
+        // not necessary to process posts for set i_result
         cexp_set.assignment(i_arg, i_result, cexp_set);
     }
     else
     {   // set[i_arg] = set[i_arg] intersect set[i_result]
+        // is necessary to process postts for set i_arg
+        cexp_set.process_post(i_arg);
         cexp_set.binary_intersection(i_arg, i_arg, i_result, cexp_set);
     }
     //
@@ -315,6 +320,10 @@ void get_op_usage(
     i_op = num_op;
     while(i_op != 0 )
     {   --i_op;
+        if( num_set > 0 )
+        {   // no more elements will be added to this set
+            cexp_set.process_post(i_op);
+        }
         //
         // this operator information
         random_itr.op_info(i_op, op, arg, i_var);
@@ -448,7 +457,7 @@ void get_op_usage(
                         // comparison result is true. It can be skipped when
                         // the comparison is false (0).
                         size_t element = 2 * cexp_index + 0;
-                        cexp_set.add_element(j_op, element);
+                        cexp_set.post_element(j_op, element);
                         //
                         op_usage[j_op] = usage_t(yes_usage);
                     }
@@ -467,7 +476,7 @@ void get_op_usage(
                         // comparison result is false. It can be skipped when
                         // the comparison is true (0).
                         size_t element = 2 * cexp_index + 1;
-                        cexp_set.add_element(j_op, element);
+                        cexp_set.post_element(j_op, element);
                         //
                         op_usage[j_op] = usage_t(yes_usage);
                     }
@@ -647,8 +656,10 @@ void get_op_usage(
                 );
 # ifndef NDEBUG
                 if( cexp_set.n_set() > 0 )
-                {   sparse::list_setvec_const_iterator itr(cexp_set, last_atom_i_op);
-                    CPPAD_ASSERT_UNKNOWN( *itr == cexp_set.end() );
+                {   cexp_set.process_post(last_atom_i_op);
+                    CPPAD_ASSERT_UNKNOWN(
+                        cexp_set.number_elements(last_atom_i_op) == 0
+                    );
                 }
 # endif
                 //
@@ -696,7 +707,9 @@ void get_op_usage(
                 }
                 // copy set infomation from last to first
                 if( cexp_set.n_set() > 0 )
+                {   cexp_set.process_post(last_atom_i_op);
                     cexp_set.assignment(i_op, last_atom_i_op, cexp_set);
+                }
                 // copy usage information from last to first
                 op_usage[i_op] = op_usage[last_atom_i_op];
             }
