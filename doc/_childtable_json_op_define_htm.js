d23f561e3cee4fa5abd9ed98c393c82d339722cb
Brad Bell <bradbell@seanet.com>
2019-07-30 13:28:29 -0700

    update gh-pages to version 20190730

--- doc/_childtable_json_op_define_htm.js
+++ doc/_childtable_json_op_define_htm.js
@@ -2,11 +2,15 @@
 document.write('\
 <select onchange="json_op_define_child(this)">\
 <option>json_op_define-&gt;</option>\
+<option>json_mul_op.cpp</option>\
+<option>json_sub_op.cpp</option>\
 <option>json_sum_op.cpp</option>\
 </select>\
 ');
 function json_op_define_child(item)
 {	var child_list = [
+		'json_mul_op.cpp.htm',
+		'json_sub_op.cpp.htm',
 		'json_sum_op.cpp.htm'
 	];
 	var index = item.selectedIndex;
--- doc/_contents.js
+++ doc/_contents.js
@@ -1,6 +1,6 @@
 var Cookie            = document.cookie;
 var Open              = '';
-var SectionCount      = 720   ;
+var SectionCount      = 722   ;
 var ImagesInitialized = false;
 Children              = new Array();
 Initialize();
