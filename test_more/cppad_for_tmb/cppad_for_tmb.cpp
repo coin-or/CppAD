c013485764c7c8a3f94da4d5e48def70cce04827
Brad Bell <bradbell@seanet.com>
2019-08-05 16:49:15 -0700

    master: cppad_for_tmb: copy atomic_two->atomic_three, chkpoint_one->chkpoint_two.

--- test_more/cppad_for_tmb/cppad_for_tmb.cpp
+++ test_more/cppad_for_tmb/cppad_for_tmb.cpp
@@ -15,7 +15,9 @@ in the Eclipse Public License, Version 2.0 are satisfied:
 extern bool implicit_ctor(void);
 extern bool prefer_reverse(void);
 extern bool multi_atomic_two(void);
+extern bool multi_atomic_three(void);
 extern bool multi_chkpoint_one(void);
+extern bool multi_chkpoint_two(void);
 
 int main(void)
 {   std::string group = "test_more/cppad_for_tmb";
@@ -26,8 +28,10 @@ int main(void)
 
     Run( implicit_ctor,            "implicit_ctor"          );
     Run( prefer_reverse,           "prefer_reverse"         );
-    Run( multi_atomic_two,             "multi_atomic_two"           );
-    Run( multi_chkpoint_one,         "multi_chkpoint_one"       );
+    Run( multi_atomic_two,         "multi_atomic_two"       );
+    Run( multi_atomic_three,       "multi_atomic_three"     );
+    Run( multi_chkpoint_one,       "multi_chkpoint_one"     );
+    Run( multi_chkpoint_two,       "multi_chkpoint_two"     );
 
     // check for memory leak
     bool memory_ok = CppAD::thread_alloc::free_all();
