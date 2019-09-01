2b78b3b3992c395ef75fb3e2619da51eb44139c8
Brad Bell <bradbell@seanet.com>
2019-09-01 05:22:19 -0700

    master: json_operator: sort with simplier order.

--- cppad_lib/json_operator.cpp
+++ cppad_lib/json_operator.cpp
@@ -32,21 +32,22 @@ void set_operator_info(void)
         const char*  name;
         size_t       n_arg;
     };
+    // BEGIN_SORT_THIS_LINE_PLUS_2
     op_info op_info_vec[] = {
         { abs_json_op,      "abs",      1 }, // 1 result
-        { acosh_json_op,    "acosh",    1 }, // 1 result
         { acos_json_op,     "acos",     1 }, // 1 result
+        { acosh_json_op,    "acosh",    1 }, // 1 result
         { add_json_op,      "add",      2 }, // 1 result
-        { asinh_json_op,    "asinh",    1 }, // 1 result
         { asin_json_op,     "asin",     1 }, // 1 result
-        { atanh_json_op,    "atanh",    1 }, // 1 result
+        { asinh_json_op,    "asinh",    1 }, // 1 result
         { atan_json_op,     "atan",     1 }, // 1 result
+        { atanh_json_op,    "atanh",    1 }, // 1 result
         { atom_json_op,     "atom",     0 }, // variable number arg and result
         { cexp_eq_json_op,  "cexp_eq",  4 }, // 1 result
         { cexp_le_json_op,  "cexp_le",  4 }, // 1 result
         { cexp_lt_json_op,  "cexp_lt",  4 }, // 1 result
-        { cosh_json_op,     "cosh",     1 }, // 1 result
         { cos_json_op,      "cos",      1 }, // 1 result
+        { cosh_json_op,     "cosh",     1 }, // 1 result
         { div_json_op,      "div",      2 }, // 1 result
         { erf_json_op,      "erf",      1 }, // 1 result
         { erfc_json_op,     "erfc",     1 }, // 1 result
@@ -56,14 +57,15 @@ void set_operator_info(void)
         { log_json_op,      "log",      1 }, // 1 result
         { mul_json_op,      "mul",      2 }, // 1 result
         { sign_json_op,     "sign",     1 }, // 1 result
-        { sinh_json_op,     "sinh",     1 }, // 1 result
         { sin_json_op,      "sin",      1 }, // 1 result
+        { sinh_json_op,     "sinh",     1 }, // 1 result
         { sqrt_json_op,     "sqrt",     1 }, // 1 result
         { sub_json_op,      "sub",      2 }, // 1 result
         { sum_json_op,      "sum",      0 }, // variable number arg and result
-        { tanh_json_op,     "tanh",     1 }, // 1 result
-        { tan_json_op,      "tan",      1 }  // 1 result
+        { tan_json_op,      "tan",      1 }, // 1 result
+        { tanh_json_op,     "tanh",     1 }  // 1 result
     };
+    // END_SORT_THIS_LINE_MINUS_2
     CPPAD_ASSERT_UNKNOWN(
         size_t(n_json_op) == sizeof(op_info_vec) / sizeof(op_info_vec[0])
     );
