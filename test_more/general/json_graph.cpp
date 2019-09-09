36754a091a3d115cc702b994418f04b8adea2ad1
Brad Bell <bradbell@seanet.com>
2019-09-09 08:26:45 -0700

    unary: json: move unary examples to test (will leave just one as an example).

--- test_more/general/json_graph.cpp
+++ test_more/general/json_graph.cpp
@@ -13,6 +13,82 @@ in the Eclipse Public License, Version 2.0 are satisfied:
 
 namespace { // BEGIN_EMPTY_NAMESPACE
 // ---------------------------------------------------------------------------
+bool atan_op(void)
+{   bool ok = true;
+    using CppAD::vector;
+    using CppAD::AD;
+    double eps99 = 99.0 * std::numeric_limits<double>::epsilon();
+    //
+    // AD graph example
+    // node_1 : p[0]
+    // node_2 : x[0]
+    // node_3 : c[0]
+    // node_4 : atan(p[0])
+    // node_5 : atan(x[0])
+    // node_6 : atan(c[0])
+    // node_7 : atan(p[0]) + atan(x[0]) + atan(c[0])
+    // y[0]   = atan(p[0]) + atan(x[0]) + atan(c[0])
+    // use single quote to avoid having to escape double quote
+    std::string graph =
+        "{\n"
+        "   'function_name'  : 'atan_op example',\n"
+        "   'op_define_vec'  : [ 2, [\n"
+        "       { 'op_code':1, 'name':'atan', 'n_arg':1 } ,\n"
+        "       { 'op_code':2, 'name':'sum'             } ]\n"
+        "   ],\n"
+        "   'n_dynamic_ind'  : 1,\n"
+        "   'n_independent'  : 1,\n"
+        "   'constant_vec'   : 1, [ -0.1 ],\n" // c[0]
+        "   'op_usage_vec'   : 4, [\n"
+        "       [ 1, 1]                ,\n" // atan(p[0])
+        "       [ 1, 2]                ,\n" // atan(x[0])
+        "       [ 1, 3]                ,\n" // atan(c[0])
+        "       [ 2, 1, 3, [4, 5, 6] ] ]\n" // atan(p[0])+atan(x[0])+atan(c[0])
+        "   ,\n"
+        "   'dependent_vec' : 1, [7]\n"
+        "}\n";
+    // Convert the single quote to double quote
+    for(size_t i = 0; i < graph.size(); ++i)
+        if( graph[i] == '\'' ) graph[i] = '"';
+    //
+    // f(x, p) = atan(p_0) + atan(x_0) + atan(c_0)
+    CppAD::ADFun<double> f;
+    f.from_json(graph);
+    ok &= f.Domain() == 1;
+    ok &= f.Range() == 1;
+    ok &= f.size_dyn_ind() == 1;
+    //
+    // value of constant in function
+    vector<double> c(1);
+    c[0] = -0.1;
+    //
+    // set independent variables and parameters
+    vector<double> p(1), x(1);
+    p[0] = 0.2;
+    x[0] = 0.3;
+    //
+    // compute y = f(x, p)
+    f.new_dynamic(p);
+    vector<double> y = f.Forward(0, x);
+    //
+    // check result
+    double check = std::atan(p[0]) + std::atan(x[0]) + std::atan(c[0]);
+    ok &= CppAD::NearEqual(y[0], check, eps99, eps99);
+    //
+    // Convert to Json graph and back again
+    graph = f.to_json();
+    f.from_json(graph);
+    //
+    // compute y = f(x, p)
+    f.new_dynamic(p);
+    y = f.Forward(0, x);
+    //
+    // check result
+    ok &= CppAD::NearEqual(y[0], check, eps99, eps99);
+    //
+    return ok;
+}
+// ---------------------------------------------------------------------------
 bool asin_op(void)
 {   bool ok = true;
     using CppAD::vector;
@@ -769,6 +845,7 @@ bool cumulative_sum(void)
 
 bool json_graph(void)
 {   bool ok = true;
+    ok     &= atan_op();
     ok     &= asin_op();
     ok     &= acos_op();
     ok     &= abs_op();
