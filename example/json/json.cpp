c8a5da4a0019aac3a0c75d203b459dbda23c722c
Brad Bell <bradbell@seanet.com>
2019-12-14 08:10:34 -0700

    master: json_ad_graph: add pow function.

--- example/json/json.cpp
+++ example/json/json.cpp
@@ -34,6 +34,7 @@ extern bool comp_op(void);
 extern bool div_op(void);
 extern bool from_json(void);
 extern bool mul_op(void);
+extern bool pow_op(void);
 extern bool sparse(void);
 extern bool sub_op(void);
 extern bool sum_op(void);
@@ -58,6 +59,7 @@ int main(void)
     Run( comp_op,              "comp_op"         );
     Run( from_json,            "from_json"       );
     Run( mul_op,               "mul_op"          );
+    Run( pow_op,               "pow_op"          );
     Run( sparse,               "sparse"          );
     Run( sub_op,               "sub_op"          );
     Run( sum_op,               "sum_op"          );
