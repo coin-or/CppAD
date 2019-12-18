7310deb41a7c795af9c102c58231b08d373edd29
Brad Bell <bradbell@seanet.com>
2019-12-18 16:26:29 -0700

    update gh-pages to version 20191218

--- doc/_whats_new_19_htm.js
+++ doc/_whats_new_19_htm.js
@@ -12,6 +12,8 @@ var list_up0 = [
 'whats_new_19.htm'
 ];
 var list_current0 = [
+'whats_new_19.htm#12-18',
+'whats_new_19.htm#12-16',
 'whats_new_19.htm#12-15',
 'whats_new_19.htm#12-14',
 'whats_new_19.htm#12-13',
