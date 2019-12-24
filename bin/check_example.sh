b163b1a7fd7e537168b43f8c3613f211b51b57a8
Brad Bell <bradbell@seanet.com>
2019-12-24 04:22:13 -0700

    master: check_example.sh: check that all examples are present for more directories.

--- bin/check_example.sh
+++ bin/check_example.sh
@@ -57,33 +57,45 @@ then
     echo "Error: nothing should be between the two dashed lines above"
     exit 1
 fi
-echo 'Checking example/general file versus example names'
 #
 # fix sort order; see
 # unix.stackexchange.com/questions/87745/what-does-lc-all-c-do/87763#87763
 export LC_ALL='C'
 #
-dir='example/general'
-ls $dir/*.cpp \
-    | sed -e "s|$dir/||" -e 's|\.cpp$||' -e '/^general$/d' > check_example.1.$$
-list=`ls $dir/*.hpp \
-    | sed -e "s|$dir/||" -e 's|\.hpp$||' -e '/^general$/d'`
-for file in $list
+dir_list='
+    example/graph
+    example/json
+    example/general
+'
+for dir in $dir_list
 do
-    sed -i check_example.1.$$ -e "/^$file\$/d"
+    echo "Checking $dir file versus example names"
+    name=`echo $dir | sed -e 's|.*/||'`
+    #
+    ls $dir/*.cpp | sed  \
+        -e "s|$dir/||" -e "/^$name\\.cpp/d" -e 's|\.cpp||' > check_example.1.$$
+    if [ "$name" == 'general' ]
+    then
+        list=`ls $dir/*.hpp \
+            | sed -e "s|$dir/||" -e 's|\.hpp$||' -e '/^general$/d'`
+        for file in $list
+        do
+            sed -i check_example.1.$$ -e "/^$file\$/d"
+        done
+    fi
+    sed -n -e '/^extern bool [a-z0-9A-Z_]*(void);/p' $dir/$name.cpp \
+        | sed -e 's/extern bool \([a-z0-9A-Z_]*\)(void);/\1/' \
+        | sed -e 's/\([a-z]\)\([A-Z]\)/\1_\2/g' \
+        | tr '[A-Z]' '[a-z]' \
+        | sort > check_example.2.$$
+    if ! diff check_example.1.$$ check_example.2.$$
+    then
+        rm check_example.1.$$ check_example.2.$$
+        echo "$dir: file and function names do not agree"
+        echo 'see output above.'
+        exit 1
+    fi
 done
-sed -n -e '/^extern bool [a-z0-9A-Z_]*(void);/p' $dir/general.cpp \
-    | sed -e 's/extern bool \([a-z0-9A-Z_]*\)(void);/\1/' \
-    | sed -e 's/\([a-z]\)\([A-Z]\)/\1_\2/g' \
-    | tr '[A-Z]' '[a-z]' \
-    | sort > check_example.2.$$
-if ! diff check_example.1.$$ check_example.2.$$
-then
-    rm check_example.1.$$ check_example.2.$$
-    echo 'example/general: file and function names do not agree;'
-    echo 'see output above.'
-    exit 1
-fi
 rm check_example.1.$$ check_example.2.$$
 #
 echo 'bin/check_example.sh: OK'
