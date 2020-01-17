bf51725a56353eda352bdac0430a205f4f1016f1
Brad Bell <bradbell@seanet.com>
2020-01-17 06:45:43 -0700

    master: Add the speed template package (starting point for a new package).

--- speed/main.cpp
+++ speed/main.cpp
@@ -1,5 +1,5 @@
 /* --------------------------------------------------------------------------
-CppAD: C++ Algorithmic Differentiation: Copyright (C) 2003-19 Bradley M. Bell
+CppAD: C++ Algorithmic Differentiation: Copyright (C) 2003-20 Bradley M. Bell
 
 CppAD is distributed under the terms of the
              Eclipse Public License Version 2.0.
@@ -43,6 +43,9 @@ in the Eclipse Public License, Version 2.0 are satisfied:
 # endif
 # ifdef CPPAD_SACADO_SPEED
 # define AD_PACKAGE "sacado"
+# endif
+# ifdef CPPAD_TEMPLATE_SPEED
+# define AD_PACKAGE "template"
 # endif
 
 /*
