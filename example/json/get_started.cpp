670fcb1a5430233797348625a0f1fc5ffd26704e
Brad Bell <bradbell@seanet.com>
2020-01-22 18:08:43 -0700

    master: json: get_started.cpp: demonstrate json of derivative.

--- example/json/get_started.cpp
+++ example/json/get_started.cpp
@@ -131,6 +131,7 @@ $cref/n_arg not in definition
     /op_usage
     /n_arg Not In Definition
 /$$:
+
 $srccode%cpp% */
         "       [ 2, 1, 3, [4, 5, 6] ] ]\n" // sin(p[0])+sin(x[0])+sin(c[0])
 /* %$$
@@ -147,47 +148,70 @@ $subhead End Function$$
 $srccode%cpp% */
         "}\n";
 /* %$$
-
-$head Rest of Example Code$$
+$head Convert Single to Double Quotes$$
 $srccode%cpp% */
-    // Convert the single quote to double quote
     for(size_t i = 0; i < json.size(); ++i)
         if( json[i] == '\'' ) json[i] = '"';
-    //
-    // f(x, p) = sin(p_0) + sin(x_0) + sin(c_0)
+/* %$$
+$head double f(x, p)$$
+$srccode%cpp% */
     CppAD::ADFun<double> f;
     f.from_json(json);
-    ok &= f.Domain() == 1;
-    ok &= f.Range() == 1;
-    ok &= f.size_dyn_ind() == 1;
-    //
-    // value of constant in function
-    vector<double> c(1);
-    c[0] = -0.1;
-    //
-    // set independent variables and parameters
-    vector<double> p(1), x(1);
-    p[0] = 0.2;
-    x[0] = 0.3;
-    //
-    // compute y = f(x, p)
-    f.new_dynamic(p);
-    vector<double> y = f.Forward(0, x);
-    //
-    // check result
-    double check = std::sin(p[0]) + std::sin(x[0]) + std::sin(c[0]);
-    ok &= CppAD::NearEqual(y[0], check, eps99, eps99);
-    // -----------------------------------------------------------------------
-    // Convert to Json graph and back again
-    json = f.to_json();
-    f.from_json(json);
-    // -----------------------------------------------------------------------
+/* %$$
+$head Check f(x, p)$$
+$srccode%cpp% */
+    vector<double> c(1), p(1), x(1), y(1);
+    c[0] = -0.1; // must match value in graph
+    p[0] = 0.2;  // can be any value
+    x[0] = 0.3;  // can be any value
     //
     // compute y = f(x, p)
     f.new_dynamic(p);
     y = f.Forward(0, x);
     //
-    // check result
+    // f(x, p) = sin(p_0) + sin(x_0) + sin(c_0)
+    double check = std::sin(p[0]) + std::sin(x[0]) + std::sin(c[0]);
+    ok &= CppAD::NearEqual(y[0], check, eps99, eps99);
+
+/* %$$
+$head AD<double> f(x, p)$$
+$srccode%cpp% */
+    CppAD::ADFun< AD<double>, double > af;
+    af = f.base2ad();
+/* %$$
+$head Evaluate Derivative$$
+$srccode%cpp% */
+    // set independent variables and parameters
+    vector< AD<double> > ap(1), ax(1);
+    ap[0] = 0.2;
+    ax[0] = 0.3;
+    //
+    // record computation of y = d/dx f(x, p)
+    CppAD::Independent(ax, ap);
+    af.new_dynamic(ap);
+    vector< AD<double> > ay = af.Jacobian(ax);
+/* %$$
+$head double g(x, p) = d/dx f(x, p)$$
+$srccode%cpp% */
+    CppAD::ADFun<double> g(ax, ay);
+/* %$$
+$head Convert to Json and Back$$
+$srccode%cpp% */
+    json = g.to_json();
+    g.from_json(json);
+/* %$$
+$head Check g(x, p)$$
+$srccode%cpp% */
+    c[0] = -0.1; // must match value in graph
+    p[0] = 0.3;  // can be any value
+    x[0] = 0.4;  // can be any value
+    //
+    // compute y = f(x, p)
+    g.new_dynamic(p);
+    y = g.Forward(0, x);
+    //
+    // g(x, p) = d/dx f(x, p) = d/dx sin(x) = cos(x)
+    check = std::cos(x[0]);
     ok &= CppAD::NearEqual(y[0], check, eps99, eps99);
     //
     return ok;
