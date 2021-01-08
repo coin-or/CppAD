764d605690b3ca6c6e519c380ce0ff2aea1b656a
Brad Bell <bradbell@seanet.com>
2021-01-08 05:13:08 -0700

    master: json_lexer.cpp: add '# include <cctype>' (required on some systems).

--- cppad_lib/json_lexer.cpp
+++ cppad_lib/json_lexer.cpp
@@ -1,5 +1,5 @@
 /* --------------------------------------------------------------------------
-CppAD: C++ Algorithmic Differentiation: Copyright (C) 2003-19 Bradley M. Bell
+CppAD: C++ Algorithmic Differentiation: Copyright (C) 2003-21 Bradley M. Bell
 
   CppAD is distributed under the terms of the
                Eclipse Public License Version 2.0.
@@ -9,6 +9,7 @@ CppAD: C++ Algorithmic Differentiation: Copyright (C) 2003-19 Bradley M. Bell
   in the Eclipse Public License, Version 2.0 are satisfied:
         GNU General Public License, Version 2.0 or later.
 -------------------------------------------------------------------------- */
+# include <cctype>
 # include <cppad/local/graph/json_lexer.hpp>
 # include <cppad/local/graph/cpp_graph_op.hpp>
 # include <cppad/utility/error_handler.hpp>
