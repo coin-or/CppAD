2ee60cbd4f427db9803c9a9b750a3171869fbca0
Brad Bell <bradbell@seanet.com>
2021-03-03 05:15:58 -0700

    llvm: ir_to_graph.cpp: remove unnecessary includes.

--- cppad_lib/llvm/ir_to_graph.cpp
+++ cppad_lib/llvm/ir_to_graph.cpp
@@ -9,28 +9,6 @@ Secondary License when the conditions for such availability set forth
 in the Eclipse Public License, Version 2.0 are satisfied:
       GNU General Public License, Version 2.0 or later.
 ---------------------------------------------------------------------------- */
-# include <cppad/core/llvm_ir.hpp>
-//
-# include <llvm/IR/DerivedTypes.h>
-# include <llvm/IR/InstIterator.h>
-# include <llvm/IR/IRBuilder.h>
-# include <llvm/IR/LegacyPassManager.h>
-# include <llvm/IR/Type.h>
-# include <llvm/IR/Verifier.h>
-//
-# include <llvm/Support/FileSystem.h>
-# include <llvm/Support/Host.h>
-# include <llvm/Support/raw_os_ostream.h>
-# include <llvm/Support/TargetRegistry.h>
-# include <llvm/Support/TargetSelect.h>
-//
-# include <llvm/Target/TargetOptions.h>
-# include <llvm/Target/TargetMachine.h>
-//
-//
-# include <llvm/Transforms/InstCombine/InstCombine.h>
-# include <llvm/Transforms/Scalar.h>
-# include <llvm/Transforms/Scalar/GVN.h>
 /*
 ---------------------------------------------------------------------------
 $begin llvm_ir_to_graph$$
@@ -84,6 +62,11 @@ of this operation.
 
 $end
 */
+# include <cppad/core/llvm_ir.hpp>
+//
+# include <llvm/IR/InstIterator.h>
+# include <llvm/IR/Constants.h>
+//
 namespace CppAD { // BEGIN_CPPAD_NAMESPACE
 
 // BEGIN_TO_GRAPH
