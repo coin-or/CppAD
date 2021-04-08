3b1f7824bd3a0d794fea3c569062828e29d48827
Brad Bell <bradbell@seanet.com>
2021-04-08 10:34:09 -0700

    llvm: cppad_link.cpp: prepare for adding cpapd_link_print.

--- cppad_lib/llvm/cppad_link.cpp
+++ cppad_lib/llvm/cppad_link.cpp
@@ -9,29 +9,36 @@ Secondary License when the conditions for such availability set forth
 in the Eclipse Public License, Version 2.0 are satisfied:
       GNU General Public License, Version 2.0 or later.
 ---------------------------------------------------------------------------- */
+# include <cmath>
 /*
-$begin cppad_link$$
+$begin cppad_link_fabs$$
 $spell
-    llvm
+    cppad
+    fabs
 $$
 
-$section Extern C Links To Functions Needed by llvm_link Objects$$
+$section The cppad_link_fabs Function$$
 
-$srcthisfile%
-    0%// BEGIN_C++%// END_C++%1
-%$$
-
-$end
-*/
-# include <cmath>
-// BEGIN_C++
-//
-// fabs
+$head Source$$
+$srccode%cpp% */
 extern "C" double cppad_link_fabs(double x)
 {   return std::fabs(x); }
-//
-// sign
+/* %$$
+$end
+------------------------------------------------------------------------------
+
+$begin cppad_link_sign$$
+$spell
+    cppad
+$$
+
+$section The cppad_link_sign Function$$
+
+$head Source$$
+$srccode%cpp% */
 extern "C" double cppad_link_sign(double x)
 {   return double(x > 0.0) - double(x < 0.0); }
-//
-// END_C++
+/* %$$
+$end
+------------------------------------------------------------------------------
+*/
