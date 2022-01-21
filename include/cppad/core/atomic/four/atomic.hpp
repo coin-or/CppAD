b373b063b98dadfcaca0786006579f75f064688c
Brad Bell <bradbell@seanet.com>
2022-01-21 06:10:39 -0700

    atomic: atomic_four: get_started.

--- include/cppad/core/atomic/four/atomic.hpp
+++ include/cppad/core/atomic/four/atomic.hpp
@@ -127,7 +127,10 @@ $begin atomic_four_example$$
 
 $section Example Defining Atomic Functions: Fourth Generation$$
 
-$comment%example/atomic_four/get_started.cpp
+$childtable%
+    example/atomic_four/get_started.cpp
+%$$
+$comment%
     %example/atomic_four/norm_sq.cpp
     %example/atomic_four/tangent.cpp
     %example/atomic_four/base2ad.cpp
