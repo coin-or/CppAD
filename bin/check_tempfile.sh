4c9d2d4d9f8899660408a4ee0a82545066f5a760
Brad Bell <bradbell@seanet.com>
2022-02-21 06:43:59 -0700

    master: check_tempfile.sh: change junk -> temp.

--- bin/check_tempfile.sh
+++ bin/check_tempfile.sh
@@ -1,6 +1,6 @@
 #! /bin/bash -e
 # -----------------------------------------------------------------------------
-# CppAD: C++ Algorithmic Differentiation: Copyright (C) 2003-19 Bradley M. Bell
+# CppAD: C++ Algorithmic Differentiation: Copyright (C) 2003-22 Bradley M. Bell
 #
 # CppAD is distributed under the terms of the
 #              Eclipse Public License Version 2.0.
@@ -18,8 +18,8 @@ fi
 # -----------------------------------------------------------------------------
 list=`ls | sed -n \
     -e '/^new.[0-9]*$/d' \
-    -e '/^junk.[0-9]*$/d' \
-    -e '/\/junk.[0-9]*$/d' \
+    -e '/^temp.[0-9]*$/d' \
+    -e '/\/temp.[0-9]*$/d' \
     -e '/\.[0-9]*$/p'`
 if [ "$list" != '' ]
 then
