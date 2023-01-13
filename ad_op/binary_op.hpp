a44b7827fca7865194faba9b421e9ce2af65eefa
Brad Bell <bradbell@seanet.com>
2023-01-13 02:53:15 -0700

    ad_op: add the binary_op_t.

---
+++ ad_op/binary_op.hpp
@@ -0,0 +1,31 @@
+# ifndef BINARY_OP_HPP
+# define BINARY_OP_HPP
+// SPDX-License-Identifier: EPL-2.0 OR GPL-2.0-or-later
+// SPDX-FileCopyrightText: Bradley M. Bell <bradbell@seanet.com>
+// SPDX-FileContributor: 2023-23 Bradley M. Bell
+// ----------------------------------------------------------------------------
+
+# include "op.hpp"
+
+// binary_op_t
+template <class Base>
+class binary_op_t : public op_t<Base> {
+public:
+   // op_enum
+   virtual op_enum_t op_enum(void) const override = 0;
+   //
+   // n_arg
+   addr_t n_arg(void) const override
+   {  return 2; }
+   addr_t n_res(void) const override
+   {  return 1; }
+   //
+   // eval
+   virtual void eval(
+      addr_t                arg_index    ,
+      const Vector<addr_t>& arg_vec      ,
+      addr_t                res_index    ,
+      Vector<Base>&         value_vec    ) const override = 0;
+};
+
+# endif
--- ad_op/sub_op.hpp
+++ ad_op/sub_op.hpp
@@ -4,21 +4,16 @@
 // SPDX-FileCopyrightText: Bradley M. Bell <bradbell@seanet.com>
 // SPDX-FileContributor: 2023-23 Bradley M. Bell
 // ----------------------------------------------------------------------------
-
-# include "op.hpp"
+# include "binary_op.hpp"
 
 // sub_op_t
 template <class Base>
-class sub_op_t : public op_t<Base> {
+class sub_op_t : public binary_op_t<Base> {
 public:
    // op_enum
    op_enum_t op_enum(void) const override
    {  return sub_op_enum; }
-   // n_arg
-   addr_t n_arg(void) const override
-   {  return 2; }
-   addr_t n_res(void) const override
-   {  return 1; }
+   //
    // eval
    void eval(
       addr_t                arg_index    ,
