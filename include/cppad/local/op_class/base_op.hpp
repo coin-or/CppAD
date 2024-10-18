c7e928991b6bbd6cc2be689c37e7f9f59402b2f6
Brad Bell <bradbell@seanet.com>
2024-10-18 07:51:01 -0700

    op_class: base_op.hpp: document bianry operators.

--- include/cppad/local/op_class/base_op.hpp
+++ include/cppad/local/op_class/base_op.hpp
@@ -4,13 +4,365 @@
 // SPDX-FileCopyrightText: Bradley M. Bell <bradbell@seanet.com>
 // SPDX-FileContributor: 2024 Bradley M. Bell
 // ----------------------------------------------------------------------------
+/*
+{xrst_begin_parent base_op_t dev}
+{xrst_spell
+   vv
+}
 
-namespace CppAD { namespace local { // BEGIN namespace
+The Operator Base Class
+#######################
 
-// base_op_t
+Syntax
+******
+| ``CppAD::local::base_op_t`` *base_op*
+
+Prototype
+*********
+{xrst_literal ,
+   BEGIN NAMESPACE , END NAMESPACE
+   BEGIN BASE_OP_T , END BASE_OP_T
+}
+
+Base
+****
+is the base type for the operator; i.e., this operation was recorded
+using ``AD`` < *Base* > and computations by this routine are done using
+type *Base* .
+
+Derived Documentation
+*********************
+{xrst_toc_table}
+
+Derived Classes
+***************
+
+.. csv-table::
+   :header-rows: 1
+
+   Class Name, x, y, z
+   add_vv, variable, variable, z = x + y
+
+
+{xrst_end base_op_t}
+# ----------------------------------------------------------------------------
+{xrst_begin derived_binary_forward dev}
+{xrst_spell
+   vp
+   tpv
+}
+
+Forward Variable Binary Operators
+#################################
+
+Syntax
+******
+| *base_op* . ``forward_0`` (
+| |tab| *i_z* , *arg* , *parameter* *cap_order* , *taylor*
+| )
+| *base_op* . ``forward`` (
+| |tab| *p* , *q* , *i_z* , *arg* , *parameter* *cap_order* , *taylor*
+| )
+| *base_op* . ``forward_dir`` (
+| |tab| *q* , *r* , *i_z* , *arg* , *parameter* *cap_order* , *taylor*
+| )
+
+Prototype
+*********
+{xrst_literal
+   // BEGIN FORWARD
+   // END FORWARD
+}
+
+Derived Class Operator
+**********************
+The derived class operator corresponding to *base_op* has
+two arguments and one result.
+The last two characters in the name for a binary operator are
+``p`` (for parameter) or ``v`` (for variable).
+For example the name ``add_vp`` corresponding to addition
+with a variable on the left (first) and a parameter on the right (second).
+
+x
+*
+We use *x* to denote the first argument of this binary operation.
+This argument is a variable or a parameter depending on the operator.
+
+y
+*
+We use *y* to denote the second argument of this binary operation.
+This argument is a variable or a parameter depending on the operator.
+
+z
+*
+We use *z* to denote the result of this binary operation.
+The result is always a variable.
+
+p
+*
+is lowest order of the Taylor coefficient that we are computing;
+*p* <= *q* .
+If *p* is not present ( ``forward_0`` and ``forward_dir`` ),
+the lowest order of the Taylor coefficient is order *q* .
+
+q
+*
+is highest order of the Taylor coefficient that we are computing.
+*q* < *cap_order* .
+The default value for *q* (when it is not present) is zero.
+If *q* is not present ( ``forward_0`` ),
+the highest order of the Taylor coefficient is order zero .
+
+r
+*
+If this argument is present ( ``forward_dir`` ),
+it is number of directions for Taylor coefficients that we are computing.
+Otherwise, we are only computing the coefficients for one direction.
+
+i_z
+***
+is the variable index corresponding to the result for this operation;
+i.e. the row index in *taylor* corresponding to *z* .
+
+arg
+***
+is the vector of indices for this operator.
+
+i_x
+===
+We use *i_x* = *arg* [0] for the
+index corresponding to the first operand for this operator.
+If *x* is a parameter,
+*parameter* [ *i_x* ] is the corresponding value.
+If *x* is a variable,
+*i_x* is the row index in *taylor* corresponding to *x* .
+
+i_y
+===
+We use *i_y* = *arg* [0] for the
+index corresponding to the second operand for this operator.
+If *y* is a parameter,
+*parameter* [ *i_y* ] is the corresponding value.
+If *y* is a variable,
+*i_y* is the row index in *taylor* corresponding to *y* .
+
+parameter
+*********
+maps parameter indices to parameter values.
+
+cap_order
+*********
+is the maximum number of orders that will fit in *taylor* .
+
+taylor
+******
+
+forward_0
+=========
+The Taylor coefficient corresponding to
+variable index *i* and order zero is
+
+   *taylor* [ *i* * *cap_order* ]
+
+forward
+=======
+The Taylor coefficient corresponding to
+variable index *i* and order *k* is
+
+   *taylor* [ *i* * *cap_order* + *k*  ]
+
+forward_dir
+===========
+The zero order Taylor coefficient for a variable
+is the same for all directions.  We use the notation
+
+   *tpv* = ( *cap_order* ``- 1`` ) * ``r`` + 1
+
+which is the number of Taylor coefficients per variable.
+The zero order Taylor coefficient for variable index *i* is
+
+   *taylor* [ *i* * *tpv*  + 0 ]
+
+For *k* > 0* ,
+and *ell* = 0 , .. , *r-1* ,
+The Taylor coefficient for variable *i* ,
+order *k* , and direction *ell* is
+
+   *taylor* [ *i* * *tpv* + ( *k* ``- 1`` ) * *r* + *ell*  + 1 ]
+
+
+Input
+=====
+
+#. If *x* is a variable,
+   the Taylor coefficients for variable *x* up to order *q* .
+#. If *y* is a variable,
+   the Taylor coefficients for variable *y* up to order *q* .
+#. The Taylor coefficients for variable *z* up to order *p* ``-1`` .
+
+Output
+======
+The Taylor coefficients for variable *z*
+from order *p* up to order *q*
+(and in the case of ``forward_dir`` for all the *r* directions).
+
+{xrst_end derived_binary_forward}
+------------------------------------------------------------------------------
+{xrst_begin derived_binary_reverse dev}
+{xrst_spell
+   vp
+}
+
+Reverse Variable Binary Operators
+#################################
+
+Syntax
+******
+| *base_op* . ``reverse`` (
+| *d* , *i_z* , *arg* , *parameter* , *cap_order* , *taylor* , *nc_partial* , *partial*
+| )
+
+Prototype
+*********
+{xrst_literal
+   // BEGIN REVERSE
+   // END REVERSE
+}
+
+
+Derived Class Operator
+**********************
+The derived class operator corresponding to *base_op* has
+two arguments and one result.
+The last two characters in the name for a binary operator are
+``p`` (for parameter) or ``v`` (for variable).
+For example the name ``add_vp`` corresponding to addition
+with a variable on the left (first) and a parameter on the right (second).
+
+x
+*
+We use *x* to denote the first argument of this binary operation.
+This argument is a variable or a parameter depending on the operator.
+
+y
+*
+We use *y* to denote the second argument of this binary operation.
+This argument is a variable or a parameter depending on the operator.
+
+z
+*
+We use *z* to denote the result of this binary operation.
+The result is always a variable.
+
+G
+*
+We use :math:`G(z, y, x, w, \ldots )` to denote a scalar valued function
+of the variables up to variable index *i_z* .
+
+H
+*
+We use :math:`H(y, x, w, \ldots )` to denote the scalar valued function
+of the variables up to variable index *i_z* ``-1`` defined by
+
+.. math::
+
+   H(y, x, w, \ldots ) = G [ z(x, y), y, x, w, \ldots ) ]
+
+d
+*
+is this highest order Taylor coefficient that we are computing
+partial derivatives with respect to;
+*d* < *cap_order* and *d* < *nc_partial* .
+
+i_z
+***
+is the variable index corresponding to the result for this operation;
+i.e. the row index in *taylor* corresponding to *z* .
+
+arg
+***
+is the vector of indices for this operator.
+
+i_x
+===
+We use *i_x* = *arg* [0] for the
+index corresponding to the first operand for this operator.
+If *x* is a parameter,
+*parameter* [ *i_x* ] is the corresponding value.
+If *x* is a variable,
+*i_x* is the row index in *taylor* corresponding to *x* .
+
+i_y
+===
+We use *i_y* = *arg* [0] for the
+index corresponding to the first operand for this operator.
+If *y* is a parameter,
+*parameter* [ *i_y* ] is the corresponding value.
+If *y* is a variable,
+*i_y* is the row index in *taylor* corresponding to *y* .
+
+parameter
+*********
+maps parameter indices to parameter values.
+
+cap_order
+*********
+is the maximum number of orders that will fit in *taylor* .
+
+taylor
+******
+The Taylor coefficient corresponding to
+variable index *i* and order *k* is
+
+   *taylor* [ *i* * *cap_order* + *k*  ]
+
+nc_partial
+**********
+is the number of columns in the partial array.
+
+partial
+*******
+The partial derivative w.r.t. the variable with index *i* and
+Taylor coefficient of order *k* is
+
+   *partial* [ *i* * *nc_partial* + *k* ]
+
+for *k* = 0 , ..., *d* .
+
+Input
+=====
+For variable *i* = 0 ,..., *i_z* ,
+*partial* contains the
+partial derivatives of :math:`G(z, y, x, w, \ldots)`.
+
+Output
+======
+The array *partial* contains the
+partial derivatives of :math:`H(x, w, \ldots)`.
+The partial derivative for the variable with index *i_z* is unspecified
+(may have been used for temporary storage).
+
+{xrst_end derived_binary_reverse}
+------------------------------------------------------------------------------
+*/
+
+// BEGIN NAMESPACE
+namespace CppAD { namespace local {
+// END NAMESPACE
+
+// BEGIN BASE_OP_T
 template <class Base> class base_op_t
+// END BASE_OP_T
 {
 public:
+   // BEGIN FORWARD
+   // forward_0
+   virtual void forward_0(
+      size_t        i_z         ,
+      const addr_t* arg         ,
+      const Base*   parameter   ,
+      size_t        cap_order   ,
+      Base*         taylor      ) const = 0;
    //
    // forward
    virtual void forward(
@@ -31,16 +383,9 @@ public:
       const Base*   parameter   ,
       size_t        cap_order   ,
       Base*         taylor      ) const = 0;
+   // END FORWARD
    //
-   // forward_0
-   virtual void forward_0(
-      size_t        i_z         ,
-      const addr_t* arg         ,
-      const Base*   parameter   ,
-      size_t        cap_order   ,
-      Base*         taylor      ) const = 0;
-   //
-   // reverse
+   // BEGIN REVERSE
    virtual void reverse(
       size_t        d           ,
       size_t        i_z         ,
@@ -50,8 +395,9 @@ public:
       const Base*   taylor      ,
       size_t        nc_partial  ,
       Base*         partial     ) const = 0;
+   // END REVERSE
 };
 
-}} // END namespace
+}} // end namespace
 
 # endif
