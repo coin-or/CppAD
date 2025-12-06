5ed1b8896fbe40b25fe64880b14fcef964202f6d
Brad Bell <bradbell@seanet.com>
2025-12-06 13:30:29 -0700

    master: issue_242.sh: correct file contributors.

--- bug/issue_242.sh
+++ bug/issue_242.sh
@@ -1,7 +1,8 @@
 #! /usr/bin/env bash
 # SPDX-License-Identifier: EPL-2.0 OR GPL-2.0-or-later
 # SPDX-FileCopyrightText: Bradley M. Bell <bradbell@seanet.com>
-# SPDX-FileContributor: 2003-22 Bradley M. Bell
+# SPDX-FileContributor: 2025 Perry de Valpine <
+# SPDX-FileContributor: 2025 Bradley M. Bell
 # -----------------------------------------------------------------------------
 set -e -u
 #
